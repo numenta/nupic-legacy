ffa5ef087cea71f6d23189c40354f3778f2b5887
Matthew Taylor <mtaylor@numenta.com>
2013-06-17 22:51:49 -0700

    The contributor listing used to fetch data from the nupic.tools server, but there wasn't really a good reason for it. Now it fetches from the csv file hosted on github pages and parses it  on the client.

--- javascripts/contributors.js
+++ javascripts/contributors.js
@@ -1,14 +1,34 @@
 (function() {
     var dataUrl = 'http://issues.numenta.org:8081/contributors.json?callback=?';
+    var csvUrl = '../../resources/contributors.csv';
     var headers = ['Name', 'Github', 'Committer', 'Reviewer'];
     var html = '<table>\n<tr>';
     headers.forEach(function(header) {
         html += '<th>' + header + '</th>';
     });
     html += '</tr>\n';
-    $.getJSON(dataUrl, function(data) {
-        console.log(data);
-        data.contributors.forEach(function(person) {
+
+    function csvToJson(csv) {
+        var contributors = [],
+            lines = csv.split('\n'),
+            header = lines.shift().split(',');
+        lines.forEach(function(line) {
+            var obj = {},
+                person = line.split(',');
+            header.forEach(function(key, i) {
+                if (person[i] == '0' || person[i] == '1') {
+                    obj[key] = parseInt(person[i]);
+                } else {
+                    obj[key] = person[i];
+                }
+            });
+            contributors.push(obj);
+        });
+        return contributors;
+    }
+
+    $.ajax(csvUrl).done(function(csv) {
+        csvToJson(csv).forEach(function(person) {
             html += '<tr>';
             headers.forEach(function(header) {
                 var value = person[header];
