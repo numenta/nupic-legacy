d2610c0278c25d92e950dad4bff70b9026123f75
gshotan <gshotan@groksolutions.com>
2013-07-25 03:41:57 -0700

    removed Column class and moved all functionality into SpatialPooler class

--- py/nupic/research/SpatialPoolerTest.py
+++ py/nupic/research/SpatialPoolerTest.py
@@ -1,6 +1,6 @@
 
 import unittest2 as unittest
-from SpatialPooler import SpatialPooler, Column, ColumnParams
+from SpatialPooler import SpatialPooler
 from nupic.bindings.math import SM32 as SparseMatrix, \
                                 SM_01_32_32 as SparseBinaryMatrix, \
                                 count_gte, GetNTAReal
@@ -33,161 +33,160 @@ class SpatialPoolerTest(unittest.TestCase):
                verbosityLevel = 0,
 			)
 
-		self._columnParams = ColumnParams(
-               stimulusThreshold = 1,
-               synPermInactiveDec = 0.01,
-               synPermActiveInc = 0.1,
-               synPermActiveSharedDec = 0.04,
-               synPermOrphanDec = 0.05,
-               synPermConnected = 0.10,
-               minPctDutyCycleBeforeInh = 0.001,
-               minPctDutyCycleAfterInh = 0.001,
-               dutyCyclePeriod = 1000,
-               maxFiringBoost = 10.0,
-               maxSSFiringBoost = 2.0,
-               maxSynPermBoost = 10.0,
-            )
-
-		self._column = Column(
-				numInputs = 5,
-            	columnParams = self._columnParams,
-            	initialPermanence = [ 0.095, 0.015,0.1,0.95,0.95],
-            	receptiveField = range(5)
-			)
 
 
-	def test_orphan(self):
-		c = self._column
-		c._active = True
-		c._overlapPct = 0.2
-		self.assertEqual(c.isOrphan(),False)
+	# def test_orphan(self):
+	# 	c = self._column
+	# 	c._active = True
+	# 	c._overlapPct = 0.2
+	# 	self.assertEqual(c.isOrphan(),False)
 
-		c._active = True
-		c._overlapPct = 1
-		self.assertEqual(c.isOrphan(),False)
+	# 	c._active = True
+	# 	c._overlapPct = 1
+	# 	self.assertEqual(c.isOrphan(),False)
 
-		c._active = False
-		c._overlapPct = 0.2
-		self.assertEqual(c.isOrphan(),False)
+	# 	c._active = False
+	# 	c._overlapPct = 0.2
+	# 	self.assertEqual(c.isOrphan(),False)
 
-		c._active = False
-		c._overlapPct = 1
-		self.assertEqual(c.isOrphan(),True)
+	# 	c._active = False
+	# 	c._overlapPct = 1
+	# 	self.assertEqual(c.isOrphan(),True)
 
+	def test_updateConnectedSynapses(self):
+		pass
 
 	def test_calculateSharedInputs(self):
 		sp = SpatialPooler(numInputs = 8, numColumns=5)
-		for c in sp._columns:
-			c._connectedSynapses = SparseBinaryMatrix([numpy.zeros(sp._numInputs)])
-		sp._columns[0]._connectedSynapses = SparseBinaryMatrix([[0, 1, 0, 1, 0, 1, 0, 1]])
-		sp._columns[1]._connectedSynapses = SparseBinaryMatrix([[0, 0, 0, 1, 0, 0, 0, 1]])
-		sp._columns[2]._connectedSynapses = SparseBinaryMatrix([[0, 0, 0, 0, 0, 0, 1, 0]])
-		sp._columns[3]._connectedSynapses = SparseBinaryMatrix([[0, 0, 1, 0, 0, 0, 1, 0]])
-		sp._columns[4]._connectedSynapses = SparseBinaryMatrix([[1, 0, 0, 0, 1, 0, 0, 0]])
-		inputVector =					   [1, 1, 1, 1, 0, 0, 0, 0]
+		sp._connectedSynapses = SparseBinaryMatrix([[0, 1, 0, 1, 0, 1, 0, 1],
+													[0, 0, 0, 1, 0, 0, 0, 1],
+													[0, 0, 0, 0, 0, 0, 1, 0],
+													[0, 0, 1, 0, 0, 0, 1, 0],
+													[1, 0, 0, 0, 1, 0, 0, 0]])
+		inputVector =					numpy.array([1, 1, 1, 1, 0, 0, 0, 0])
 		activeColumns = range(5)
 		sharedTrue = set([3])
 		shared = set(sp._calculateSharedInputs(inputVector, activeColumns))
 		self.assertSetEqual(shared, sharedTrue)
 
-		for c in sp._columns:
-			c._connectedSynapses = SparseBinaryMatrix([numpy.zeros(sp._numInputs)])
-		sp._columns[0]._connectedSynapses = SparseBinaryMatrix([[0, 1, 0, 1, 0, 1, 0, 1]])
-		sp._columns[1]._connectedSynapses = SparseBinaryMatrix([[0, 0, 0, 1, 0, 0, 0, 1]])
-		sp._columns[2]._connectedSynapses = SparseBinaryMatrix([[0, 0, 0, 0, 0, 0, 1, 0]])
-		sp._columns[3]._connectedSynapses = SparseBinaryMatrix([[0, 0, 1, 0, 0, 0, 1, 0]])
-		sp._columns[4]._connectedSynapses = SparseBinaryMatrix([[1, 0, 0, 0, 1, 0, 0, 0]])
-		inputVector =					   [1, 1, 1, 0, 1, 1, 1, 1]
+		sp._connectedSynapses = SparseBinaryMatrix([[0, 1, 0, 1, 0, 1, 0, 1],
+													[0, 0, 0, 1, 0, 0, 0, 1],
+													[0, 0, 0, 0, 0, 0, 1, 0],
+													[0, 0, 1, 0, 0, 0, 1, 0],
+													[1, 0, 0, 0, 1, 0, 0, 0]])
+		inputVector =					numpy.array([1, 1, 1, 0, 1, 1, 1, 1])
 		activeColumns = range(5)
 		sharedTrue = set([6,7])
 		shared = set(sp._calculateSharedInputs(inputVector, activeColumns))
 		self.assertSetEqual(shared, sharedTrue)
 
-		for c in sp._columns:
-			c._connectedSynapses = SparseBinaryMatrix([numpy.zeros(sp._numInputs)])
-		sp._columns[0]._connectedSynapses = SparseBinaryMatrix([[0, 1, 0, 1, 0, 1, 0, 1]])
-		sp._columns[1]._connectedSynapses = SparseBinaryMatrix([[0, 0, 0, 1, 0, 0, 0, 1]])
-		sp._columns[2]._connectedSynapses = SparseBinaryMatrix([[0, 0, 0, 0, 0, 0, 1, 0]])
-		sp._columns[3]._connectedSynapses = SparseBinaryMatrix([[0, 0, 1, 0, 0, 0, 1, 0]])
-		sp._columns[4]._connectedSynapses = SparseBinaryMatrix([[1, 0, 0, 0, 1, 0, 0, 0]])
-		inputVector =					   [0, 0, 0, 0, 0, 0, 0, 0]
+		sp._connectedSynapses = SparseBinaryMatrix([[0, 1, 0, 1, 0, 1, 0, 1],
+													[0, 0, 0, 1, 0, 0, 0, 1],
+													[0, 0, 0, 0, 0, 0, 1, 0],
+													[0, 0, 1, 0, 0, 0, 1, 0],
+													[1, 0, 0, 0, 1, 0, 0, 0]])
+		inputVector =					numpy.array([0, 0, 0, 0, 0, 0, 0, 0])
 		activeColumns = range(5)
 		sharedTrue = set([])
 		shared = set(sp._calculateSharedInputs(inputVector, activeColumns))
 		self.assertSetEqual(shared, sharedTrue)
 
 
-		for c in sp._columns:
-			c._connectedSynapses = SparseBinaryMatrix([numpy.zeros(sp._numInputs)])
-		sp._columns[0]._connectedSynapses = SparseBinaryMatrix([[0, 1, 0, 1, 0, 1, 0, 1]])
-		sp._columns[1]._connectedSynapses = SparseBinaryMatrix([[0, 0, 0, 1, 0, 0, 0, 1]])
-		sp._columns[2]._connectedSynapses = SparseBinaryMatrix([[0, 0, 0, 0, 0, 0, 1, 0]])
-		sp._columns[3]._connectedSynapses = SparseBinaryMatrix([[0, 0, 1, 0, 0, 0, 1, 0]])
-		sp._columns[4]._connectedSynapses = SparseBinaryMatrix([[1, 0, 0, 0, 1, 0, 0, 0]])
-		inputVector =					   [1, 0, 1, 0, 1, 1, 1, 0]
+		sp._connectedSynapses = SparseBinaryMatrix([[0, 1, 0, 1, 0, 1, 0, 1],
+													[0, 0, 0, 1, 0, 0, 0, 1],
+													[0, 0, 0, 0, 0, 0, 1, 0],
+													[0, 0, 1, 0, 0, 0, 1, 0],
+													[1, 0, 0, 0, 1, 0, 0, 0]])
+		inputVector =					numpy.array([1, 0, 1, 0, 1, 1, 1, 0])
 		activeColumns = [1,2,3]
 		sharedTrue = set([6])
 		shared = set(sp._calculateSharedInputs(inputVector, activeColumns))
 		self.assertSetEqual(shared, sharedTrue)
 		
-		for c in sp._columns:
-			c._connectedSynapses = SparseBinaryMatrix([numpy.zeros(sp._numInputs)])
-		sp._columns[0]._connectedSynapses = SparseBinaryMatrix([[0, 1, 0, 1, 0, 1, 0, 1]])
-		sp._columns[1]._connectedSynapses = SparseBinaryMatrix([[0, 0, 0, 1, 0, 0, 0, 1]])
-		sp._columns[2]._connectedSynapses = SparseBinaryMatrix([[0, 0, 0, 0, 0, 0, 1, 0]])
-		sp._columns[3]._connectedSynapses = SparseBinaryMatrix([[0, 0, 1, 0, 0, 0, 1, 0]])
-		sp._columns[4]._connectedSynapses = SparseBinaryMatrix([[1, 0, 0, 0, 1, 0, 0, 0]])
-		inputVector =					   [1, 0, 1, 1, 1, 1, 1, 1]
+		sp._connectedSynapses = SparseBinaryMatrix([[0, 1, 0, 1, 0, 1, 0, 1],
+													[0, 0, 0, 1, 0, 0, 0, 1],
+													[0, 0, 0, 0, 0, 0, 1, 0],
+													[0, 0, 1, 0, 0, 0, 1, 0],
+													[1, 0, 0, 0, 1, 0, 0, 0]])
+		inputVector =					numpy.array([1, 0, 1, 1, 1, 1, 1, 1])
 		activeColumns = [0,1,3,4]
 		sharedTrue = set([3,7])
 		shared = set(sp._calculateSharedInputs(inputVector, activeColumns))
 		self.assertSetEqual(shared, sharedTrue)
-		#import pdb; pdb.set_trace()
 
 
 
-	def test_computeOverlap(self):
+	def test_calculateOverlap(self):
 		"""
 		test that column computes overlap and percent overlap correctly
 		"""
-		c = self._column
-		c._numInputs = 10
-		c._connectedSynapses = SparseBinaryMatrix([numpy.ones(c._numInputs)])
-		inputVector = numpy.zeros(c._numInputs, dtype='float32')
-		overlap, overlapPct = c.computeOverlap(inputVector)
-		self.assertEqual(overlap,0)
-		self.assertEqual(overlapPct,0)
+		sp = SpatialPooler(numInputs = 10, numColumns=5)
+		sp._connectedSynapses = \
+			SparseBinaryMatrix([[1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+								[0, 0, 1, 1, 1, 1, 1, 1, 1, 1],
+								[0, 0, 0, 0, 1, 1, 1, 1, 1, 1],
+								[0, 0, 0, 0, 0, 0, 1, 1, 1, 1],
+								[0, 0, 0, 0, 0, 0, 0, 0, 1, 1]])
+		sp._connectedCounts = numpy.array([10.0, 8.0, 6.0, 4.0, 2.0])
+		inputVector = numpy.zeros(sp._numInputs, dtype='float32')
+		sp._calculateOverlap(inputVector)
+		trueOverlaps = list(numpy.array([0, 0, 0, 0, 0]))
+		trueOverlapsPct = list(numpy.array([0, 0, 0, 0, 0]))
+		overlaps = list(sp._overlaps)
+		overlapsPct = list(sp._overlapsPct)
+		self.assertListEqual(overlaps,trueOverlaps)
+		self.assertListEqual(overlapsPct,trueOverlapsPct)
 
-		inputVector = numpy.ones(c._numInputs, dtype='float32')
-		overlap, overlapPct = c.computeOverlap(inputVector)
-		self.assertEqual(overlap,10)
-		self.assertEqual(overlapPct,1)
+		sp._connectedSynapses = \
+			SparseBinaryMatrix([[1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+								[0, 0, 1, 1, 1, 1, 1, 1, 1, 1],
+								[0, 0, 0, 0, 1, 1, 1, 1, 1, 1],
+								[0, 0, 0, 0, 0, 0, 1, 1, 1, 1],
+								[0, 0, 0, 0, 0, 0, 0, 0, 1, 1]])
+		sp._connectedCounts = numpy.array([10.0, 8.0, 6.0, 4.0, 2.0])
+		inputVector = numpy.ones(sp._numInputs, dtype='float32')
+		sp._calculateOverlap(inputVector)
+		trueOverlaps = list(numpy.array([10, 8, 6, 4, 2]))
+		trueOverlapsPct = list(numpy.array([1, 1, 1, 1, 1]))
+		overlaps = list(sp._overlaps)
+		overlapsPct = list(sp._overlapsPct)
+		self.assertListEqual(overlaps,trueOverlaps)
+		self.assertListEqual(overlapsPct,trueOverlapsPct)
 
-		inputVector = numpy.zeros(c._numInputs, dtype='float32')
-		inputVector[0] = 1
-		overlap, overlapPct = c.computeOverlap(inputVector)
-		self.assertEqual(overlap,1)
-		self.assertAlmostEqual(overlapPct,0.1)
+		sp._connectedSynapses = \
+			SparseBinaryMatrix([[1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+								[0, 0, 1, 1, 1, 1, 1, 1, 1, 1],
+								[0, 0, 0, 0, 1, 1, 1, 1, 1, 1],
+								[0, 0, 0, 0, 0, 0, 1, 1, 1, 1],
+								[0, 0, 0, 0, 0, 0, 0, 0, 1, 1]])
+		sp._connectedCounts = numpy.array([10.0, 8.0, 6.0, 4.0, 2.0])
+		inputVector = numpy.zeros(sp._numInputs, dtype='float32')
+		inputVector[9] = 1
+		sp._calculateOverlap(inputVector)
+		trueOverlaps = list(numpy.array([1, 1, 1, 1, 1]))
+		trueOverlapsPct = list(numpy.array([0.1, 0.125, 1.0/6, 0.25, 0.5]))
+		overlaps = list(sp._overlaps)
+		overlapsPct = list(sp._overlapsPct)
+		self.assertListEqual(overlaps,trueOverlaps)
+		self.assertListEqual(overlapsPct,trueOverlapsPct)
 
 		#zig-zag
-		connectedSynapses = numpy.zeros(10)
-		connectedSynapses[range(0,10,2)] = 1
-		c._connectedSynapses = SparseBinaryMatrix([connectedSynapses])
-		inputVector = numpy.zeros(10, dtype='float32')
-		inputVector[range(1,10,2)] = 1
-		overlap, overlapPct = c.computeOverlap(inputVector)
-		self.assertEqual(overlap,0)
-		self.assertEqual(overlapPct,0)
-
-		connectedSynapses = numpy.zeros(10)
-		connectedSynapses[range(0,7)] = 1
-		c._connectedSynapses = SparseBinaryMatrix([connectedSynapses])
-		inputVector = numpy.zeros(10, dtype='float32')
-		inputVector[range(5,7)] = 1
-		overlap, overlapPct = c.computeOverlap(inputVector)
-		self.assertEqual(overlap,2)
-		self.assertAlmostEqual(overlapPct,2.0/7.0)
-
+		sp._connectedSynapses = \
+			SparseBinaryMatrix([[1, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+								[0, 1, 0, 0, 0, 0, 1, 0, 0, 0],
+								[0, 0, 1, 0, 0, 0, 0, 1, 0, 0],
+								[0, 0, 0, 1, 0, 0, 0, 0, 1, 0],
+								[0, 0, 0, 0, 1, 0, 0, 0, 0, 1]])
+		sp._connectedCounts = numpy.array([2.0, 2.0, 2.0, 2.0, 2.0])
+		inputVector = numpy.zeros(sp._numInputs, dtype='float32')
+		inputVector[range(0,10,2)] = 1
+		sp._calculateOverlap(inputVector)
+		trueOverlaps = list(numpy.array([1, 1, 1, 1, 1]))
+		trueOverlapsPct = list(numpy.array([0.5, 0.5, 0.5, 0.5, 0.5]))
+		overlaps = list(sp._overlaps)
+		overlapsPct = list(sp._overlapsPct)
+		self.assertListEqual(overlaps,trueOverlaps)
+		self.assertListEqual(overlapsPct,trueOverlapsPct)
 
 
 	def test_initPermanence1(self):
@@ -199,19 +198,18 @@ class SpatialPoolerTest(unittest.TestCase):
 		"""
 
 		sp = self._sp
-		cp = sp._columnParams
 		sp._receptiveFieldRadius = 2
 		sp._receptiveFieldPctPotential = 1
 		perm = sp._initPermanence(0)
-		connected = (perm > cp.synPermConnected).astype(int)
+		connected = (perm > sp._synPermConnected).astype(int)
 		numcon = (connected.nonzero()[0]).size
 		self.assertEqual(numcon, sp._numInputs)
-		maxThresh = cp.synPermConnected + cp.synPermActiveInc/4
+		maxThresh = sp._synPermConnected + sp._synPermActiveInc/4
 		self.assertEqual((perm <= maxThresh).all(),True)
 
 		sp._receptiveFieldPctPotential = 0
 		perm = sp._initPermanence(0)
-		connected = (perm > cp.synPermConnected).astype(int)
+		connected = (perm > sp._synPermConnected).astype(int)
 		numcon = (connected.nonzero()[0]).size
 		self.assertEqual(numcon, 0)
 
@@ -219,13 +217,13 @@ class SpatialPoolerTest(unittest.TestCase):
 		sp._receptiveFieldRadius = 100
 		sp._numInputs = 100
 		perm = sp._initPermanence(0)
-		connected = (perm > cp.synPermConnected).astype(int)
+		connected = (perm > sp._synPermConnected).astype(int)
 		numcon = (connected.nonzero()[0]).size
 		self.assertGreater(numcon, 0)
 		self.assertLess(numcon,sp._numInputs)
 
-		minThresh = cp.synPermActiveInc / 2.0
-		connThresh = cp.synPermConnected
+		minThresh = sp._synPermActiveInc / 2.0
+		connThresh = sp._synPermConnected
 		self.assertEqual(numpy.logical_and((perm >= minThresh),
 			(perm <= connThresh)).any(),True)
 
@@ -237,53 +235,41 @@ class SpatialPoolerTest(unittest.TestCase):
 		a column's receptive field
 		"""
 		sp = self._sp
-		cp = sp._columnParams
 
 		sp._numInputs = 10
 		sp._receptiveFieldRadius = 1
 		index = 0
 		sp._receptiveFieldPctPotential = 1
+		#import pdb; pdb.set_trace()
 		perm = sp._initPermanence(index)
-  		permNonZero = (perm > 0).astype(int)
-		allInputs = set(range(sp._numInputs))
-		connMask = set([0,1,9])
-		unconnMask = allInputs - connMask
-		self.assertEqual((perm[list(connMask)]).all() ,True)
-		self.assertEqual((perm[list(unconnMask)]).any(), False)
+  		connected = list((perm > 0).astype(int))
+		trueConnected = [1,1,0,0,0,0,0,0,0,1]
+		self.assertListEqual(connected,trueConnected)
 
 		sp._receptiveFieldRadius = 1
 		sp._receptiveFieldPctPotential = 1
 		index = 5
 		perm = sp._initPermanence(index)
-  		permNonZero = (perm > 0).astype(int)
-		allInputs = set(range(sp._numInputs))
-		connMask = set([4,5,6])
-		unconnMask = allInputs - connMask
-		self.assertEqual((perm[list(connMask)]).all() ,True)
-		self.assertEqual((perm[list(unconnMask)]).any(), False)
+  		connected = list((perm > 0).astype(int))
+		trueConnected = [0,0,0,0,1,1,1,0,0,0]
+		self.assertListEqual(connected,trueConnected)
 
 
 		sp._receptiveFieldRadius = 1
 		sp._receptiveFieldPctPotential = 1
 		index = 9
 		perm = sp._initPermanence(index)
-  		permNonZero = (perm > 0).astype(int)
-		allInputs = set(range(sp._numInputs))
-		connMask = set([0,8,9])
-		unconnMask = allInputs - connMask
-		self.assertEqual((perm[list(connMask)]).all() ,True)
-		self.assertEqual((perm[list(unconnMask)]).any(), False)
+  		connected = list((perm > 0).astype(int))
+		trueConnected = [1,0,0,0,0,0,0,0,1,1]
+		self.assertListEqual(connected,trueConnected)
 
 		sp._receptiveFieldRadius = 4
 		sp._receptiveFieldPctPotential = 1
 		index = 2
 		perm = sp._initPermanence(index)
-  		permNonZero = (perm > 0).astype(int)
-		allInputs = set(range(sp._numInputs))
-		connMask = allInputs - set([7])
-		unconnMask = allInputs - connMask
-		self.assertEqual((perm[list(connMask)]).all() ,True)
-		self.assertEqual((perm[list(unconnMask)]).any(), False)
+  		connected = list((perm > 0).astype(int))
+		trueConnected = [1,1,1,1,1,1,1,0,1,1]
+		self.assertListEqual(connected,trueConnected)
 
 	def test_initPermanence3(self):
 		"""
@@ -297,41 +283,36 @@ class SpatialPoolerTest(unittest.TestCase):
 		to the mathematical formula. also check the effects of
 		supplying a maxPeriod to the function.
 		"""
-		dc = 1000.0
+		dc = numpy.zeros(5)
+		dc = numpy.array([1000.0, 1000.0, 1000.0, 1000.0, 1000.0])
 		period = 1000
-		maxperiod = -1
-		newval = 0
-		dc = Column._updateDutyCycle(dc, newval, period, maxperiod)
-		self.assertEqual(dc,999)
+		newvals = numpy.zeros(5)
+		newDc = SpatialPooler._updateDutyCycle(dc, newvals, period)
+		trueNewDc = [999, 999, 999, 999, 999]
+		self.assertListEqual(list(newDc),trueNewDc)
 
-		dc = 1000.0
+		dc = numpy.array([1000.0, 1000.0, 1000.0, 1000.0, 1000.0])
 		period = 1000
-		maxperiod = -1
-		newval = 1000
-		dc = Column._updateDutyCycle(dc, newval, period, maxperiod)
-		self.assertEqual(dc,1000)
+		newvals = numpy.zeros(5)
+		newvals.fill(1000)
+		newDc = SpatialPooler._updateDutyCycle(dc, newvals, period)
+		trueNewDc = list(dc)
+		self.assertListEqual(list(newDc),trueNewDc)
 
-		dc = 1000.0
+		dc = numpy.array([1000, 1000, 1000, 1000, 1000])
+		newvals = numpy.array([2000, 4000, 5000, 6000, 7000])
 		period = 1000
-		maxperiod = -1
-		newval = 2000
-		dc = Column._updateDutyCycle(dc, newval, period, maxperiod)
-		self.assertEqual(dc,1001)
+		newDc = SpatialPooler._updateDutyCycle(dc, newvals, period)
+		trueNewDc = [1001, 1003, 1004, 1005, 1006]
+		self.assertListEqual(list(newDc),trueNewDc)
 
-		#test effeects of max period
-		dc = 1000.0
-		period = 1000
-		maxperiod = 2
-		newval = 500
-		dc = Column._updateDutyCycle(dc, newval, period, maxperiod)
-		self.assertEqual(dc,750)
-
-		dc = 1000.0
-		period = 1000
-		maxperiod = 10
-		newval = 500
-		dc = Column._updateDutyCycle(dc, newval, period, maxperiod)
-		self.assertEqual(dc,950)				
+		dc = numpy.array([1000, 800, 600, 400, 2000])
+		newvals = numpy.zeros(5)
+		period = 2
+		newDc = SpatialPooler._updateDutyCycle(dc, newvals, period)
+		trueNewDc = [500, 400, 300, 200, 1000]
+		self.assertListEqual(list(newDc),trueNewDc)
+		
 
 	# def test_compute1(self):
 	# 	sp = self._sp
