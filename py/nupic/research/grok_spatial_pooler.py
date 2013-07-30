9643dbfe8f23457db669f1aa4e5aef65baabf036
gshotan <gshotan@groksolutions.com>
2013-07-30 16:31:49 -0700

    separated grok and base spatial poolers into two files, updated import path, made stylistic changes

---
+++ py/nupic/research/grok_spatial_pooler.py
@@ -0,0 +1,109 @@
+# ----------------------------------------------------------------------
+# Numenta Platform for Intelligent Computing (NuPIC)
+# Copyright (C) 2013, Numenta, Inc.  Unless you have purchased from
+# Numenta, Inc. a separate commercial license for this software code, the
+# following terms and conditions apply:
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see http://www.gnu.org/licenses.
+#
+# http://numenta.org/licenses/
+# ----------------------------------------------------------------------
+
+import copy
+import cPickle
+import itertools
+import numpy
+from operator import itemgetter
+
+from nupic.bindings.math import SM32 as SparseMatrix, \
+										SM_01_32_32 as SparseBinaryMatrix, \
+										count_gte, GetNTAReal
+from nupic.bindings.algorithms import Inhibition2 as Inhibition, \
+													 cpp_overlap, cpp_overlap_sbm
+from nupic.bindings.algorithms import adjustMasterValidPermanence
+from nupic.bindings.math import Random as NupicRandom
+from nupic.math.cross import cross
+import nupic.research.fdrutilities as fdru
+from nupic.research.spatial_pooler import SpatialPooler
+
+realDType = GetNTAReal()
+
+
+
+class GrokSpatialPooler(SpatialPooler):
+	"""
+	This class implements the SpatialPooler used in Grok.
+	"""
+
+	def __init__(self,
+							 numInputs,
+							 numColumns,
+							 localAreaDensity=0.1,
+							 numActiveColumnsPerInhArea=-1,
+							 stimulusThreshold=0,
+							 minDistance=0.0,
+							 maxBoost=10.0,
+							 seed=-1,
+							 spVerbosity=0,
+							 ):
+
+		super(GrokSpatialPooler,self).__init__(
+				inputDimensions=numInputs,
+				columnDimensions=numColumns,
+				potentialRadius=numInputs,
+				potentialPct=0.5,
+				globalInhibition=True,
+				localAreaDensity=localAreaDensity,
+				numActiveColumnsPerInhArea=numActiveColumnsPerInhArea,
+				stimulusThreshold=stimulusThreshold,
+				seed=seed
+			)
+
+		#verify input is valid
+		assert(numColumns > 0)
+		assert(numInputs > 0)
+
+		# save arguments
+		self._numInputs = numInputs
+		self._numColumns = numColumns
+		self._minDistance = minDistance
+
+		#set active duty cycles to ones, because they set anomaly scores to 0
+		self._activeDutyCycles = numpy.ones(self._numColumns)
+
+		# set of columns to be 'hungry' for learning
+		self._boostFactors *= maxBoost
+	
+
+	def compute(self, inputVector, learn=True):
+		assert (numpy.size(inputVector) == self._numInputs)
+		inputVector = numpy.array(inputVector, dtype=realDType)
+
+		overlaps, overlapsPct = self._calculateOverlap(inputVector)
+		vipColumns = self._selectVIPColumns(overlapsPct)
+		vipOverlaps = overlaps.copy()
+		vipOverlaps[vipColumns] = max(overlaps) + 1.0
+		activeColumns = self._inhibitColumns(vipOverlaps)
+		anomalyScore = self._calculateAnomalyScore(overlaps, activeColumns)
+
+		# if not learn: - don't let columns that never learned win! ???
+		if self._isUpdateRound():
+			self._updateMinDutyCycles()
+
+		self._updateBookeeping(learn)
+
+		return numpy.array(activeColumns), anomalyScore
+
+
+	def _selectVIPColumns(self, overlapsPct):
+		return numpy.where(overlapsPct >= (1.0 - self._minDistance))[0]
---
+++ tests/unit/py/nupic/research/grok_spatial_pooler_test.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+# ----------------------------------------------------------------------
+# Numenta Platform for Intelligent Computing (NuPIC)
+# Copyright (C) 2013, Numenta, Inc.  Unless you have purchased from
+# Numenta, Inc. a separate commercial license for this software code, the
+# following terms and conditions apply:
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see http://www.gnu.org/licenses.
+#
+# http://numenta.org/licenses/
+# ----------------------------------------------------------------------
+
+import numpy
+from mock import Mock, patch, ANY, call
+import unittest2 as unittest
+from nupic.bindings.math import SM32 as SparseMatrix, \
+                                SM_01_32_32 as SparseBinaryMatrix, \
+                                count_gte, GetNTAReal
+from nupic.research.spatial_pooler import GrokSpatialPooler
+
+class GrokSpatialPoolerTest(unittest.TestCase):
+
+	def setUp(self):
+		self._sp = GrokSpatialPooler(
+			   numInputs = 5,
+               numColumns = 5,
+               localAreaDensity = 0.1,
+               numActiveColumnsPerInhArea=-1,
+               stimulusThreshold=1,
+               minDistance = 0.0,
+               seed=-1,
+               spVerbosity=0,
+			)
+
+	def testSelectVIPColumns(self):
+		sp = self._sp
+		sp._numColumns = 7
+		sp._minDistance = 0.0
+		sp._overlapsPct = numpy.array([1.0, 0.7, 0.8, 0.1, 1.0, 0.3, 0.1])
+		vipColumns = sp._selectVIPColumns(sp._overlapsPct)
+		trueVIPColumns = [0, 4]
+		self.assertListEqual(trueVIPColumns, list(vipColumns))
+
+		sp._numColumns = 7
+		sp._minDistance = 0.1
+		sp._overlapsPct = numpy.array([0.0, 0.9, 0.85, 0.91, 1.0, 0.3, 0.89])
+		vipColumns = sp._selectVIPColumns(sp._overlapsPct)
+		trueVIPColumns = [1, 3, 4]
+		self.assertListEqual(trueVIPColumns, list(vipColumns))
+
+		sp._numColumns = 7
+		sp._minDistance = 0.15
+		sp._overlapsPct = numpy.array([0.0, 0.9, 0.85, 0.91, 1.0, 0.3, 0.89])
+		vipColumns = sp._selectVIPColumns(sp._overlapsPct)
+		trueVIPColumns = [1, 2, 3, 4, 6]
+		self.assertListEqual(trueVIPColumns, list(vipColumns))
+
+		sp._numColumns = 7
+		sp._minDistance = 1.0
+		sp._overlapsPct = numpy.array([0.0, 0.9, 0.85, 0.91, 1.0, 0.3, 0.89])
+		vipColumns = sp._selectVIPColumns(sp._overlapsPct)
+		trueVIPColumns = range(7)
+		self.assertListEqual(trueVIPColumns, list(vipColumns))
+
+		sp._numColumns = 7
+		sp._minDistance = 0.99
+		sp._overlapsPct = numpy.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
+		vipColumns = sp._selectVIPColumns(sp._overlapsPct)
+		trueVIPColumns = []
+		self.assertListEqual(trueVIPColumns, list(vipColumns))
+
+
+if __name__ == "__main__":
+  unittest.main()
