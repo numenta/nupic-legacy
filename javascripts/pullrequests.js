af64550b19bc1300be98287de8f1e66f4e3d092c
Matthew Taylor <mtaylor@numenta.com>
2013-07-31 08:58:18 -0700

    Reformatting pull request report. Emphasizing PRs over 7 days old.

--- javascripts/pullrequests.js
+++ javascripts/pullrequests.js
@@ -1,4 +1,6 @@
 (function() {
+    var OVERDUE_GAP = 7 * 24 * 60 * 60 * 1000;
+    var now = new Date().getTime();
     var dataUrl = 'http://issues.numenta.org:8081/prStatus.json?repo=numenta/nupic&callback=?';
     var prTemplate = Handlebars.compile($("#pr-template").html());
     var emptyPrTemplate = Handlebars.compile($("#empty-pr-template").html());
@@ -6,8 +8,13 @@
     $.getJSON(dataUrl, function(prs) {
         $pr.html('');
         if (prs.length) {
-            prs.forEach(function(pr) {
-                console.log(pr);
+            prs.sort(function(a, b) {
+                return new Date(a.created_at).getTime() - new Date(b.created_at).getTime();
+            }).forEach(function(pr) {
+                if ((now - new Date(pr.created_at).getTime()) > OVERDUE_GAP) {
+                    pr.overdue = true;
+                }
+                pr.created_at = pr.created_at.split('T').shift();
                 pr.latest_status = pr.statuses[0];
                 var html = prTemplate(pr);
                 $pr.append(html);
