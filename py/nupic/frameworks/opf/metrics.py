7a1fc513852f633e84926674b5ad0371aa2db995
Ellery <ewulczyn@groksolutions.com>
2013-08-01 15:52:20 -0700

    can now run anomaly models with custom error metrics

--- py/nupic/frameworks/opf/metrics.py
+++ py/nupic/frameworks/opf/metrics.py
@@ -767,11 +767,16 @@ class MetricMovingMean(AggregateMetric):
 
 def evalCustomErrorMetric(expr, prediction, groundTruth, tools):
   sandbox = SafeInterpreter(writer=StringIO())
-  sandbox.symtable['prediction'] = tools.mostLikely(prediction)
+  if(type(prediction)==float):
+    sandbox.symtable['prediction']=prediction
+  elif(type(prediction)==dict):
+    sandbox.symtable['prediction'] = tools.mostLikely(prediction)
+    sandbox.symtable['EXP'] = tools.expValue(prediction)
+    sandbox.symtable['probabilityDistribution'] = prediction
+  else:
+    assert False, "prediction is neither a dict nor float"
   sandbox.symtable['groundTruth'] = groundTruth
   sandbox.symtable['tools'] = tools
-  sandbox.symtable['EXP'] = tools.expValue(prediction)
-  sandbox.symtable['probabilityDistribution'] = prediction
   error = sandbox(expr)
   return error
 
