851782b3f7bb8dc2f37f7711703d76f6ec18bbf8
Marek Otahal <markotahal@gmail.com>
2013-08-04 22:35:58 +0200

    update APR for linux64
    old version: 1.2.2
    new ver: 1.4.8

--- external/linux64/include/apr-1/arch/unix/apr_arch_internal_time.h
+++ external/linux64/include/apr-1/arch/unix/apr_arch_internal_time.h
@@ -14,44 +14,11 @@
  * limitations under the License.
  */
 
-#ifndef APR_SUPPORT_H
-#define APR_SUPPORT_H
-
-/**
- * @file apr_support.h
- * @brief APR Support functions
- */
+#ifndef TIME_INTERNAL_H
+#define TIME_INTERNAL_H
 
 #include "apr.h"
-#include "apr_network_io.h"
-#include "apr_file_io.h"
 
-#ifdef __cplusplus
-extern "C" {
-#endif /* __cplusplus */
+void apr_unix_setup_time(void);
 
-/**
- * @defgroup apr_support Internal APR support functions
- * @ingroup APR 
- * @{
- */
-
-/**
- * Wait for IO to occur or timeout.
- *
- * @param f The file to wait on.
- * @param s The socket to wait on if @a f is @c NULL.
- * @param for_read If non-zero wait for data to be available to read,
- *        otherwise wait for data to be able to be written. 
- * @return APR_TIMEUP if we run out of time.
- */
-apr_status_t apr_wait_for_io_or_timeout(apr_file_t *f, apr_socket_t *s,
-                                        int for_read);
-
-/** @} */
-
-#ifdef __cplusplus
-}
-#endif
-
-#endif  /* ! APR_SUPPORT_H */
+#endif  /* TIME_INTERNAL_H */
