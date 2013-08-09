4929a8fa29e25eabdb807ebf72e012024eee67fd
Carl Friess <carlfriess@subutai2.gateway.2wire.net>
2013-08-08 21:51:41 -0700

    Adds sorting for contributor list. Default sorting: Reviewer -> Committer -> Name

--- javascripts/contributors.js
+++ javascripts/contributors.js
@@ -27,6 +27,22 @@
     $.ajax(csvUrl).done(function(csv) {
         var contribs = csvToJson(csv);
         $('#contributors').html(tmpl({headings: headings, contributors: contribs}));
+
+        $(document).ready(function() {
+            $("table").tablesorter({ 
+                sortList: [[3,0],[2,0],[0,0]] 
+            });
+            $("#tableHeaderCommitter").width(110);
+            $("#tableHeaderReviewer").width(90);
+            $(".tableHeaderTriangle").fadeOut(1000);
+            $("thead").hover(function(){
+                $(".tableHeaderTriangle").stop().fadeIn(100);
+            },function(){
+                setTimeout(function(){
+                    $(".tableHeaderTriangle").stop().fadeOut(500);
+                },1000);
+            });
+        });
     });
 
 }());
