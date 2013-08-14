f50edf4c6f64c10115945d99137ac937beb770d9
Austin Marshall <amarshall@numenta.com>
2013-08-14 11:52:50 -0700

    Triple verbosity and removing --target in favor of --prefix in --install-option

--- build.sh
+++ build.sh
@@ -61,13 +61,13 @@ PY_VER=`python -c 'import platform; print platform.python_version()[:3]'`
 function pythonSetup {
     python "$NUPIC/build_system/setup.py" --autogen
 
-    # workaround for matplotlib install bug: (and asteval) - numpy must already be installed
+    # workaround for matplotlib install bug: numpy must already be installed
     # see http://stackoverflow.com/questions/11797688/matplotlib-requirements-with-pip-install-in-virtualenv
     # https://github.com/matplotlib/matplotlib/wiki/MEP11
-    PATH=$NUPIC_INSTALL:$PATH pip install --find-links=file://$NUPIC/external/common/pip-cache --no-index --index-url=file:///dev/null --target=$NUPIC_INSTALL/lib/python${PY_VER}/site-packages --install-option="--install-scripts=$NUPIC_INSTALL/bin" --ignore-installed numpy==1.7.1
+    PATH=$NUPIC_INSTALL:$PATH pip install -vvv --find-links=file://$NUPIC/external/common/pip-cache --no-index --index-url=file:///dev/null --install-option="--install-scripts=$NUPIC_INSTALL/bin" --install-option="--prefix=$NUPIC_INSTALL" --ignore-installed numpy==1.7.1
     exitOnError $?
 
-    PATH=$NUPIC_INSTALL:$PATH pip install --find-links=file://$NUPIC/external/common/pip-cache --no-index --index-url=file:///dev/null --target=$NUPIC_INSTALL/lib/python${PY_VER}/site-packages --install-option="--install-scripts=$NUPIC_INSTALL/bin" --ignore-installed -r $NUPIC/external/common/requirements.txt
+    PATH=$NUPIC_INSTALL:$PATH pip install -vvv --find-links=file://$NUPIC/external/common/pip-cache --no-index --index-url=file:///dev/null --install-option="--install-scripts=$NUPIC_INSTALL/bin" --install-option="--prefix=$NUPIC_INSTALL" -r $NUPIC/external/common/requirements.txt
     exitOnError $?
     #cov-core may fail to install properly, reporting something to the effect of:
     #
