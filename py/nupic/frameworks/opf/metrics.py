b9c4ee730d5e0e4f6d65f626980f7471e2db353a
Marek Otahal <markotahal@gmail.com>
2013-09-10 22:59:31 +0200

    apply comments
    use isinstance() (handles inheritance)

--- py/nupic/frameworks/opf/metrics.py
+++ py/nupic/frameworks/opf/metrics.py
@@ -767,7 +767,7 @@ class MetricMovingMean(AggregateMetric):
 
 def evalCustomErrorMetric(expr, prediction, groundTruth, tools):
   sandbox = SafeInterpreter(writer=StringIO())
-  if(type(prediction) == dict):
+  if isinstance(prediction, dict):
     sandbox.symtable['prediction'] = tools.mostLikely(prediction)
     sandbox.symtable['EXP'] = tools.expValue(prediction)
     sandbox.symtable['probabilityDistribution'] = prediction
