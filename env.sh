9266897fb73cb7167700971d3294f4fa229a9f68
Ian Danforth <iandanforth@gmail.com>
2013-09-16 13:31:48 -0700

    Combine PY_VER and PY_VERSION

--- env.sh
+++ env.sh
@@ -25,7 +25,7 @@
 # to invocation as described in the README.
 
 # get PYTHON_VERSION early here
-PY_VER=`python -c 'import sys; print sys.version[:3]'`
+export PY_VERSION=`python -c 'import sys; print sys.version[:3]'`
 
 #orig values for paths, before env.sh has been run
 if [[ -z $_PATH ]]; then
@@ -36,11 +36,8 @@ if [[ -z $_PYTHONPATH ]]; then
 fi
 
 export PATH="$NTA/bin:$PATH"
-export PYTHONPATH="$NTA/lib/python${PY_VER}/site-packages:$PYTHONPATH"
+export PYTHONPATH="$NTA/lib/python$PY_VERSION/site-packages:$PYTHONPATH"
 export NTA_ROOTDIR="$NTA"
-
-# Numpy requires this to be set to function properly
-export PY_VERSION=`python -c 'import platform; print platform.python_version()[:3]'`
 
 # Setup the path to data for OPF experiments
 export NTA_DATA_PATH="$NTA/share/prediction/data:$NTA_DATA_PATH"
