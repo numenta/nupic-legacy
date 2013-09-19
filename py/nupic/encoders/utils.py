2d3481f5f6f647449ce6cae3599fd76449946004
Marek Otahal <markotahal@gmail.com>
2013-09-19 02:18:09 +0200

    optimize bitsToString
    .append() is recommended
    speed wars between += and .append()
    use mutable array and the fact that it;s (mostly) a SDR
    -1's are changed only in 2%

--- py/nupic/encoders/utils.py
+++ py/nupic/encoders/utils.py
@@ -23,12 +23,11 @@
 
 
 ############################################################################
-def bitsToString(array):
+def bitsToString(arr):
   """Returns a string representing a numpy array of 0's and 1's"""
-  s = ""
-  for i in xrange(len(array)):
-    if array[i] == 0:
-      s += "."
-    else:
-      s += "*"
+  s = array.array('c','.'*len(arr))
+  for i in xrange(len(arr)):
+    if arr[i] == 1:
+      s[i]='*'
   return s
+
