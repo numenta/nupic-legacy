577a15e1884517f800d0cbd646db3ce943c86323
Scott Purdy <scott@numenta.org>
2013-10-01 15:39:54 -0700

    Convert filters test to unittest format (mostly) and fix style issues.

--- tests/unit/py2/nupic/data/filters_test.py
+++ tests/unit/py2/nupic/data/filters_test.py
@@ -1,118 +1,160 @@
+#!/usr/bin/env python
+# ----------------------------------------------------------------------
+# Numenta Platform for Intelligent Computing (NuPIC)
+# Copyright (C) 2013, Numenta, Inc.  Unless you have purchased from
+# Numenta, Inc. a separate commercial license for this software code, the
+# following terms and conditions apply:
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see http://www.gnu.org/licenses.
+#
+# http://numenta.org/licenses/
+# ----------------------------------------------------------------------
+
+"""Unit tests for filters module.
+
+NOTE: This test was migrated from the old repo and could use some refactoring.
+"""
+
+from datetime import datetime
+
+import numpy
+import unittest2 as unittest
+
 from nupic.regions.RecordSensor import RecordSensor
 from nupic.data.file_record_stream import FileRecordStream
 from nupic.data.datasethelpers import findDataset
 from nupic.encoders import MultiEncoder
 from nupic.data.filters import DeltaFilter
-import numpy
-from datetime import datetime, timedelta
-
-r = RecordSensor()
-filename = findDataset("extra/qa/delta.csv")
-datasource = FileRecordStream(filename)
-r.dataSource = datasource
-n=50
-encoder = MultiEncoder({'blah': dict(fieldname="s", type='ScalarEncoder', n=n,
-                                     w=11, minval=0, maxval=100)})
-
-r.encoder = encoder
 
 
-"""
-data looks like:        should generate deltas
-  "t"   "s"               "dt"     "ds"
 
-  t     10                 X
-  t+1s  20                 1s      10
-  t+1d  50                 86399   30
-
-r t+1d+1s  60              X
-  r+1d+3s  65              2s       5
-
-"""
-
-# Test #1 -- no deltas
-# Make sure we get a reset when the gym changes
-resetOut = numpy.zeros((1,), dtype='float')
-sequenceIdOut = numpy.zeros((1,), dtype='float')
-dataOut = numpy.zeros((n,), dtype='float')
-sourceOut = numpy.zeros((1,), dtype='float')
-categoryOut = numpy.zeros((1,), dtype='float')
-
-outputs = dict(resetOut=resetOut,
-               sourceOut = sourceOut,
-               sequenceIdOut = sequenceIdOut,
-               dataOut = dataOut,
-               categoryOut = categoryOut)
-inputs = dict()
-r.verbosity=0
-
-r.compute(inputs, outputs)
-lr = r.lastRecord
-assert lr['t'] == datetime(year=2011, month=2, day=24, hour=16, minute=8, second=0)
-assert lr['s'] == 10
-assert lr['_reset'] == 1
-assert 'dt' not in lr
-assert 'ds' not in lr
+class FiltersTest(unittest.TestCase):
 
 
-r.compute(inputs, outputs)
-lr = r.lastRecord
-assert lr['t'] == datetime(year=2011, month=2, day=24, hour=16, minute=8, second=1)
-assert lr['s'] == 20
-assert lr['_reset'] == 0
+  def testDeltaFilter(self):
+    """
+    data looks like:        should generate deltas
+      "t"   "s"               "dt"     "ds"
 
-r.compute(inputs, outputs)
-lr = r.lastRecord
-assert lr['t'] == datetime(year=2011, month=2, day=25, hour=16, minute=8, second=0)
-assert lr['s'] == 50
-assert lr['_reset'] == 0
+      t     10                 X
+      t+1s  20                 1s      10
+      t+1d  50                 86399   30
 
-r.compute(inputs, outputs)
-lr = r.lastRecord
-assert lr['t'] == datetime(year=2011, month=2, day=25, hour=16, minute=8, second=1)
-assert lr['s'] == 60
-assert lr['_reset'] == 1
+    r t+1d+1s  60              X
+      r+1d+3s  65              2s       5
 
-r.compute(inputs, outputs)
-lr = r.lastRecord
-assert lr['t'] == datetime(year=2011, month=2, day=25, hour=16, minute=8, second=3)
-assert lr['s'] == 65
-assert lr['_reset'] == 0
+    """
+    r = RecordSensor()
+    filename = findDataset("extra/qa/delta.csv")
+    datasource = FileRecordStream(filename)
+    r.dataSource = datasource
+    n = 50
+    encoder = MultiEncoder({'blah': dict(fieldname="s", type='ScalarEncoder',
+                                         n=n, w=11, minval=0, maxval=100)})
+
+    r.encoder = encoder
+
+    # Test #1 -- no deltas
+    # Make sure we get a reset when the gym changes
+    resetOut = numpy.zeros((1,), dtype='float')
+    sequenceIdOut = numpy.zeros((1,), dtype='float')
+    dataOut = numpy.zeros((n,), dtype='float')
+    sourceOut = numpy.zeros((1,), dtype='float')
+    categoryOut = numpy.zeros((1,), dtype='float')
+
+    outputs = dict(resetOut=resetOut,
+                   sourceOut = sourceOut,
+                   sequenceIdOut = sequenceIdOut,
+                   dataOut = dataOut,
+                   categoryOut = categoryOut)
+    inputs = dict()
+    r.verbosity=0
+
+    r.compute(inputs, outputs)
+    lr = r.lastRecord
+    self.assertEqual(lr['t'], datetime(year=2011, month=2, day=24, hour=16,
+                                       minute=8, second=0))
+    self.assertEqual(lr['s'], 10)
+    self.assertEqual(lr['_reset'], 1)
+    self.assertTrue('dt' not in lr)
+    self.assertTrue('ds' not in lr)
+
+    r.compute(inputs, outputs)
+    lr = r.lastRecord
+    self.assertEqual(lr['t'], datetime(year=2011, month=2, day=24, hour=16,
+                                       minute=8, second=1))
+    self.assertEqual(lr['s'], 20)
+    self.assertEqual(lr['_reset'], 0)
+
+    r.compute(inputs, outputs)
+    lr = r.lastRecord
+    self.assertEqual(lr['t'], datetime(year=2011, month=2, day=25, hour=16,
+                                       minute=8, second=0))
+    self.assertEqual(lr['s'], 50)
+    self.assertEqual(lr['_reset'], 0)
+
+    r.compute(inputs, outputs)
+    lr = r.lastRecord
+    self.assertEqual(lr['t'], datetime(year=2011, month=2, day=25, hour=16,
+                                       minute=8, second=1))
+    self.assertEqual(lr['s'], 60)
+    self.assertEqual(lr['_reset'], 1)
+
+    r.compute(inputs, outputs)
+    lr = r.lastRecord
+    self.assertEqual(lr['t'], datetime(year=2011, month=2, day=25, hour=16,
+                                       minute=8, second=3))
+    self.assertEqual(lr['s'], 65)
+    self.assertEqual(lr['_reset'], 0)
+
+    # Add filters
+
+    r.preEncodingFilters = [DeltaFilter("s", "ds"), DeltaFilter("t", "dt")]
+    r.rewind()
+
+    # skip first record, which has a reset
+
+    r.compute(inputs, outputs)
+    lr = r.lastRecord
+    self.assertEqual(lr['t'], datetime(year=2011, month=2, day=24, hour=16,
+                                       minute=8, second=1))
+    self.assertEqual(lr['s'], 20)
+    self.assertEqual(lr['_reset'], 1)  # this record should have a reset since
+                                       # it is first of a sequence
+    self.assertEqual(lr['dt'], 1)
+    self.assertEqual(lr['ds'], 10)
+
+    r.compute(inputs, outputs)
+    lr = r.lastRecord
+    self.assertEqual(lr['t'], datetime(year=2011, month=2, day=25, hour=16,
+                                       minute=8, second=0))
+    self.assertEqual(lr['s'], 50)
+    self.assertEqual(lr['_reset'], 0)
+    self.assertEqual(lr['dt'], 3600 * 24 - 1)
+    self.assertEqual(lr['ds'], 30)
+
+    # next reset record is skipped
+
+    r.compute(inputs, outputs)
+    lr = r.lastRecord
+    self.assertEqual(lr['t'], datetime(year=2011, month=2, day=25, hour=16,
+                                       minute=8, second=3))
+    self.assertEqual(lr['s'], 65)
+    self.assertEqual(lr['_reset'], 1)
+    self.assertEqual(lr['dt'], 2)
+    self.assertEqual(lr['ds'], 5)
 
 
-# Add filters
 
-r.preEncodingFilters = [DeltaFilter("s", "ds"), DeltaFilter("t", "dt")]
-r.rewind()
-
-# skip first record, which has a reset
-
-r.compute(inputs, outputs)
-lr = r.lastRecord
-assert lr['t'] == datetime(year=2011, month=2, day=24, hour=16, minute=8, second=1)
-assert lr['s'] == 20
-assert lr['_reset'] == 1  # this record should have a reset since it is first of a sequence
-assert lr['dt'] == 1
-assert lr['ds'] == 10
-
-
-r.compute(inputs, outputs)
-lr = r.lastRecord
-assert lr['t'] == datetime(year=2011, month=2, day=25, hour=16, minute=8, second=0)
-assert lr['s'] == 50
-assert lr['_reset'] == 0
-assert lr['dt'] == (3600*24 - 1)
-assert lr['ds'] == 30
-
-# next reset record is skipped
-
-
-r.compute(inputs, outputs)
-lr = r.lastRecord
-assert lr['t'] == datetime(year=2011, month=2, day=25, hour=16, minute=8, second=3)
-assert lr['s'] == 65
-assert lr['_reset'] == 1
-assert lr['dt'] == 2
-assert lr['ds'] == 5
-
-print "All delta tests passed"
+if __name__ == "__main__":
+  unittest.main()
