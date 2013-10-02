d2df6c417fd76267b587eba53e0af38c2bd4bd40
Scott Purdy <scott@numenta.org>
2013-10-02 14:26:28 -0700

    Add back in _createPredictionLogger override in DummyModelRunner to get the swarming test working again.

--- py/nupic/swarming/DummyModelRunner.py
+++ py/nupic/swarming/DummyModelRunner.py
@@ -592,6 +592,20 @@ class OPFDummyModelRunner(OPFModelRunner):
     return ModelFactory.create(modelDescription)
 
 
+  def _createPredictionLogger(self):
+    """
+    Creates the model's PredictionLogger object, which is an interface to write
+    model results to a permanent storage location
+    """
+
+    class DummyLogger:
+      def writeRecord(self, record): pass
+      def writeRecords(self, records, progressCB): pass
+      def close(self): pass
+
+    self._predictionLogger = DummyLogger()
+
+
   def __shouldSysExit(self, iteration):
     """
     Checks to see if the model should exit based on the exitAfter dummy
