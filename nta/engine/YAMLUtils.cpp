75d5ce80d04a33a7ddb3079ea97ae70bbf00504c
Marek Otahal <markotahal@gmail.com>
2013-10-15 03:44:24 +0200

    Revert "enable check (was blocked by nupic 1.x Nodes)"
    This reverts commit 6fcb8926d85b71267175decaf966e90d3f74b191.
    causing some problems now, investigate later

--- nta/engine/YAMLUtils.cpp
+++ nta/engine/YAMLUtils.cpp
@@ -311,10 +311,11 @@ ValueMap toValueMap(const char* yamlstring,
       ParameterSpec & ps = item.second;
       if (ps.defaultValue != "")
       {
-        if (ps.accessMode != ParameterSpec::CreateAccess)
-        {
-          NTA_THROW << "Default value for non-create parameter: " << item.first;
-        }
+        // TODO: This check should be uncommented after dropping NuPIC 1.x nodes (which don't comply)
+        // if (ps.accessMode != ParameterSpec::CreateAccess)
+        // {
+        //   NTA_THROW << "Default value for non-create parameter: " << item.first;
+        // }
         
         try {
 #ifdef YAMLDEBUG
