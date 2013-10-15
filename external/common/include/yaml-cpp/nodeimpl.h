c247bd2101aefb575afdcfdec109359e29f0bcda
Marek Otahal <markotahal@gmail.com>
2013-10-15 02:08:34 +0200

    update yaml-cpp headers to 0.3
    libyaml.a has to be rebuilt to this version too

--- external/common/include/yaml-cpp/nodeimpl.h
+++ external/common/include/yaml-cpp/nodeimpl.h
@@ -1,42 +1,43 @@
-#pragma once
-
 #ifndef NODEIMPL_H_62B23520_7C8E_11DE_8A39_0800200C9A66
 #define NODEIMPL_H_62B23520_7C8E_11DE_8A39_0800200C9A66
 
+#if defined(_MSC_VER) || (defined(__GNUC__) && (__GNUC__ == 3 && __GNUC_MINOR__ >= 4) || (__GNUC__ >= 4)) // GCC supports "pragma once" correctly since 3.4
+#pragma once
+#endif
 
-#include "nodeutil.h"
+
+#include "yaml-cpp/nodeutil.h"
+#include <cassert>
 
 namespace YAML
 {
 	// implementation of templated things
 	template <typename T>
-	inline const T Node::Read() const {
+	inline const T Node::to() const {
 		T value;
 		*this >> value;
 		return value;
 	}
-	
-	template <typename T>
-	Node::operator T() const {
-		return Read<T>();
-	}
 
 	template <typename T>
-	inline void operator >> (const Node& node, T& value) {
+	inline typename enable_if<is_scalar_convertible<T> >::type operator >> (const Node& node, T& value) {
 		if(!ConvertScalar(node, value))
 			throw InvalidScalar(node.m_mark);
 	}
 	
 	template <typename T>
 	inline const Node *Node::FindValue(const T& key) const {
-		switch(GetType()) {
-			case CT_MAP:
-				return FindValueForKey(key);
-			case CT_SEQUENCE:
+		switch(m_type) {
+			case NodeType::Null:
+			case NodeType::Scalar:
+				throw BadDereference();
+			case NodeType::Sequence:
 				return FindFromNodeAtIndex(*this, key);
-			default:
-				return 0;
+			case NodeType::Map:
+				return FindValueForKey(key);
 		}
+		assert(false);
+		throw BadDereference();
 	}
 	
 	template <typename T>
@@ -54,14 +55,9 @@ namespace YAML
 	
 	template <typename T>
 	inline const Node& Node::GetValue(const T& key) const {
-		if(!m_pContent)
-			throw BadDereference();
-		
-		const Node *pValue = FindValue(key);
-		if(!pValue)
-			throw MakeTypedKeyNotFound(m_mark, key);
-
-		return *pValue;
+		if(const Node *pValue = FindValue(key))
+			return *pValue;
+		throw MakeTypedKeyNotFound(m_mark, key);
 	}
 	
 	template <typename T>
@@ -72,47 +68,18 @@ namespace YAML
 	inline const Node *Node::FindValue(const char *key) const {
 		return FindValue(std::string(key));
 	}
+
+	inline const Node *Node::FindValue(char *key) const {
+		return FindValue(std::string(key));
+	}
 	
 	inline const Node& Node::operator [] (const char *key) const {
 		return GetValue(std::string(key));
 	}
 
-	template <typename T>
-	inline bool operator == (const T& value, const Node& node) {
-		return value == node.operator T();
+	inline const Node& Node::operator [] (char *key) const {
+		return GetValue(std::string(key));
 	}
-	
-	template <typename T>
-	inline bool operator == (const Node& node, const T& value) {
-		return value == node.operator T();
-	}
-	
-	template <typename T>
-	inline bool operator != (const T& value, const Node& node) {
-		return value != node.operator T();
-	}
-	
-	template <typename T>
-	inline bool operator != (const Node& node, const T& value) {
-		return value != node.operator T();
-	}
-
-	inline bool operator == (const char *value, const Node& node) {
-		return std::string(value) == node;
-	}
-	
-	inline bool operator == (const Node& node, const char *value) {
-		return std::string(value) == node;
-	}
-	
-	inline bool operator != (const char *value, const Node& node) {
-		return std::string(value) != node;
-	}
-	
-	inline bool operator != (const Node& node, const char *value) {
-		return std::string(value) != node;
-	}
-	
 }
 
 #endif // NODEIMPL_H_62B23520_7C8E_11DE_8A39_0800200C9A66
--- external/common/include/yaml-cpp/stlemitter.h
+++ external/common/include/yaml-cpp/stlemitter.h
@@ -1,37 +1,44 @@
-#pragma once
-
 #ifndef STLEMITTER_H_62B23520_7C8E_11DE_8A39_0800200C9A66
 #define STLEMITTER_H_62B23520_7C8E_11DE_8A39_0800200C9A66
+
+#if defined(_MSC_VER) || (defined(__GNUC__) && (__GNUC__ == 3 && __GNUC_MINOR__ >= 4) || (__GNUC__ >= 4)) // GCC supports "pragma once" correctly since 3.4
+#pragma once
+#endif
 
 
 #include <vector>
 #include <list>
+#include <set>
 #include <map>
 
 namespace YAML
 {
-	template <typename T>
-	inline Emitter& operator << (Emitter& emitter, const std::vector <T>& v) {
-		typedef typename std::vector <T> vec;
+	template<typename Seq>
+	inline Emitter& EmitSeq(Emitter& emitter, const Seq& seq) {
 		emitter << BeginSeq;
-		for(typename vec::const_iterator it=v.begin();it!=v.end();++it)
-			emitter << *it;
-		emitter << EndSeq;
-		return emitter;
-	}	
-
-	template <typename T>
-	inline Emitter& operator << (Emitter& emitter, const std::list <T>& v) {
-		typedef typename std::list <T> list;
-		emitter << BeginSeq;
-		for(typename list::const_iterator it=v.begin();it!=v.end();++it)
+		for(typename Seq::const_iterator it=seq.begin();it!=seq.end();++it)
 			emitter << *it;
 		emitter << EndSeq;
 		return emitter;
 	}
 	
+	template<typename T>
+	inline Emitter& operator << (Emitter& emitter, const std::vector<T>& v) {
+		return EmitSeq(emitter, v);
+	}	
+
+	template<typename T>
+	inline Emitter& operator << (Emitter& emitter, const std::list<T>& v) {
+		return EmitSeq(emitter, v);
+	}
+
+	template<typename T>
+	inline Emitter& operator << (Emitter& emitter, const std::set<T>& v) {
+		return EmitSeq(emitter, v);
+	}
+	
 	template <typename K, typename V>
-	inline Emitter& operator << (Emitter& emitter, const std::map <K, V>& m) {
+	inline Emitter& operator << (Emitter& emitter, const std::map<K, V>& m) {
 		typedef typename std::map <K, V> map;
 		emitter << BeginMap;
 		for(typename map::const_iterator it=m.begin();it!=m.end();++it)
