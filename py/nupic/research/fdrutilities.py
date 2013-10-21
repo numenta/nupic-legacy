292f9911432914f68d3f5b347e44d7dc4d1a88ca
Marek Otahal <markotahal@gmail.com>
2013-10-21 22:43:28 +0200

    exit early in sameSegment test

--- py/nupic/research/fdrutilities.py
+++ py/nupic/research/fdrutilities.py
@@ -475,11 +475,11 @@ def sameSegment(seg1, seg2):
   # check that they are within 0.001.
   for field in [1, 2, 3, 4, 5, 6]:
     if abs(seg1[0][field] - seg2[0][field]) > 0.001:
-      result = False
+      return False
 
   # Compare number of synapses
   if len(seg1[1:]) != len(seg2[1:]):
-    result = False
+    return False
 
   # Now compare synapses, ignoring order of synapses
   for syn in seg2[1:]:
