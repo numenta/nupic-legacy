234be69b7979e6bd76f6dda0ee2952943cb8ad3a
Marek Otahal <markotahal@gmail.com>
2013-11-14 05:40:01 +0100

    use boost::math for fact(), binomial_coeficient()

--- nta/math/functions.hpp
+++ nta/math/functions.hpp
@@ -35,10 +35,12 @@
 #include <boost/math/special_functions/digamma.hpp>
 #include <boost/math/special_functions/beta.hpp>
 #include <boost/math/special_functions/erf.hpp>
+#include <boost/math/special_functions/factorials.hpp>
+#include <boost/math/special_functions/binomial.hpp>
+
 
 namespace nta {
 
-  // TODO: replace other functions by boost/math
 
   static const double pi  =  3.14159265358979311600e+00;
 
@@ -73,54 +75,19 @@ namespace nta {
   //--------------------------------------------------------------------------------
   double fact(unsigned long n)
   {
-    static double a[171];
-    static bool init = true;
-    
-    if (init) {
-      init = false;
-      a[0] = 1.0;
-      for (size_t i = 1; i != 171; ++i)
-	a[i] = i * a[i-1];
-    }
-    
-    if (n < 171)
-      return a[n];
-    else
-      return exp(lgamma(n+1.0));
-  }
-
-  //--------------------------------------------------------------------------------
-  double lfact(unsigned long n)
-  {
-    static double a[2000];
-    static bool init = true;
-    
-    if (init) {
-      for (size_t i = 0; i != 2000; ++i)
-	a[i] = lgamma(i+1.0);
-    }
-
-    if (n < 2000)
-      return a[n];
-    else
-      return lgamma(n+1.0);
+    return boost::math::factorial<double>(n);
   }
 
   //--------------------------------------------------------------------------------
   double binomial(unsigned long n, unsigned long k)
   {
     {
-      NTA_ASSERT(k <= n)
-	<< "binomial: Wrong arguments: n= " << n << " k= " << k;
+      NTA_ASSERT(k <= n) << "binomial: Wrong arguments: n= " << n << " k= " << k;
     }
-
-    if (n < 171)
-      return floor(0.5 + fact(n) / (fact(k) * fact(n-k)));
-    else
-      return floor(0.5 + exp(lfact(n) - lfact(k) - lfact(n-k)));
+    return boost::math::binomial_coefficient<double>(n, k);
   }
 
   //--------------------------------------------------------------------------------
-};
+}
 
 #endif //NTA_MATH_FUNCTIONS_HPP
