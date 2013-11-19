363d321da3097759766feed39df87c14e301a7d9
Marek Otahal <markotahal@gmail.com>
2013-11-19 14:27:56 +0100

    Utility: more tests

--- tests/unit/py2/nupic/encoders/utility_test.py
+++ tests/unit/py2/nupic/encoders/utility_test.py
@@ -32,14 +32,14 @@ class UtilityEncoderTest(unittest.TestCase):
   """testing Utility encoder"""
 
   def setUp(self):
-    self.data = [1,2,3]
+    self.data = [-1,0,10]
 
     # encoder for score: 0..100, fine-grained to 0.5
     self.scoreEnc = ScalarEncoder(3, 0, 100, resolution=0.5, name='score')
 
     # encoder for the input (data) part
-    elem = ScalarEncoder(1,0,3,resolution=1)
-    self.dataEnc = VectorEncoder(len(self.data), elem, typeCastFn=int, name='data')
+    elem = ScalarEncoder(5,-5,50,resolution=1)
+    self.dataEnc = VectorEncoder(len(self.data), elem, typeCastFn=float, name='data')
 
     # utility encoder
     def sumAll(list):
@@ -47,12 +47,36 @@ class UtilityEncoderTest(unittest.TestCase):
 
     self.fn = sumAll
 
-    self.utilityEnc = None
+    self.utilityEnc = UtilityEncoder(self.dataEnc, self.scoreEnc, feval=self.fn, name='sum-er')
 
   def testInitialization(self):
     """creating a utility encoder"""
     util = UtilityEncoder(self.dataEnc, self.scoreEnc, feval=self.fn, name='starter')
     assert True==isinstance(util, UtilityEncoder)
+
+  def testEncoding(self):
+    """check encoding.."""
+    sc = self.fn(self.data) # expected
+    score = self.utilityEnc.getScoreIN(self.data)
+    assert sc == score
+
+    enc = self.utilityEnc.encode(self.data)
+    print "encoded=", enc
+
+  def testDecoding(self):
+    """decoding.."""
+    enc = self.utilityEnc.encode(self.data)
+    sc = self.fn(self.data) # expected
+    score = self.utilityEnc.getScoreOUT(enc) #real
+    print "score", score
+    assert sc == score[0]
+
+    dec = self.utilityEnc.decode(enc)
+    print "decoded=", dec
+
+    res = self.dataEnc.getData(dec)
+    assert res==self.data
+
 
 ##########################################################
 if __name__ == '__main__':
