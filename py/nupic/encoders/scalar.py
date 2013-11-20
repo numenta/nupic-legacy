2c63a0cd87c0b4dcbfd8b2b68233c291a0515ee0
Marek Otahal <markotahal@gmail.com>
2013-11-20 17:50:42 +0100

    fix set min w >= 21
    as written in description

--- py/nupic/encoders/scalar.py
+++ py/nupic/encoders/scalar.py
@@ -275,7 +275,7 @@ class ScalarEncoder(Encoder):
   def _checkReasonableSettings(self):
     """(helper function) check if the settings are reasonable for SP to work"""
     # checks for likely mistakes in encoder settings
-    if self.w <= 21:
+    if self.w < 21:
       raise RuntimeError("Number of bits in the SDR (%d) must be greater than 2, and recommended >= 21 (use forced=True to override)"
                          % self.w)
 
