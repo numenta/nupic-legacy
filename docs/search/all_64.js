811280d60ab6733e4422ac124807039ab20742cb
Matthew Taylor <matt@numenta.org>
2013-12-11 00:00:22 +0000

    NuPIC Doxygen automated doc build.

--- docs/search/all_64.js
+++ docs/search/all_64.js
@@ -2,5 +2,7 @@ var searchData=
 [
   ['debugprint',['debugPrint',['../classnupic_1_1research_1_1_t_p_1_1_segment.html#ab61fcbf5e56a3fbe2b9abb1983856d62',1,'nupic::research::TP::Segment']]],
   ['dopooling',['doPooling',['../classnupic_1_1research_1_1_t_p_1_1_t_p.html#a8b92690cd04e1d0a03cb384a85e7c9e3',1,'nupic::research::TP::TP']]],
-  ['dutycycle',['dutyCycle',['../classnupic_1_1research_1_1_t_p_1_1_segment.html#a837551aa80adb16d9e318b8ac43a530f',1,'nupic::research::TP::Segment']]]
+  ['dutycycle',['dutyCycle',['../classnupic_1_1research_1_1_t_p_1_1_segment.html#a837551aa80adb16d9e318b8ac43a530f',1,'nupic::research::TP::Segment']]],
+  ['dutycyclealphas',['dutyCycleAlphas',['../classnupic_1_1research_1_1_t_p_1_1_segment.html#a2534da83b4f72e45c16e522bab6fd7bf',1,'nupic::research::TP::Segment']]],
+  ['dutycycletiers',['dutyCycleTiers',['../classnupic_1_1research_1_1_t_p_1_1_segment.html#a1414b23d4e5875c342ae6e13e3cc533f',1,'nupic::research::TP::Segment']]]
 ];
--- docs/search/variables_64.js
+++ docs/search/variables_64.js
@@ -1,4 +1,6 @@
 var searchData=
 [
-  ['dopooling',['doPooling',['../classnupic_1_1research_1_1_t_p_1_1_t_p.html#a8b92690cd04e1d0a03cb384a85e7c9e3',1,'nupic::research::TP::TP']]]
+  ['dopooling',['doPooling',['../classnupic_1_1research_1_1_t_p_1_1_t_p.html#a8b92690cd04e1d0a03cb384a85e7c9e3',1,'nupic::research::TP::TP']]],
+  ['dutycyclealphas',['dutyCycleAlphas',['../classnupic_1_1research_1_1_t_p_1_1_segment.html#a2534da83b4f72e45c16e522bab6fd7bf',1,'nupic::research::TP::Segment']]],
+  ['dutycycletiers',['dutyCycleTiers',['../classnupic_1_1research_1_1_t_p_1_1_segment.html#a1414b23d4e5875c342ae6e13e3cc533f',1,'nupic::research::TP::Segment']]]
 ];
