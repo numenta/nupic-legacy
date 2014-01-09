59c100e4552c7a51a1606b3a3dfe555614636484
Daniel Stanton <geometry180@gmail.com>
2014-01-09 10:34:32 -0600

    updated copyright years in touched files
    Since it's a new year, I incremented the year for the files I'll be
    proposing changes to.

--- nta/algorithms/svm.cpp
+++ nta/algorithms/svm.cpp
@@ -1,7 +1,7 @@
 /*
  * ---------------------------------------------------------------------
  * Numenta Platform for Intelligent Computing (NuPIC)
- * Copyright (C) 2013, Numenta, Inc.  Unless you have purchased from
+ * Copyright (C) 2014, Numenta, Inc.  Unless you have purchased from
  * Numenta, Inc. a separate commercial license for this software code, the
  * following terms and conditions apply:
  *
