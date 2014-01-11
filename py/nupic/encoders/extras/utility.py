6c75594f817cb31a801ffc720999c04a52f284be
Marek Otahal <markotahal@gmail.com>
2014-01-11 23:55:38 +0100

    Utility: add license + doc

--- py/nupic/encoders/extras/utility.py
+++ py/nupic/encoders/extras/utility.py
@@ -1,3 +1,34 @@
+# ----------------------------------------------------------------------
+# Numenta Platform for Intelligent Computing (NuPIC)
+# Copyright (C) 2013, Numenta, Inc.  Unless you have an agreement
+# with Numenta, Inc., for a separate license for this software code, the
+# following terms and conditions apply:
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see http://www.gnu.org/licenses.
+#
+# http://numenta.org/licenses/
+# ----------------------------------------------------------------------
+
+"""
+This class implements a UtilityEncoder. It's a special kind of an encoder that applies a (user-defined) function 
+to the original input and attaches that to the output. This can be used eg. for some pre-processing of the data in the CLA, for example when modeling agents. 
+
+The visualisation of the principle: 
+
+Input: 'XAXAXA'
+fn="drop all 'A's"
+Output: encoded('XAXAXA''XXX')
+"""
 
 from nupic.encoders.multi import MultiEncoder
 from nupic.encoders.base import Encoder
