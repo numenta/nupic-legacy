51fd1ef342797a2fa8ff3ae7bd31c9bbba1c5699
Christopher L. Simons <christopherleesimons@gmail.com>
2014-01-13 22:15:33 -0500

    Renamed NTA_CONF_DIR variable to NTA_CONF_PATH per #554.

--- examples/opf/bin/OpfRunExperiment.py
+++ examples/opf/bin/OpfRunExperiment.py
@@ -35,7 +35,7 @@ import nupic.support
 def main():
   """Run according to options in sys.argv"""
   # Init the NuPic logging configuration from the nupic-logging.conf configuration
-  # file. This is found either in the NTA_CONF_DIR directory (if defined) or
+  # file. This is found either in the NTA_CONF_PATH directory (if defined) or
   # in the 'conf' subdirectory of the NuPic install location.
   nupic.support.initLogging(verbose=True)
 
