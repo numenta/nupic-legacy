8f4ebc407c6d7f497398f77a343fa57813a6303f
Chetan Surpur <chetan51@gmail.com>
2014-02-04 14:08:13 -0800

    Converted to unittest format

--- tests/unit/py2/nupic/algorithms/knn_classifier_test/knn_classifier_test.py
+++ tests/unit/py2/nupic/algorithms/knn_classifier_test/knn_classifier_test.py
@@ -20,16 +20,12 @@
 # http://numenta.org/licenses/
 # ----------------------------------------------------------------------
 
-"""
-## @file
-This file contains tests for the k Nearest Neighbor classifier.
-"""
-
 import numpy
 import time
 import cPickle
 from nupic.regions.PyRegion import RealNumpyDType
 from nupic.algorithms.KNNClassifier import KNNClassifier
+import unittest2 as unittest
 
 import pca_knn_data
 
@@ -134,173 +130,186 @@ def getNumTestPatterns(short=0):
   return numPatterns, numClasses
 
 
-def testKNNClassifier(short = 0):
-  """ Test the KNN classifier in this module. short can be:
-      0 (short), 1 (medium), or 2 (long)
-  """
 
-  failures = ""
-  if short != 2:
-    numpy.random.seed(42)
-  else:
-    seed_value = int(time.time())
-    # seed_value = 1276437656
-    #seed_value = 1277136651
-    numpy.random.seed(seed_value)
-    print 'Seed used: %d' %seed_value
-    f = open('seedval', 'a')
-    f.write(str(seed_value))
-    f.write('\n')
-    f.close()
-  failures += testKMoreThanOne()
+class KNNClassifierTest(unittest.TestCase):
+  """Tests for k Nearest Neighbor classifier"""
 
+  def _testKNNClassifier(self, short = 0):
+    """ Test the KNN classifier in this module. short can be:
+        0 (short), 1 (medium), or 2 (long)
+    """
 
-  print "\nTesting KNN Classifier on dense patterns"
-  numPatterns, numClasses = getNumTestPatterns(short)
-  patterns = numpy.random.rand(numPatterns,100)
-  patternDict = dict()
-
-  # Assume there are no repeated patterns -- if there are, then
-  # numpy.random would be completely broken.
-  for i in xrange(numPatterns):
-    randCategory = numpy.random.randint(0,numClasses-1)
-    patternDict[i] = dict()
-    patternDict[i]['pattern'] = patterns[i]
-    patternDict[i]['category'] = randCategory
-
-  # for i in patterns:
-  #   iString = str(i.tolist())
-  #   if not patternDict.has_key(iString):
-  #     randCategory = numpy.random.randint(0,numClasses-1)
-  #     patternDict[iString] = dict()
-  #     patternDict[iString]['pattern'] = i
-  #     patternDict[iString]['category'] = randCategory
+    failures = ""
+    if short != 2:
+      numpy.random.seed(42)
+    else:
+      seed_value = int(time.time())
+      # seed_value = 1276437656
+      #seed_value = 1277136651
+      numpy.random.seed(seed_value)
+      print 'Seed used: %d' %seed_value
+      f = open('seedval', 'a')
+      f.write(str(seed_value))
+      f.write('\n')
+      f.close()
+    failures += testKMoreThanOne()
 
 
-  print "\nTesting KNN Classifier with L2 norm"
+    print "\nTesting KNN Classifier on dense patterns"
+    numPatterns, numClasses = getNumTestPatterns(short)
+    patterns = numpy.random.rand(numPatterns,100)
+    patternDict = dict()
 
-  knn = KNNClassifier(k=1)
-  failures += testClassifier(knn, patternDict, "KNN Classifier with L2 norm test")
-
-
-  return
-  print "\nTesting KNN Classifier with L1 norm"
-
-  knnL1 = KNNClassifier(k=1, distanceNorm=1.0)
-  failures += testClassifier(knnL1, patternDict, "KNN Classifier with L1 norm test")
-
-
-  numPatterns, numClasses = getNumTestPatterns(short)
-  patterns = (numpy.random.rand(numPatterns,25) > 0.7).astype(RealNumpyDType)
-  patternDict = dict()
-
-  for i in patterns:
-    iString = str(i.tolist())
-    if not patternDict.has_key(iString):
+    # Assume there are no repeated patterns -- if there are, then
+    # numpy.random would be completely broken.
+    for i in xrange(numPatterns):
       randCategory = numpy.random.randint(0,numClasses-1)
-      patternDict[iString] = dict()
-      patternDict[iString]['pattern'] = i
-      patternDict[iString]['category'] = randCategory
+      patternDict[i] = dict()
+      patternDict[i]['pattern'] = patterns[i]
+      patternDict[i]['category'] = randCategory
+
+    # for i in patterns:
+    #   iString = str(i.tolist())
+    #   if not patternDict.has_key(iString):
+    #     randCategory = numpy.random.randint(0,numClasses-1)
+    #     patternDict[iString] = dict()
+    #     patternDict[iString]['pattern'] = i
+    #     patternDict[iString]['category'] = randCategory
 
 
-  print "\nTesting KNN on sparse patterns"
+    print "\nTesting KNN Classifier with L2 norm"
 
-  knnDense = KNNClassifier(k=1)
-  failures += testClassifier(knnDense, patternDict, "KNN Classifier on sparse pattern test")
-
-  if len(failures) != 0:
-    raise Exception("Tests failed: \n" + failures)
-  f = open('seedval', 'a')
-  f.write('Pass\n')
-  f.close()
+    knn = KNNClassifier(k=1)
+    failures += testClassifier(knn, patternDict, "KNN Classifier with L2 norm test")
 
 
-def test_pca_knn(short = 0):
+    return
+    print "\nTesting KNN Classifier with L1 norm"
 
-  print '\nTesting PCA/k-NN classifier'
-  print 'Mode=', short,
-
-  numDims = 10
-  numClasses = 10
-  k = 10
-  numPatternsPerClass = 100
-  numPatterns = int(.9 * numClasses * numPatternsPerClass)
-  numTests = numClasses * numPatternsPerClass - numPatterns
-  numSVDSamples = int(.1 * numPatterns)
-  keep = 1
-
-  train_data, train_class, test_data, test_class = \
-      pca_knn_data.generate(numDims, numClasses, k, numPatternsPerClass,
-                            numPatterns, numTests, numSVDSamples, keep)
-
-  pca_knn = KNNClassifier(k=k,numSVDSamples=numSVDSamples,
-                          numSVDDims=keep)
-
-  knn = KNNClassifier(k=k)
+    knnL1 = KNNClassifier(k=1, distanceNorm=1.0)
+    failures += testClassifier(knnL1, patternDict, "KNN Classifier with L1 norm test")
 
 
-  print 'Training PCA k-NN'
+    numPatterns, numClasses = getNumTestPatterns(short)
+    patterns = (numpy.random.rand(numPatterns,25) > 0.7).astype(RealNumpyDType)
+    patternDict = dict()
 
-  for i in range(numPatterns):
-    knn.learn(train_data[i], train_class[i])
-    pca_knn.learn(train_data[i], train_class[i])
+    for i in patterns:
+      iString = str(i.tolist())
+      if not patternDict.has_key(iString):
+        randCategory = numpy.random.randint(0,numClasses-1)
+        patternDict[iString] = dict()
+        patternDict[iString]['pattern'] = i
+        patternDict[iString]['category'] = randCategory
 
 
-  print 'Testing PCA k-NN'
+    print "\nTesting KNN on sparse patterns"
 
-  numWinnerFailures = 0
-  numInferenceFailures = 0
-  numDistFailures = 0
-  numAbsErrors = 0
+    knnDense = KNNClassifier(k=1)
+    failures += testClassifier(knnDense, patternDict, "KNN Classifier on sparse pattern test")
 
-  for i in range(numTests):
+    self.assertEqual(len(failures), 0,
+      "Tests failed: \n" + failures)
 
-    winner, inference, dist, categoryDist = knn.infer(test_data[i])
-    pca_winner, pca_inference, pca_dist, pca_categoryDist \
-      = pca_knn.infer(test_data[i])
+    f = open('seedval', 'a')
+    f.write('Pass\n')
+    f.close()
 
-    if 0:
-      print '>>', test_class[i], winner, pca_winner,
+
+  def _testPCAKNN(self, short = 0):
+
+    print '\nTesting PCA/k-NN classifier'
+    print 'Mode=', short,
+
+    numDims = 10
+    numClasses = 10
+    k = 10
+    numPatternsPerClass = 100
+    numPatterns = int(.9 * numClasses * numPatternsPerClass)
+    numTests = numClasses * numPatternsPerClass - numPatterns
+    numSVDSamples = int(.1 * numPatterns)
+    keep = 1
+
+    train_data, train_class, test_data, test_class = \
+        pca_knn_data.generate(numDims, numClasses, k, numPatternsPerClass,
+                              numPatterns, numTests, numSVDSamples, keep)
+
+    pca_knn = KNNClassifier(k=k,numSVDSamples=numSVDSamples,
+                            numSVDDims=keep)
+
+    knn = KNNClassifier(k=k)
+
+
+    print 'Training PCA k-NN'
+
+    for i in range(numPatterns):
+      knn.learn(train_data[i], train_class[i])
+      pca_knn.learn(train_data[i], train_class[i])
+
+
+    print 'Testing PCA k-NN'
+
+    numWinnerFailures = 0
+    numInferenceFailures = 0
+    numDistFailures = 0
+    numAbsErrors = 0
+
+    for i in range(numTests):
+
+      winner, inference, dist, categoryDist = knn.infer(test_data[i])
+      pca_winner, pca_inference, pca_dist, pca_categoryDist \
+        = pca_knn.infer(test_data[i])
+
+      if 0:
+        print '>>', test_class[i], winner, pca_winner,
+        if winner != test_class[i]:
+          print 'k-NN wrong',
+        if pca_winner != test_class[i]:
+          print 'PCA/k-NN wrong',
+        print
+
       if winner != test_class[i]:
-        print 'k-NN wrong',
-      if pca_winner != test_class[i]:
-        print 'PCA/k-NN wrong',
-      print
+        numAbsErrors += 1
 
-    if winner != test_class[i]:
-      numAbsErrors += 1
+      if pca_winner != winner:
+        numWinnerFailures += 1
 
-    if pca_winner != winner:
-      numWinnerFailures += 1
+      if (numpy.abs(pca_inference - inference) > 1e-4).any():
+        numInferenceFailures += 1
 
-    if (numpy.abs(pca_inference - inference) > 1e-4).any():
-      numInferenceFailures += 1
+      if (numpy.abs(pca_dist - dist) > 1e-4).any():
+        numDistFailures += 1
 
-    if (numpy.abs(pca_dist - dist) > 1e-4).any():
-      numDistFailures += 1
+    s0 = 100*float(numTests - numAbsErrors) / float(numTests)
+    s1 = 100*float(numTests - numWinnerFailures) / float(numTests)
+    s2 = 100*float(numTests - numInferenceFailures) / float(numTests)
+    s3 = 100*float(numTests - numDistFailures) / float(numTests)
 
-  s0 = 100*float(numTests - numAbsErrors) / float(numTests)
-  s1 = 100*float(numTests - numWinnerFailures) / float(numTests)
-  s2 = 100*float(numTests - numInferenceFailures) / float(numTests)
-  s3 = 100*float(numTests - numDistFailures) / float(numTests)
+    print 'PCA/k-NN success rate=', s0, '%'
+    print 'Winner success=', s1, '%'
+    print 'Inference success=', s2, '%'
+    print 'Distance success=', s3, '%'
 
-  print 'PCA/k-NN success rate=', s0, '%'
-  print 'Winner success=', s1, '%'
-  print 'Inference success=', s2, '%'
-  print 'Distance success=', s3, '%'
-
-  if s1 != 100.0:
-    raise Exception("PCA/k-NN test failed")
-  else:
-    print 'PCA/-kNN test passed'
+    self.assertEqual(s1, 100.0,
+      "PCA/k-NN test failed")
 
 
+  def testKNNClassifierShort(self):
+    self._testKNNClassifier(0)
 
-#===============================================================================
-# When invoked from command line, run the tests
-#===============================================================================
-if __name__ == '__main__':
-  testKNNClassifier(0)
-  test_pca_knn(0)
-  testKNNClassifier(1)
-  test_pca_knn(1)
+
+  def testPCAKNNShort(self):
+    self._testPCAKNN(0)
+
+
+  def testKNNClassifierMedium(self):
+    self._testKNNClassifier(1)
+
+
+  def testPCAKNNMedium(self):
+    self._testPCAKNN(1)
+
+
+
+if __name__ == "__main__":
+  unittest.main()
