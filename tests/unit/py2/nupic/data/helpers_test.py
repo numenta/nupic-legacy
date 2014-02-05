895ad7a06412c0fff56cec2a044fa2608b980745
Chetan Surpur <chetan51@gmail.com>
2014-02-05 10:53:55 -0800

    Switch to unittest format

--- tests/unit/py2/nupic/data/helpers_test.py
+++ tests/unit/py2/nupic/data/helpers_test.py
@@ -21,103 +21,101 @@
 # ----------------------------------------------------------------------
 
 import os
-import sys
-import datetime
-import shutil
-import tempfile
-from nupic.support import title
+import unittest2 as unittest
+
 from nupic.data.datasethelpers import (_getDataDirs,
                                        findDataset,
                                        uncompressAndCopyDataset)
 
-def testGetDataDirs():
-  """ """
-  title()
-  dd =  _getDataDirs()
-  dd = zip(*dd)[0]
 
-  # Make sure the local data is there
-  assert ('data') in dd
-  # Make sure there are no duplicates
-  assert len(set(dd)) == len(dd)
 
-  nta_data_path = os.environ.get('NTA_DATA_PATH', None)
-  os.environ['NTA_DATA_PATH'] = 'XXX:YYY'
+class HelpersTest(unittest.TestCase):
 
-  dd =  _getDataDirs()
-  dd = zip(*dd)[0]
 
-  assert 'XXX' in dd and 'YYY' in dd
+  def testGetDataDirs(self):
+    dd =  _getDataDirs()
+    dd = zip(*dd)[0]
 
-  if nta_data_path is None:
-    del os.environ['NTA_DATA_PATH']
-  else:
-    os.environ['NTA_DATA_PATH'] = nta_data_path
+    # Make sure the local data is there
+    self.assertTrue(('data') in dd)
+    # Make sure there are no duplicates
+    self.assertEqual(len(set(dd)), len(dd))
 
-def testFindDataset():
-  title()
-  # Test non-existing dataset (relative path)
-  try:
-    findDataset('no_such_dataset.csv')
-    assert False
-  except:
-    pass
-  # Test non-existing dataset (absolute path)
-  try:
-    findDataset('/no_such_dataset.csv')
-    assert False
-  except:
-    pass
-  # Test existing dataset (relative path)
-  if not os.path.isdir('data'):
-    os.makedirs('data')
-  datasetPath = 'test_find_dataset.csv'
-  filename = 'data/test_find_dataset.csv'
-  # This is the uncompressed name.
-  fullPath = os.path.abspath(filename)
-  if os.path.exists(fullPath):
+    nta_data_path = os.environ.get('NTA_DATA_PATH', None)
+    os.environ['NTA_DATA_PATH'] = 'XXX:YYY'
+
+    dd =  _getDataDirs()
+    dd = zip(*dd)[0]
+
+    self.assertTrue('XXX' in dd and 'YYY' in dd)
+
+    if nta_data_path is None:
+      del os.environ['NTA_DATA_PATH']
+    else:
+      os.environ['NTA_DATA_PATH'] = nta_data_path
+
+
+  def testFindDataset(self):
+    # Test non-existing dataset (relative path)
+    try:
+      findDataset('no_such_dataset.csv')
+      self.fail("Shoudn't have found dataset")
+    except:
+      pass
+    # Test non-existing dataset (absolute path)
+    try:
+      findDataset('/no_such_dataset.csv')
+      self.fail("Shoudn't have found dataset")
+    except:
+      pass
+    # Test existing dataset (relative path)
+    if not os.path.isdir('data'):
+      os.makedirs('data')
+    datasetPath = 'test_find_dataset.csv'
+    filename = 'data/test_find_dataset.csv'
+    # This is the uncompressed name.
+    fullPath = os.path.abspath(filename)
+    if os.path.exists(fullPath):
+      os.remove(fullPath)
+    fullPathCompressed = fullPath + ".gz"
+    if os.path.exists(fullPathCompressed):
+      os.remove(fullPathCompressed)
+
+    # Create the "dataset"
+    open(filename, 'w').write('123')
+    path = findDataset(datasetPath)
+    self.assertEqual(path, fullPath)
+    self.assertTrue(os.path.exists(path))
+
+    # This should do nothing, since it is already compressed
+    path = uncompressAndCopyDataset(path)
+    self.assertEqual(path, fullPath)
+
+    # Test existing dataset (absolute path)
+    self.assertEqual(findDataset(fullPath), fullPath)
+
+    # Test existing dataset (compressed path)
+
+    # Create the compressed file
+    import gzip
+    f = gzip.GzipFile(fullPathCompressed, 'w')
+    f.write('1,2,3\n')
+    f.close()
+    self.assertTrue(os.path.isfile(fullPathCompressed))
+
+    # Remove the original file
     os.remove(fullPath)
-  fullPathCompressed = fullPath + ".gz"
-  if os.path.exists(fullPathCompressed):
+
+    self.assertEqual(findDataset(datasetPath), fullPathCompressed)
+    # This should put the uncompressed file in the same directory
+    path = uncompressAndCopyDataset(fullPathCompressed)
+    self.assertEqual(path, fullPath)
+    self.assertTrue(os.path.isfile(path))
+
+    os.remove(fullPath)
     os.remove(fullPathCompressed)
 
-  # Create the "dataset"
-  open(filename, 'w').write('123')
-  path = findDataset(datasetPath)
-  assert path == fullPath
-  assert os.path.exists(path)
 
-  # This should do nothing, since it is already compressed
-  path = uncompressAndCopyDataset(path)
-  assert path == fullPath
-
-  # Test existing dataset (absolute path)
-  assert findDataset(fullPath) == fullPath
-
-  # Test existing dataset (compressed path)
-
-  # Create the compressed file
-  import gzip
-  f = gzip.GzipFile(fullPathCompressed, 'w')
-  f.write('1,2,3\n')
-  f.close()
-  assert os.path.isfile(fullPathCompressed)
-
-  # Remove the original file
-  os.remove(fullPath)
-
-  assert findDataset(datasetPath) == fullPathCompressed
-  # This should put the uncompressed file in the same directory
-  path = uncompressAndCopyDataset(fullPathCompressed)
-  assert path == fullPath
-  assert os.path.isfile(path)
-
-  os.remove(fullPath)
-  os.remove(fullPathCompressed)
-
-def test():
-  testFindDataset()
-  testGetDataDirs()
 
 if __name__=='__main__':
-  test()
+  unittest.main()
