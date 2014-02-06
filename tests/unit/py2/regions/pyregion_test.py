ebda958702117282ec020165bb783a63628607ad
Chetan Surpur <chetan51@gmail.com>
2014-02-06 11:56:58 -0800

    Started converting to unittest format

--- tests/unit/py2/regions/pyregion_test.py
+++ tests/unit/py2/regions/pyregion_test.py
@@ -20,78 +20,83 @@
 # http://numenta.org/licenses/
 # ----------------------------------------------------------------------
 
+import unittest2 as unittest
+
 from nupic.regions.PyRegion import PyRegion
 
-def test():
-  class NoInit(PyRegion):
-    pass
 
-  try:
-    ni = NoInit()
-  except TypeError, e:
-    assert str(e) == "Can't instantiate abstract class NoInit with abstract methods __init__, compute, initialize"
 
-  class X(PyRegion):
-    def __init__(self):
-      self.x = 5
+class PyRegionTest(unittest.TestCase):
 
-  # Test unimplemented getSpec (results in NotImplementedError)
-  try:
-    X.getSpec()
-    assert False
-  except NotImplementedError:
-    pass
 
-  # Test unimplemented abstract methods (x can't be instantiated)
-  try:
-    x = X()
-  except TypeError, e:
-    assert str(e) == "Can't instantiate abstract class X with abstract methods compute, initialize"
+  def testClass(self):
+    class NoInit(PyRegion):
+      pass
 
-  # Test unimplemented @not_implemented methods
-  class Y(PyRegion):
-    def __init__(self):
-      self.zzz = 5
-      self._zzz = 3
-    def initialize(self): pass
-    def compute(self): pass
-    def getOutputElementCount(self): pass
+    try:
+      ni = NoInit()
+    except TypeError, e:
+      assert str(e) == "Can't instantiate abstract class NoInit with abstract methods __init__, compute, initialize"
 
-  # Can instantiate because all abstract methods are implemented
-  y = Y()
+    class X(PyRegion):
+      def __init__(self):
+        self.x = 5
 
-  # Can call the default getParameter() from PyRegion
-  assert y.getParameter('zzz', -1) == 5
+    # Test unimplemented getSpec (results in NotImplementedError)
+    try:
+      X.getSpec()
+      assert False
+    except NotImplementedError:
+      pass
 
-  # Accessing an attribute whose name starts with '_' via getParameter()
-  try:
-    y.getParameter('_zzz', -1) == 5
-    assert False
-  except Exception, e:
-    assert str(e) == 'Parameter name must not start with an underscore'
+    # Test unimplemented abstract methods (x can't be instantiated)
+    try:
+      x = X()
+    except TypeError, e:
+      assert str(e) == "Can't instantiate abstract class X with abstract methods compute, initialize"
 
-  # Calling not implemented method result in NotImplementedError
-  try:
-    y.setParameter('zzz', 4)
-    assert False
-  except NotImplementedError, e:
-    assert str(e) == \
-    'The unimplemented method setParameter() was called by test()'
+    # Test unimplemented @not_implemented methods
+    class Y(PyRegion):
+      def __init__(self):
+        self.zzz = 5
+        self._zzz = 3
+      def initialize(self): pass
+      def compute(self): pass
+      def getOutputElementCount(self): pass
 
-  class Z(object):
-    def __init__(self):
-      y = Y()
-      y.setParameter('zzz, 4')
+    # Can instantiate because all abstract methods are implemented
+    y = Y()
 
-  try:
-    z = Z()
-  except NotImplementedError, e:
-    assert str(e) == \
-    'The unimplemented method setParameter() was called by Z.__init__()'
+    # Can call the default getParameter() from PyRegion
+    assert y.getParameter('zzz', -1) == 5
 
-  print 'Done.'
+    # Accessing an attribute whose name starts with '_' via getParameter()
+    try:
+      y.getParameter('_zzz', -1) == 5
+      assert False
+    except Exception, e:
+      assert str(e) == 'Parameter name must not start with an underscore'
+
+    # Calling not implemented method result in NotImplementedError
+    try:
+      y.setParameter('zzz', 4)
+      assert False
+    except NotImplementedError, e:
+      assert str(e) == \
+      'The unimplemented method setParameter() was called by PyRegionTest.testClass()'
+
+    class Z(object):
+      def __init__(self):
+        y = Y()
+        y.setParameter('zzz, 4')
+
+    try:
+      z = Z()
+    except NotImplementedError, e:
+      assert str(e) == \
+      'The unimplemented method setParameter() was called by Z.__init__()'
 
 
 
 if __name__ == '__main__':
-  test()
+  unittest.main()
