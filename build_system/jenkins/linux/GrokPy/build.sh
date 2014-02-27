734cfd2e8e8def2ce3ab06004085a26ef492d0f9
David Ragazzi <davidragazzi@Davids-iMac.local>
2014-02-27 20:14:14 -0300

    Deletion of old build system
    Deletion of old build system (scripts at root folder will be removed
    later)

--- build_system/jenkins/linux/GrokPy/build.sh
+++
@@ -1,5 +0,0 @@
-TAG=$1
-WORKSPACE=$2
-cd $WORKSPACE
-python setup.py build
-echo $TAG > $WORKSPACE/build/lib/grokpy/.buildinfo
--- build_system/post_build/buildinfo.py
+++
@@ -1,5 +0,0 @@
-templateType = "copy_files"
-fileCollector = "defaultFileCollector"
-dependencies = []
-guid = "{24D2781B-4F7D-420D-AFF2-0522DD20BA07}"
-
