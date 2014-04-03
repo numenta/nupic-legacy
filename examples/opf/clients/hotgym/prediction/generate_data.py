6f470d80c6c0a680aa59f07e00c445e2c201687d
Matthew Taylor <rhyolight@gmail.com>
2014-04-03 10:43:33 -0700

    Working towards group swarming.

--- examples/opf/clients/hotgym/prediction/generate_data.py
+++ examples/opf/clients/hotgym/prediction/generate_data.py
@@ -1,3 +1,26 @@
+#!/usr/bin/python
+
+# ----------------------------------------------------------------------
+# Numenta Platform for Intelligent Computing (NuPIC)
+# Copyright (C) 2013, Numenta, Inc.  Unless you have an agreement
+# with Numenta, Inc., for a separate license for this software code, the
+# following terms and conditions apply:
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see http://www.gnu.org/licenses.
+#
+# http://numenta.org/licenses/
+# ----------------------------------------------------------------------
+
 import os
 import csv
 import datetime
@@ -52,19 +75,22 @@ def _process_line(line):
   gym_name = line[1]
   if gym_name not in data_out.keys():
     data_out[gym_name] = _create_output_header()
-  data_out[gym_name].append(_line_to_data(line));
+  data_out[gym_name].append(_line_to_data(line))
 
 
 def _write_data_files():
+  written_files = {}
   if not os.path.exists(LOCAL_DATA):
     os.makedirs(LOCAL_DATA)
   for name, data in data_out.iteritems():
     file_path = os.path.join(LOCAL_DATA, _to_file_name(name))
+    written_files[name] = file_path
     with open(file_path, 'wb') as file_out:
       writer = csv.writer(file_out)
       for line in data:
-        writer.writerow(line);
+        writer.writerow(line)
     print "Wrote output file: %s" % file_path
+  return written_files
 
 
 
@@ -72,13 +98,14 @@ def run():
   with open(INPUT, 'rb') as file_handle:
     reader = csv.reader(file_handle)
     # Skip header line.
-    reader.next();
+    reader.next()
     for line in reader:
       _process_line(line)
     # Now that all the data has been input and processed, write out the files.
-    _write_data_files()
+    written_files = _write_data_files()
     print "Low: %f\t\tHigh: %f" % (low, high)
+    return written_files
 
 
 if __name__ == '__main__':
-  run();
+  run()
