3e153e48926bacaa3fe18e3f12e577054c6faff8
David Ragazzi <david_ragazzi@hotmail.com>
2014-04-15 11:20:46 -0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+ 
+setup(
+    name = 'nupic',
+    packages = ['nupic'],
+    version = '0.0.1',
+    description = 'Numenta Platform for Intelligent Computing',
+    author='Numenta',
+    author_email='help@numenta.org',
+    url='https://github.com/numenta/nupic',
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: OS Independent',
+        'Development Status :: 1 - Planning',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: GIS'
+    ]
+)
