892787b01e0257e2ea90c36d0c0cc00b700a63fc
tester <test@nupic.com>
2014-04-15 11:15:31 -0700

    doxify doc

--- py/nupic/encoders/extras/utility.py
+++ py/nupic/encoders/extras/utility.py
@@ -54,12 +54,14 @@ class UtilityEncoder(MultiEncoder):
   """
   
   def __init__(self, inputEncoder, utilityEncoder, feval=None, feedbackDelay=0, name=None, forced=False):
-    """param inputEncoder: original encoder, accepts input; 
-       param feval: an evaluation function: must handle all inputs for inputEncoder, its output must be acceptable by utilityEncoder; util=feval(input);
-       param utilityEncoder: encoder, maps output of feval to some values. Must take all outputs of feval;
-       param feedbackDelay: (int) #of steps input sensors/actions are away from score default 0), that means:
+    """
+       @param inputEncoder: original encoder, accepts input; 
+       @param feval: an evaluation function: must handle all inputs for inputEncoder, its output must be acceptable by utilityEncoder; util=feval(input);
+       @param utilityEncoder: encoder, maps output of feval to some values. Must take all outputs of feval;
+       @param feedbackDelay: (int) #of steps input sensors/actions are away from score default 0), that means:
                  if set to 1, current score is linked with state&action taken 1-step ago; eg: now score(dry)=100 links with state(raining) and action(open umbrella);
-       param forced: used to allow this encoder in OPF description.py (set input/utilityEncoder to None), some steps need to be applied by hand (default False)"""
+       @param forced: used to allow this encoder in OPF description.py (set input/utilityEncoder to None), some steps need to be applied by hand (default False)
+    """
 
     if (not forced) and not(isinstance(inputEncoder,Encoder)  and isinstance(utilityEncoder, Encoder)):
       raise Exception("must provide an encoder and a function that takes encoder's output and transforms to utility encoder's input")
@@ -85,8 +87,10 @@ class UtilityEncoder(MultiEncoder):
     self._bufferedState = [] # list of prev states #TODO: make work for arbitrary n-back (now fixed for 0/1) 
 
   def encodeIntoArray(self, input, output):
-    """on the original input, first compute the utility and then append it as "input" for encoding; 
-       the feval function is applied before any encoding"""
+    """
+    on the original input, first compute the utility and then append it as "input" for encoding; 
+    the feval function is applied before any encoding
+    """
     input = self._handleNBack(input)
     score = self.getScoreIN(input)
     encoded_in = self._encoder.encode(input)
@@ -99,7 +103,10 @@ class UtilityEncoder(MultiEncoder):
     return output
 
   def getScoreIN(self, input):
-    """compute score of the feedforward input"""
+    """
+    compute score of the feedforward input
+    @returns score computed by **feval** for the input
+    """
     if self.evaluate is not None:
       return self.evaluate(input)
     else:
@@ -107,7 +114,10 @@ class UtilityEncoder(MultiEncoder):
 
 
   def getScoreOUT(self, encoded):
-    """get the score from the encoded representation"""
+    """
+    @param encoded 
+    @returns score from the encoded representation
+    """
     # get the score's portion of data
     score_bits = encoded[self._offset:self.getWidth()]
     dec = self._utility.topDownCompute(score_bits)
@@ -124,9 +134,11 @@ class UtilityEncoder(MultiEncoder):
     return decoded[0][fieldName][0]
 
   def setEvaluationFn(self, feval):
-    """set the feval function;
-       an evaluation function: must handle all inputs from inputEncoder, 
-       its output must be acceptable by utilityEncoder; util=feval(input)"""
+    """
+    set the feval function;
+    @param feval - an evaluation function: must handle all inputs from inputEncoder, 
+    its output must be acceptable by utilityEncoder; util=feval(input)
+    """
     if not(type(feval)==FunctionType or feval is None):
       raise Exception("feval must be a function (or None for disabled). type(feval)=",type(feval))
     self.evaluate=feval
@@ -134,7 +146,8 @@ class UtilityEncoder(MultiEncoder):
   def _handleNBack(self,state):
     """handle the possible delay between score & state; 
        use buffer of inputs for that
-       param state: the input state to be remembered (buffered)"""
+       @param state: the input state to be remembered (buffered)
+    """
     if(self.delay==0): #nothing changes
       return state
     elif(self.delay==1):
@@ -148,9 +161,11 @@ class UtilityEncoder(MultiEncoder):
 
 ######################################################
 class SimpleUtilityEncoder(UtilityEncoder):
-  """simple version of utility; 
+  """
+  simple version of @ref UtilityEncoder
   data is a vector of numbers; defaults=5elements, -5..5, resolution=1;
-  utility is a scalar, 0..100, resolution 1"""
+  utility is a scalar, 0..100, resolution 1
+  """
 
   def __init__(self, length=5, feedbackDelay=0, minval=-5, maxval=5, resolution=1, scoreMin=0, scoreMax=100, scoreResolution=1, forced=False):
     dataS = ScalarEncoder(21, minval, maxval, resolution=resolution, name='idx')
