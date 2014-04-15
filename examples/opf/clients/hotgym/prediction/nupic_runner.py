037ea930471fe964529f49be9ad2ccb00b0086f0
Matthew Taylor <rhyolight@gmail.com>
2014-04-15 13:52:29 -0700

    Making helpers non-executable.

--- examples/opf/clients/hotgym/prediction/nupic_runner.py
+++ examples/opf/clients/hotgym/prediction/nupic_runner.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python
-
 # ----------------------------------------------------------------------
 # Numenta Platform for Intelligent Computing (NuPIC)
 # Copyright (C) 2013, Numenta, Inc.  Unless you have an agreement
--- examples/opf/clients/hotgym/prediction/swarm_helper.py
+++ examples/opf/clients/hotgym/prediction/swarm_helper.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python
-
 import os
 import pprint
 
