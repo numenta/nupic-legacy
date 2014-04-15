4bdc8dbd667186ac4e8f8574655bcf4411bdd482
David Ragazzi <david_ragazzi@hotmail.com>
2014-04-15 11:32:00 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name = 'nupic',
     packages = ['nupic'],
-    version = '0.0.1',
+    version = '1.0.0',
     description = 'Numenta Platform for Intelligent Computing',
     author='Numenta',
     author_email='help@numenta.org',
@@ -13,9 +13,14 @@ setup(
         'Programming Language :: Python :: 2',
         'License :: OSI Approved :: GNU General Public License (GPL)',
         'Operating System :: OS Independent',
-        'Development Status :: 1 - Planning',
+        'Development Status :: 4 - Beta',
         'Environment :: Console',
         'Intended Audience :: Science/Research',
         'Topic :: Scientific/Engineering :: GIS'
-    ]
+    ],
+    long_description = """\
+NuPIC is a library that provides the building blocks for online prediction systems. The library contains the Cortical Learning Algorithm (CLA), but also the Online Prediction Framework (OPF) that allows clients to build prediction systems out of encoders, models, and metrics.
+
+For more information, see numenta.org or the NuPIC wiki (https://github.com/numenta/nupic/wiki).
+"""
 )
