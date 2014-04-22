5c3fdc4d82d1625deb57caa15f223b663f9ae904
Matthew Taylor <rhyolight@gmail.com>
2014-04-22 13:22:18 -0700

    Putting together one-gym example as first step.

---
+++ examples/opf/clients/hotgym/prediction/one_gym/nupic_runner.py
@@ -0,0 +1,71 @@
+# ----------------------------------------------------------------------
+# Numenta Platform for Intelligent Computing (NuPIC)
+# Copyright (C) 2013, Numenta, Inc.  Unless you have an agreement
+# with Numenta, Inc., for a separate license for this software code, the
+# following terms and conditions apply:
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see http://www.gnu.org/licenses.
+#
+# http://numenta.org/licenses/
+# ----------------------------------------------------------------------
+import importlib
+import os
+
+from nupic.frameworks.opf.modelfactory import ModelFactory
+
+from swarm_helper import swarm_for_input
+from io_helper import run_io_through_nupic
+
+
+DATA_DIR = "./local_data"
+MODEL_PARAMS_DIR = "./model_params"
+
+
+
+def _create_model(model_params):
+  model = ModelFactory.create(model_params)
+  model.enableInference({"predictedField": "kw_energy_consumption"})
+  return model
+
+
+
+def _get_model_params_from_name(gym_name):
+  imported_model_params = importlib.import_module(
+    "model_params.%s_model_params" % (
+      gym_name.replace(" ", "_").replace("-", "_")
+    )
+  ).MODEL_PARAMS
+  return imported_model_params
+
+
+
+def run_model(gym_name, plot=False):
+  print "Creating model from %s..." % gym_name
+  model = _create_model(_get_model_params_from_name(gym_name))
+  input_data = ["%s/%s.csv" % (DATA_DIR, gym_name.replace(" ", "_"))]
+  run_io_through_nupic(input_data, [model], [gym_name], plot)
+
+
+
+def run_all_models(plot=False):
+  models = []
+  names = []
+  input_files = []
+  for input_file in sorted(os.listdir(DATA_DIR)):
+    name = os.path.splitext(input_file)[0]
+    names.append(name)
+    input_files.append(os.path.abspath(os.path.join(DATA_DIR, input_file)))
+    models.append(_create_model(_get_model_params_from_name(name)))
+  run_io_through_nupic(input_files, models, names, plot)
+
+
---
+++ examples/opf/clients/hotgym/prediction/one_gym/swarm_helper.py
@@ -0,0 +1,89 @@
+import os
+import pprint
+
+from nupic.swarming import permutations_runner
+from base_swarm_description import BASE_SWARM_DESCRIPTION
+
+
+INPUT_FILE = "Balgowlah_Platinum.csv"
+PERMUTATIONS_PATH = "swarm/permutations.py"
+
+
+def _get_swarm_description_for(input_data_file_path):
+  print "Constructing swarm desc for %s" % input_data_file_path
+  desc_copy = dict(BASE_SWARM_DESCRIPTION)
+  stream = desc_copy["streamDef"]["streams"][0]
+  stream["info"] = input_data_file_path
+  stream["source"] = "file://%s" % input_data_file_path
+  return desc_copy
+
+
+
+def _model_params_to_string(model_params):
+  pp = pprint.PrettyPrinter(indent=2)
+  return pp.pformat(model_params)
+
+
+
+def _write_model_params_file(model_params, name):
+  clean_name = name.replace(" ", "_").replace("-", "_")
+  params_name = "%s_model_params.py" % clean_name
+  out_dir = os.path.join(os.getcwd(), 'model_params')
+  if not os.path.isdir(out_dir):
+    os.mkdir(out_dir)
+  out_path = os.path.join(os.getcwd(), 'model_params', params_name)
+  with open(out_path, "wb") as out_file:
+    model_params_string = _model_params_to_string(model_params)
+    out_file.write("MODEL_PARAMS = \\\n%s" % model_params_string)
+    print "Wrote model params file to %s" % out_path
+  return out_path
+
+
+
+def _swarm_for_best_model_params(name, max_workers=4):
+  output_label = name
+  perm_work_dir = os.getcwd()
+  options = {
+    "maxWorkers": max_workers, "overwrite": True,
+  }
+  options.verbosityCount = 0
+  model_params = permutations_runner.runWithPermutationsScript(
+    PERMUTATIONS_PATH, options, output_label, perm_work_dir
+  )
+  model_params_file = _write_model_params_file(model_params, name)
+  return model_params_file, model_params
+
+
+
+def swarm_for_input(name):
+  # swarm_description = _get_swarm_description_for(input_file_path)
+  print "================================================="
+  print "= Swarming on %s data..." % name
+  print "================================================="
+  # return _swarm_for_best_model_params(swarm_description, name)
+  _swarm_for_best_model_params(name)
+
+
+
+def _run_swarm(file_path):
+  name = os.path.splitext(os.path.basename(file_path))[0]
+  return swarm_for_input(name)
+
+
+
+def _report(output):
+  print "\nWrote the following model param files:"
+  def model_report(one_output):
+    print "\t%s" % one_output[0]
+  if isinstance(output, list):
+    for i in output:
+      model_report(i)
+  else:
+    model_report(output)
+
+
+
+def swarm():
+  output = _run_swarm(INPUT_FILE)
+  # _report(output)
+  return output
