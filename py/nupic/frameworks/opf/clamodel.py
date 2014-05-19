bd2ee6b8ce43663ef2a14106c0e2199f9601fc41
Scott Purdy <scott@fer.io>
2014-05-19 13:58:34 -0700

    Fixes bug in model for getting classifier in the case where it doesn't exist.

--- py/nupic/frameworks/opf/clamodel.py
+++ py/nupic/frameworks/opf/clamodel.py
@@ -997,8 +997,9 @@ class CLAModel(Model):
     """
     Returns reference to the network's Classifier region
     """
-    if 'Classifier' in self._netInfo.net.regions:
-      return self._netInfo.net.regions['Classifier']
+    if (self._netInfo.net is not None and
+        "Classifier" in self._netInfo.net.regions):
+      return self._netInfo.net.regions["Classifier"]
     else:
       return None
 
