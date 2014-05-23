716189f8fda4ee5c8cbc07465e139035d900bf39
Marek Otahal <markotahal@gmail.com>
2014-05-23 04:39:25 +0200

    make anomaly.py a class for further modularity
    and convert tests / usecases accordingly;
    Just create an instance of nupic.algorithms.anomaly.Anomaly()
    and call the computeAnomalyScore() method

--- py/nupic/algorithms/anomaly.py
+++ py/nupic/algorithms/anomaly.py
@@ -24,30 +24,33 @@
 import numpy
 
 
+class Anomaly(object):
+  """basic class that computes anomaly"""
 
-def computeAnomalyScore(activeColumns, prevPredictedColumns):
-  """Compute the anomaly score as the percent of active columns not predicted.
+  def computeAnomalyScore(self, activeColumns, prevPredictedColumns):
+    """Compute the anomaly score as the percent of active columns not predicted.
+  
+    :param activeColumns: array of active column indices
+    :param prevPredictedColumns: array of columns indices predicted in previous step
+    :returns: the computed anomaly score
+    """
+    nActiveColumns = len(activeColumns)
 
-  :param activeColumns: array of active column indices
-  :param prevPredictedColumns: array of columns indices predicted in previous step
-  :returns: the computed anomaly score
-  """
-  nActiveColumns = len(activeColumns)
+    if nActiveColumns > 0:
+      # Test whether each element of a 1-D array is also present in a second
+      # array. Sum to get the total # of columns that are active and were
+      # predicted.
+      score = numpy.in1d(activeColumns, prevPredictedColumns).sum()
 
-  if nActiveColumns > 0:
-    # Test whether each element of a 1-D array is also present in a second
-    # array. Sum to get the total # of columns that are active and were
-    # predicted.
-    score = numpy.in1d(activeColumns, prevPredictedColumns).sum()
+      # Get the percent of active columns that were NOT predicted, that is
+      # our anomaly score.
+      score = (nActiveColumns - score) / float(nActiveColumns)
+    elif len(prevPredictedColumns) > 0:
+      # There were predicted columns but none active.
+      score = 1.0
+    else:
+      # There were no predicted or active columns.
+      score = 0.0
 
-    # Get the percent of active columns that were NOT predicted, that is
-    # our anomaly score.
-    score = (nActiveColumns - score) / float(nActiveColumns)
-  elif len(prevPredictedColumns) > 0:
-    # There were predicted columns but none active.
-    score = 1.0
-  else:
-    # There were no predicted or active columns.
-    score = 0.0
+    return score
 
-  return score
