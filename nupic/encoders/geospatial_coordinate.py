cefe76a63f6a8e50e3d19b4b6b9e819141ace7ac
Chetan Surpur <chetan51@gmail.com>
2014-06-13 21:52:03 +0530

    Add some overlap between adjacent geospatial encodings

--- nupic/encoders/geospatial_coordinate.py
+++ nupic/encoders/geospatial_coordinate.py
@@ -104,13 +104,14 @@ class GeospatialCoordinateEncoder(CoordinateEncoder):
     Returns radius for given speed.
 
     Tries to get the encodings of consecutive readings to be
-    right next to each other.
+    adjacent with some overlap.
 
     @param speed (float) Speed (in meters per second)
-    @return (int) Radius for given spede
+    @return (int) Radius for given speed
     """
+    overlap = 1.5
     coordinatesPerTimestep = speed * self.timestep / self.scale
-    radius = int(round(float(coordinatesPerTimestep) / 2))
+    radius = int(round(float(coordinatesPerTimestep) / 2 * overlap))
     minRadius = int(math.ceil((math.sqrt(self.w) - 1) / 2))
     return max(radius, minRadius)
 
