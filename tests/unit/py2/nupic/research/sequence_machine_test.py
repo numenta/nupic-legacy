79e30f28e0c2981f0fdcd4062274b5f4e119c117
Chetan Surpur <chetan51@gmail.com>
2014-07-22 12:58:43 -0700

    Added SequenceMachine

---
+++ tests/unit/py2/nupic/research/sequence_machine_test.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# ----------------------------------------------------------------------
+# Numenta Platform for Intelligent Computing (NuPIC)
+# Copyright (C) 2014, Numenta, Inc.  Unless you have an agreement
+# with Numenta, Inc., for a separate license for this software code, the
+# following terms and conditions apply:
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see http://www.gnu.org/licenses.
+#
+# http://numenta.org/licenses/
+# ----------------------------------------------------------------------
+import unittest
+
+from nupic.research.pattern_machine import ConsecutivePatternMachine
+from nupic.research.sequence_machine import SequenceMachine
+
+
+
+class SequenceMachineTest(unittest.TestCase):
+
+
+  def setUp(self):
+    self.patternMachine = ConsecutivePatternMachine(100, 5)
+    self.sequenceMachine = SequenceMachine(self.patternMachine)
+
+
+  def testGenerateFromNumbers(self):
+    numbers = range(0, 10) + [None] + range(10, 19)
+    sequence = self.sequenceMachine.generateFromNumbers(numbers)
+    self.assertEqual(len(sequence), 20)
+    self.assertEqual(sequence[0], self.patternMachine.get(0))
+    self.assertEqual(sequence[10], None)
+    self.assertEqual(sequence[11], self.patternMachine.get(10))
+
+
+
+if __name__ == '__main__':
+  unittest.main()
