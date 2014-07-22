d68aa557431badafbed1f1d5272fc2f9b2a05e71
Chetan Surpur <chetan51@gmail.com>
2014-07-22 11:49:24 -0700

    Added PatternMachine and ConsecutivePatternMachine

---
+++ nupic/research/pattern_machine.py
@@ -0,0 +1,140 @@
+# ----------------------------------------------------------------------
+# Numenta Platform for Intelligent Computing (NuPIC)
+# Copyright (C) 2014, Numenta, Inc.  Unless you have an agreement
+# with Numenta, Inc., for a separate license for this software code, the
+# following terms and conditions apply:
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see http://www.gnu.org/licenses.
+#
+# http://numenta.org/licenses/
+# ----------------------------------------------------------------------
+
+"""
+Utilities for generating and manipulating patterns, for use in
+experimentation and tests.
+"""
+
+import random
+
+
+
+class PatternMachine(object):
+  """
+  Base pattern machine class.
+  """
+
+  def __init__(self,
+               n,
+               w,
+               num=100,
+               seed=42):
+    """
+    @param n   (int) Number of available bits in pattern.
+    @param w   (int) Number of on bits in pattern.
+    @param num (int) Number of available patterns.
+    """
+    # Save member variables
+    self.n = n
+    self.w = w
+    self.num = num
+
+    # Initialize member variables
+    random.seed(seed)
+    self.patterns = dict()
+
+    self._generate()
+
+
+  def get(self, number):
+    """
+    Return a pattern for a number.
+
+    @param number (int) Number of pattern
+
+    @return (set) Indices of on bits
+    """
+    if not number in self.patterns:
+      raise IndexError("Invalid number")
+
+    return self.patterns[number]
+
+
+  def numbersForBit(self, bit):
+    """
+    Return the set of pattern numbers that match a bit.
+
+    @param bit (int) Index of bit
+
+    @return (set) Indices of numbers
+    """
+    if bit >= self.n:
+      raise IndexError("Invalid bit")
+
+    numbers = set()
+
+    for index, pattern in self.patterns.iteritems():
+      if bit in pattern:
+        numbers.add(index)
+
+    return numbers
+
+
+  def numberMapForBits(self, bits):
+    """
+    Return a map from number to matching on bits,
+    for all numbers that match a set of bits.
+
+    @param bits (set) Indices of bits
+
+    @return (dict) Mapping from number => on bits.
+    """
+    numberMap = dict()
+
+    for bit in bits:
+      numbers = self.numbersForBit(bit)
+
+      for number in numbers:
+        if not number in numberMap:
+          numberMap[number] = set()
+
+        numberMap[number].add(bit)
+
+    return numberMap
+
+
+  def _generate(self):
+    """
+    Generates set of random patterns.
+    """
+    for i in xrange(self.num):
+      pattern = random.sample(xrange(self.n), self.w)
+      self.patterns[i] = set(pattern)
+
+
+
+class ConsecutivePatternMachine(PatternMachine):
+  """
+  Pattern machine class that generates patterns with non-overlapping,
+  consecutive on bits.
+  """
+
+  def _generate(self):
+    """
+    Generates set of consecutive patterns.
+    """
+    n = self.n
+    w = self.w
+
+    for i in xrange(n / w):
+      pattern = set(xrange(i * w, (i+1) * w))
+      self.patterns[i] = pattern
---
+++ tests/unit/py2/nupic/research/pattern_machine_test.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+# ----------------------------------------------------------------------
+# Numenta Platform for Intelligent Computing (NuPIC)
+# Copyright (C) 2014, Numenta, Inc.  Unless you have an agreement
+# with Numenta, Inc., for a separate license for this software code, the
+# following terms and conditions apply:
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see http://www.gnu.org/licenses.
+#
+# http://numenta.org/licenses/
+# ----------------------------------------------------------------------
+import unittest
+
+from nupic.research.pattern_machine import (PatternMachine,
+                                            ConsecutivePatternMachine)
+
+
+
+class PatternMachineTest(unittest.TestCase):
+
+
+  def setUp(self):
+    self.patternMachine = PatternMachine(10000, 5, num=50)
+
+
+  def testGet(self):
+    patternA = self.patternMachine.get(48)
+    self.assertEqual(len(patternA), 5)
+
+    patternB = self.patternMachine.get(49)
+    self.assertEqual(len(patternB), 5)
+
+    self.assertEqual(patternA & patternB, set())
+
+
+  def testGetOutOfBounds(self):
+    args = [50]
+    self.assertRaises(IndexError, self.patternMachine.get, *args)
+
+
+  def testNumbersForBit(self):
+    pattern = self.patternMachine.get(49)
+
+    for bit in pattern:
+      self.assertEqual(self.patternMachine.numbersForBit(bit), {49})
+
+
+  def testNumbersForBitOutOfBounds(self):
+    args = [10000]
+    self.assertRaises(IndexError, self.patternMachine.numbersForBit, *args)
+
+
+  def testNumberMapForBits(self):
+    pattern = self.patternMachine.get(49)
+    numberMap = self.patternMachine.numberMapForBits(pattern)
+
+    self.assertEqual(numberMap.keys(), [49])
+    self.assertEqual(numberMap[49], pattern)
+
+
+
+class ConsecutivePatternMachineTest(unittest.TestCase):
+
+
+  def setUp(self):
+    self.patternMachine = ConsecutivePatternMachine(100, 5)
+
+
+  def testGet(self):
+    pattern = self.patternMachine.get(18)
+    self.assertEqual(len(pattern), 5)
+    self.assertEqual(pattern, {90, 91, 92, 93, 94})
+
+    pattern = self.patternMachine.get(19)
+    self.assertEqual(len(pattern), 5)
+    self.assertEqual(pattern, {95, 96, 97, 98, 99})
+
+
+  def testGetOutOfBounds(self):
+    args = [20]
+    self.assertRaises(IndexError, self.patternMachine.get, *args)
+
+
+
+if __name__ == '__main__':
+  unittest.main()
