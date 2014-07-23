7845452332c21b1e7f222a15ba99c6c916e39768
Chetan Surpur <chetan51@gmail.com>
2014-07-22 23:24:47 -0700

    Fix copyrights

--- tests/integration/py2/nupic/algorithms/tm_test.py
+++ tests/integration/py2/nupic/algorithms/tm_test.py
@@ -15,9 +15,9 @@
 # See the GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program.  If not, see httm://www.gnu.org/licenses.
+# along with this program.  If not, see http://www.gnu.org/licenses.
 #
-# httm://numenta.org/licenses/
+# http://numenta.org/licenses/
 # ----------------------------------------------------------------------
 
 import argparse
