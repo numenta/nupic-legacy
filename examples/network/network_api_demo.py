9d19f1dc765e77d7b2a461888288f1baceb0e9ea
Scott Purdy <scott@fer.io>
2014-08-04 12:34:44 -0700

    Adds Python Network example that mimics the hotgym anomaly OPF client and yields identical anomaly scores.

--- examples/network/network_api_demo.py
+++ examples/network/network_api_demo.py
@@ -20,8 +20,10 @@
 # http://numenta.org/licenses/
 # ----------------------------------------------------------------------
 
-import json
 import copy
+import csv
+import json
+import os
 
 from nupic.algorithms.anomaly import computeAnomalyScore
 from nupic.data.datasethelpers import findDataset
@@ -29,107 +31,105 @@ from nupic.data.file_record_stream import FileRecordStream
 from nupic.engine import Network
 from nupic.encoders import MultiEncoder
 
-
 _VERBOSITY = 0         # how chatty the demo should be
 _SEED = 1956             # the random seed used throughout
-_DATA_PATH = "../prediction/data/extra/hotgym/rec-center-hourly.csv"
+_DATA_PATH = "extra/hotgym/rec-center-hourly.csv"
 _OUTPUT_PATH = "test_output.csv"
-
+_NUM_RECORDS = 2000
 
 # Config field for SPRegion
-g_spRegionConfig = dict(
-  spVerbosity = _VERBOSITY,
-  spatialImp = 'cpp',
-  globalInhibition = 1,
-  columnCount = 2048,
-  inputWidth   = 0,
-  numActivePerInhArea = 40,
-  seed = 1956,
-  potentialPct = 0.8,
-  synPermConnected = 0.1,
-  synPermActiveInc = 0.0001,
-  synPermInactiveDec = 0.0005,
-  maxBoost = 1.0
-  )
-
+SP_PARAMS = {
+    "spVerbosity": _VERBOSITY,
+    "spatialImp": "cpp",
+    "globalInhibition": 1,
+    "columnCount": 2048,
+    "inputWidth": 0,
+    "numActiveColumnsPerInhArea": 40,
+    "seed": 1956,
+    "potentialPct": 0.8,
+    "synPermConnected": 0.1,
+    "synPermActiveInc": 0.0001,
+    "synPermInactiveDec": 0.0005,
+    "maxBoost": 1.0,
+}
 
 # Config field for TPRegion
-g_tpRegionConfig = dict(
-  verbosity = _VERBOSITY,
-  columnCount = 2048,
-  cellsPerColumn = 32,
-  inputWidth   = 2048,
-  seed = 1960,
-  temporalImp = 'cpp',
-  newSynapseCount = 20,
-  maxSynapsesPerSegment = 32,
-  maxSegmentsPerCell = 128,
-  initialPerm = 0.21,
-  permanenceInc = 0.1,
-  permanenceDec = 0.1,
-  globalDecay = 0.0,
-  maxAge = 0,
-  minThreshold = 9,
-  activationThreshold = 12,
-  outputType = 'normal',
-  pamLength = 3
-  )
+TP_PARAMS = {
+    "verbosity": _VERBOSITY,
+    "columnCount": 2048,
+    "cellsPerColumn": 32,
+    "inputWidth": 2048,
+    "seed": 1960,
+    "temporalImp": 'cpp',
+    "newSynapseCount": 20,
+    "maxSynapsesPerSegment": 32,
+    "maxSegmentsPerCell": 128,
+    "initialPerm": 0.21,
+    "permanenceInc": 0.1,
+    "permanenceDec": 0.1,
+    "globalDecay": 0.0,
+    "maxAge": 0,
+    "minThreshold": 9,
+    "activationThreshold": 12,
+    "outputType": "normal",
+    "pamLength": 3,
+}
+
 
 
 def createEncoder():
   """Create the encoder instance for our test and return it."""
   encoder = MultiEncoder()
   encoder.addMultipleEncoders({
-    'timestamp': dict(fieldname='timestamp',
-                      type='DateEncoder',
-                      timeOfDay=(21, 9.5)),
-
-    'consumption': dict(clipInput=True,
-                        fieldname='consumption',
-                        maxval = 100.0,
-                        minval = 0.0,
-                        n=50,
-                        type='ScalarEncoder',
-                        name='consumption',
-                        w=21),
-    })
+      "consumption": {
+          "clipInput": True,
+          "fieldname": u"consumption",
+          "maxval": 100.0,
+          "minval": 0.0,
+          "n": 50,
+          "name": u"consumption",
+          "type": "ScalarEncoder",
+          "w": 21,
+      },
+      "timestamp_timeOfDay": {
+          "fieldname": u"timestamp",
+          "name": u"timestamp_timeOfDay",
+          "timeOfDay": (21, 9.5),
+          "type": "DateEncoder",
+      },
+      #"timestamp_weekend": {
+      #    "fieldname": u"timestamp",
+      #    "name": u"timestamp_weekend",
+      #    "type": "DateEncoder",
+      #    "weekend": 21,
+      #},
+  })
 
   return encoder
 
-"""         _________________
- Input --> |                 |
-           | Spatial Pooler  |
-           |                 |
-           |vvvvvvvvvvvvvvvvv|
-           |                 |
-           | Temporal Pooler |
-           |_________________|
-                   |
-                   '------> Output
-"""
 
 
-def createNetwork():
-  print "Creating network..."
+def createNetwork(dataSource):
+  """Create the Network instance.
 
+  :param dataSource: a RecordStream instance to get data from
+  :returns: a Network instance ready to run
+  """
   encoder = createEncoder()
-  trainFile = findDataset(_DATA_PATH)
-  print (trainFile)
-  dataSource = FileRecordStream(streamID=trainFile)
-  #dataSource.setAutoRewind(True)
 
   network = Network()
 
   # Our input is sensor data from the gym.csv file
-  network.addRegion("sensor", "py.RecordSensor", json.dumps(dict(verbosity = _VERBOSITY)))
-  sensor = network.regions['sensor'].getSelf()
+  network.addRegion("sensor", "py.RecordSensor",
+                    json.dumps({"verbosity": _VERBOSITY}))
+  sensor = network.regions["sensor"].getSelf()
   sensor.encoder = encoder
   sensor.dataSource = dataSource
 
   # |sensor| -> |spatialPoolerRegion|
   # Create the spatial pooler region
-  g_spRegionConfig['inputWidth'] = encoder.getWidth()
-  network.addRegion("spatialPoolerRegion", "py.SPRegion", json.dumps(g_spRegionConfig))
+  SP_PARAMS["inputWidth"] = encoder.getWidth()
+  network.addRegion("spatialPoolerRegion", "py.SPRegion", json.dumps(SP_PARAMS))
 
   # Link the SP region to the sensor input
   network.link("sensor", "spatialPoolerRegion", "UniformLink", "")
@@ -142,8 +142,8 @@ def createNetwork():
 
   # |sensor| -> |spatialPoolerRegion| -> |temporalPoolerRegion|
   # Add the Temporal Pooler Region on top of the existing network
-  g_tpRegionConfig['inputWidth'] = g_spRegionConfig['columnCount']
-  network.addRegion("temporalPoolerRegion", "py.TPRegion", json.dumps(g_tpRegionConfig))
+  TP_PARAMS["inputWidth"] = SP_PARAMS["columnCount"]
+  network.addRegion("temporalPoolerRegion", "py.TPRegion", json.dumps(TP_PARAMS))
 
   network.link("spatialPoolerRegion", "temporalPoolerRegion", "UniformLink", "")
   network.link("temporalPoolerRegion", "spatialPoolerRegion", "UniformLink", "",
@@ -151,87 +151,68 @@ def createNetwork():
   network.link("sensor", "temporalPoolerRegion", "UniformLink", "",
                srcOutput="resetOut", destInput="resetIn")
 
+  network.initialize()
+
+  spatialPoolerRegion = network.regions["spatialPoolerRegion"]
+  spatialPoolerRegion.setParameter("topDownMode", False)
+  spatialPoolerRegion.setParameter("learningMode", True)
+  spatialPoolerRegion.setParameter("inferenceMode", True)
+  spatialPoolerRegion.setParameter("anomalyMode", False)
+
+  temporalPoolerRegion = network.regions["temporalPoolerRegion"]
+  temporalPoolerRegion.setParameter("learningMode", True)
+  temporalPoolerRegion.setParameter("inferenceMode", True)
+  temporalPoolerRegion.setParameter("anomalyMode", True)
+  temporalPoolerRegion.setParameter("topDownMode", True)
+
   return network
 
 
-def runNetwork(network):
+def runNetwork(network, writer):
+  """Run the network and write output to writer.
 
-  network.initialize()
-
-  spatialPoolerRegion = network.regions['spatialPoolerRegion']
-  spatialPoolerRegion.setParameter('learningMode', 1)
-  #spatialPoolerRegion.setParameter('inferenceMode', 0)
-
-  #network.run(500)
-  #spatialPoolerRegion.setParameter('learningMode', 0)
-  spatialPoolerRegion.setParameter('inferenceMode', 1)
-  spatialPoolerRegion.setParameter('anomalyMode', 0)
-
-  temporalPoolerRegion = network.regions['temporalPoolerRegion']
-  temporalPoolerRegion.setParameter('learningMode', 1)
-  temporalPoolerRegion.setParameter('inferenceMode', 1)
-  temporalPoolerRegion.setParameter('anomalyMode', 1)
-  temporalPoolerRegion.setParameter('topDownMode', 1)
-  network.setMaxEnabledPhase(network.maxPhase)
+  :param network: a Network instance to run
+  :param writer: a csv.writer instance to write output to
+  """
+  sensorRegion = network.regions["sensor"]
+  spatialPoolerRegion = network.regions["spatialPoolerRegion"]
+  temporalPoolerRegion = network.regions["temporalPoolerRegion"]
 
   prevPredictedColumns = []
 
-  #"""
-  for _ in xrange(50):
+  i = 0
+  for _ in xrange(_NUM_RECORDS):
     # Run the network for a single iteration
     network.run(1)
 
-    activeColumns = spatialPoolerRegion.getOutputData("bottomUpOut").nonzero()[0]
+    activeColumns = spatialPoolerRegion.getOutputData(
+        "bottomUpOut").nonzero()[0]
 
     # Calculate the anomaly score using the active columns
     # and previous predicted columns
     anomalyScore = computeAnomalyScore(activeColumns, prevPredictedColumns)
 
+    # Write out the anomaly score along with the record number and consumption
+    # value.
+    consumption = sensorRegion.getOutputData("sourceOut")[0]
+    writer.writerow((i, consumption, anomalyScore))
+
     # Store the predicted columns for the next timestep
-    predictedColumns = temporalPoolerRegion.getOutputData("topDownOut").nonzero()[0]
+    predictedColumns = temporalPoolerRegion.getOutputData(
+        "topDownOut").nonzero()[0]
     prevPredictedColumns = copy.deepcopy(predictedColumns)
 
-    print anomalyScore
-  """
+    i += 1
 
-  # Run inference on the network for 100 iterations"
-  with open(_DATA_PATH, "rb") as inputCSV:
-    with open(_OUTPUT_PATH, "wb") as outputCSV:
 
-      reader = csv.reader(inputCSV)
-      # Skip the header lines
-      reader.next()
-      reader.next()
-      reader.next()
-
-      writer = csv.writer(outputCSV)
-      # Write the column titles for the output file
-      writer.writerow(["timestamp", "consumption", "anomaly_score"])
-
-      for index, inputRow in enumerate(reader):
-        if index % 1000 == 0 and index != 0:
-          print ("read %s lines" % index)
-
-        if index > 5000:
-          break
-
-        # Run the network for a single iteration
-        network.run(1)
-
-        activeColumns = spatialPoolerRegion.getOutputData("bottomUpOut").nonzero()[0]
-
-        # Calculate the anomaly score using the active columns
-        # and previous predicted columns
-        anomalyScore = computeAnomalyScore(activeColumns, prevPredictedColumns)
-
-        # Store the predicted columns for the next timestep
-        predictedColumns = temporalPoolerRegion.getOutputData("bottomUpOut").nonzero()[0]
-        prevPredictedColumns = copy.deepcopy(predictedColumns)
-
-        # Write the anomaly as "timestamp, consumption, anomaly"
-        writer.writerow([str(inputRow[0]), inputRow[1], anomalyScore])
-  #"""
 
 if __name__ == "__main__":
-  network = createNetwork()
-  runNetwork(network)
+  trainFile = findDataset(_DATA_PATH)
+  dataSource = FileRecordStream(streamID=trainFile)
+
+  network = createNetwork(dataSource)
+  outputPath = os.path.join(os.path.dirname(__file__), _OUTPUT_PATH)
+  with open(outputPath, "w") as outputFile:
+    writer = csv.writer(outputFile)
+    print "Writing output to %s" % outputPath
+    runNetwork(network, writer)
