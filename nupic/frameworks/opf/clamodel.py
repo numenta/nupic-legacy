a9fed689fc90b5cdb058914b86ad07b9e1b38882
Scott Purdy <scott@fer.io>
2014-09-11 13:25:08 -0700

    Bug fixes from Marek's comments.

--- nupic/frameworks/opf/clamodel.py
+++ nupic/frameworks/opf/clamodel.py
@@ -623,7 +623,7 @@ class CLAModel(Model):
       # Calculate the anomaly score using the active columns
       # and previous predicted columns.
       score = self._anomalyInst.computeAnomalyScore(
-          activeColumns, self.prevPredictedColumns)
+          activeColumns, self._prevPredictedColumns)
 
       # Store the predicted columns for the next timestep.
       predictedColumns = tp.getOutputData("topDownOut").nonzero()[0]
