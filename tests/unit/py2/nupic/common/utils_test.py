7b2163145ccc15c3f0e0828975956d1798b934bc
Marek Otahal <markotahal@gmail.com>
2014-09-16 11:40:42 +0200

    steal tests from utils.MovingAverage

---
+++ tests/unit/py2/nupic/common/utils_test.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# ----------------------------------------------------------------------
+# Numenta Platform for Intelligent Computing (NuPIC)
+# Copyright (C) 2013-2014, Numenta, Inc.  Unless you have an agreement
+# with Numenta, Inc., for a separate license for this software code, the
+# following terms and conditions apply:
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see http://www.gnu.org/licenses.
+#
+# http://numenta.org/licenses/
+# ----------------------------------------------------------------------
+
+"""Unit tests for utils module."""
+
+import math
+import numpy
+import unittest2 as unittest
+
+from nupic.common.utils import MovingAverage
+from nupic.support.unittesthelpers.testcasebase import TestCaseBase
+
+
+class UtilsTest(TestCaseBase):
+  """testing common.utils"""
+
+
+
+  def testMovingAverage(self):
+    """
+    Test that the (internal) moving average maintains the averages correctly,
+    even for null initial condition and when the number of values goes over
+    windowSize.  Pass in integers and floats.
+    """
+    historicalValues = []
+    total = 0
+    windowSize = 3
+    newAverage, historicalValues, total = (
+      MovingAverage.compute(historicalValues, total, 3, windowSize)
+    )
+    self.assertEqual(newAverage, 3.0)
+    self.assertEqual(historicalValues, [3.0])
+    self.assertEqual(total, 3.0)
+
+    newAverage, historicalValues, total = (
+      MovingAverage.compute(historicalValues, total, 4, windowSize)
+    )
+    self.assertEqual(newAverage, 3.5)
+    self.assertEqual(historicalValues, [3.0, 4.0])
+    self.assertEqual(total, 7.0)
+
+    newAverage, historicalValues, total = (
+      MovingAverage.compute(historicalValues, total, 5.0, windowSize)
+    )
+    self.assertEqual(newAverage, 4.0)
+    self.assertEqual(historicalValues, [3.0, 4.0, 5.0])
+    self.assertEqual(total, 12.0)
+
+    # Ensure the first value gets popped
+    newAverage, historicalValues, total = (
+      MovingAverage.compute(historicalValues, total, 6.0, windowSize)
+    )
+    self.assertEqual(newAverage, 5.0)
+    self.assertEqual(historicalValues, [4.0, 5.0, 6.0])
+    self.assertEqual(total, 15.0)
+
+
+
+if __name__ == "__main__":
+  unittest.main()
+
