b7bccbb9b6eb00c3fd85d66f9b0c6e15ddec539f
Matthew Taylor <rhyolight@gmail.com>
2014-09-22 16:29:14 -0700

    Testing use of nupic-linux64 with PIL removed.

--- ci/travis/before_install-linux.sh
+++ ci/travis/before_install-linux.sh
@@ -37,8 +37,8 @@ echo ">>> sudo apt-get install python-libtiff"
 sudo apt-get install python-libtiff
 
 echo ">>> Installing nupic-linux64..."
-git clone https://github.com/numenta/nupic-linux64.git
-(cd nupic-linux64 && git reset --hard fb9e6502993b1c4480714ad8e933145fcbb883a8)
+git clone https://github.com/rhyolight/nupic-linux64.git
+(cd nupic-linux64 && git fetch rhyolight && git checkout remove-pil rhyolight/remove-pil)
 source nupic-linux64/bin/activate
 
 # Let's see what pip packages have been installed now...
