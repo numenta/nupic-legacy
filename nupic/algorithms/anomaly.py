b5353df0190fe7b8e96f84f318711d586b1d2a4d
Marek Otahal <markotahal@gmail.com>
2014-10-08 10:10:32 +0200

    complement meaning for "anomaly likelihood mode" score
    it is an anomaly, so we won't just return likelihood code,
    but its complement:
    anomaly score= (1 - likelihood)
    
    learned data->hi likelihood->lo anomaly
    unexpected data->lo likelihood->hi anomaly

--- nupic/algorithms/anomaly.py
+++ nupic/algorithms/anomaly.py
@@ -129,14 +129,13 @@ class Anomaly(object):
     if self._mode == Anomaly.MODE_PURE:
       score = anomalyScore
     elif self._mode == Anomaly.MODE_LIKELIHOOD:
-      # TODO add tests for likelihood modes
       if inputValue is None:
         raise ValueError("Selected anomaly mode 'Anomaly.MODE_LIKELIHOOD' \
 requires 'inputValue' as parameter to compute() method. ")
 
       probability = self._likelihood.anomalyProbability(
           inputValue, anomalyScore, timestamp)
-      score = probability
+      score = 1 - probability # low likelihood -> hi anomaly
     elif self._mode == Anomaly.MODE_WEIGHTED:
       probability = self._likelihood.anomalyProbability(
           inputValue, anomalyScore, timestamp)
