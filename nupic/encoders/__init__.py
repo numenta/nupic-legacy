f9ec910d2227239791f947ca2450fc304c0a60a0
Scott Purdy <scott@fer.io>
2014-11-04 11:43:07 -0800

    Removes VectorEncoder, SimpleVectorEncoder, VectorEncoderOPF.

--- nupic/encoders/__init__.py
+++ nupic/encoders/__init__.py
@@ -32,7 +32,6 @@ from delta import DeltaEncoder
 from scalarspace import ScalarSpaceEncoder
 from coordinate import CoordinateEncoder
 from geospatial_coordinate import GeospatialCoordinateEncoder
-from nupic.encoders.vector import VectorEncoder, SimpleVectorEncoder
 from nupic.encoders.passthru import PassThruEncoder
 from nupic.encoders.extras.utility import UtilityEncoder, SimpleUtilityEncoder
 # multiencoder must be imported last because it imports * from this module!
