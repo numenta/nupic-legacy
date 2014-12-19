188ed4e90308f20dcd87f4a4204d4fd330f9625b
Matthew Taylor <matt@numenta.org>
2014-12-19 10:32:36 -0800

    Using text from numenta.com for description

--- setup.py
+++ setup.py
@@ -166,7 +166,7 @@ def setupNupic():
       "Topic :: Scientific/Engineering :: Artificial Intelligence"
     ],
     long_description = """\
-Numenta Platform for Intelligent Computing: a brain-inspired machine intelligence platform, and biologically accurate neural network based on cortical learning algorithms.
+Numenta Platform for Intelligent Computing: a machine intelligence platform that implements the HTM learning algorithms. HTM is a detailed computational theory of the neocortex. At the core of HTM are time-based continuous learning algorithms that store and recall spatial and temporal patterns. NuPIC is suited to a variety of problems, particularly anomaly detection and prediction of streaming data sources.
 
 For more information, see http://numenta.org or the NuPIC wiki at https://github.com/numenta/nupic/wiki.
 """
