9aa1eb0c812ec4219544c02b307647421989f913
Matthew Taylor <matt@numenta.org>
2014-12-30 07:53:34 -0800

    Expanded one pip call into 3 for twine/wheel dependencies. Fixed a bug in wheel filename usage.

--- ci/travis/after_success-release.sh
+++ ci/travis/after_success-release.sh
@@ -21,26 +21,33 @@
 # ----------------------------------------------------------------------
 
 echo
-echo Running after_success-release.sh...
+echo "Running after_success-release.sh..."
 echo
 
-echo Updating python modules for wheel...
-sudo pip install -U "pip==1.5.6" "setuptools>=0.9" "wheel>=0.21" || exit
-
-echo Installing twine...
+echo "Updating setuptools..."
+sudo pip install -U "setuptools>=0.9" || exit
+echo "Updating pip..."
+sudo pip install -U "pip==1.5.6" || exit
+echo "Installing wheel..."
+sudo pip install wheel || exit
+echo "Installing twine..."
 sudo pip install twine || exit
 
 # Creates wheel in dist/nupic-0.0.X-py2-none-any.whl
+echo "Creating wheel..."
 python setup.py bdist_wheel || exit
 
 generic_filename=`ls dist/*.whl`
+echo "Wheel created at ${generic_filename}."
+
 # Change the name of the wheel based on our platform if on OS X...
 if [ "${TRAVIS_OS_NAME}" = "osx" ]; then
     platform=`python -c "import distutils.util; print distutils.util.get_platform()"` || exit
     new_filename=$(echo $generic_filename | sed -e "s/none/${platform}/")
     mv $generic_filename $new_filename
+    echo "Moved wheel to ${new_filename} before ${platform} deployment."
 else
-    new_filename=generic_filename
+    new_filename="${generic_filename}"
 fi
 
 twine upload "$new_filename" -u "${PYPI_USERNAME}" -p "${PYPI_PASSWD}"
