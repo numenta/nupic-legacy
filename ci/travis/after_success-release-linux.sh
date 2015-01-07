8c7b80ad6122a3f05e1f81e5d10f64c3033405e4
Matthew Taylor <matt@numenta.org>
2015-01-07 11:32:44 -0800

    Adding wheel creating to linux build, comment cleanup

--- ci/travis/after_success-release-linux.sh
+++ ci/travis/after_success-release-linux.sh
@@ -32,10 +32,19 @@ sudo pip install twine || exit
 echo "Creating distribution files..."
 # This release build creates the source distribution. All other release builds
 # should not.
-python setup.py sdist bdist bdist_wheel || exit
+python setup.py sdist bdist || exit
 
 echo "Created the following distribution files:"
 ls -l dist
 
 echo "Attempting to upload all distribution files to PyPi..."
 twine upload dist/* -u "${PYPI_USERNAME}" -p "${PYPI_PASSWD}"
+
+# I want to create the linux wheel here just to see what gets produced with the
+# intent to later upload it to a place other than PyPi, because PyPi rejects
+# linux platform wheel files.
+# See: https://bitbucket.org/pypa/pypi-metadata-formats/issue/15/enhance-the-platform-tag-definition-for
+python setup.py bdist_wheel || exit
+echo "Created the following linux platform wheel:"
+ls dist/*.whl
+echo "Not doing anything with this wheel at this time."
