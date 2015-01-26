1bfa91fff3624637f281a0207e69afd9d046fa7a
Matthew Taylor <matt@numenta.org>
2015-01-26 11:03:37 -0800

    Add archflags env var before deploy command on OSX

--- ci/travis/after_success-release-osx.sh
+++ ci/travis/after_success-release-osx.sh
@@ -38,7 +38,7 @@ export PATH=/Users/travis/Library/Python/2.7/bin:${PATH}
 echo "Creating distribution files..."
 # We are not creating sdist here, because it's being created and uploaded in the
 # linux Travis-CI release build.
-python setup.py bdist bdist_wheel || exit
+ARCHFLAGS="-arch x86_64" python setup.py bdist bdist_wheel || exit
 
 echo "Created the following distribution files:"
 ls -l dist
