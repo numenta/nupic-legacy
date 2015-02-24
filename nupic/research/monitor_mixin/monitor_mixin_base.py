b816dff9f3c77b818da6a875d6b3343e964dbbdc
Ryan McCall <mccall.ryan@gmail.com>
2015-02-23 16:43:07 -0800

    Making base method more generic

--- nupic/research/monitor_mixin/monitor_mixin_base.py
+++ nupic/research/monitor_mixin/monitor_mixin_base.py
@@ -138,29 +138,31 @@ class MonitorMixinBase(object):
     return []
 
 
-  def mmGetCellActivityPlot(self, cellTrace, activityType, title="",
-                            showReset=False, resetShading=0.25):
-    """ Returns plot of the cell activity.
+  def mmGetCellActivityPlot(self, cellTrace, cellCount, activityType,
+                            title="", showReset=False, resetShading=0.25):
+    """ Returns plot of the cell activity. Note that if many timesteps of
+    activities are input, matplotlib's image interpolation may omit activities
+    (columns in the image).
 
-    @param cellTrace a list of cell activities
+    @param cellTrace (list) a temporally ordered list of sets of cell activities
 
-    @param activityType a label for the type of cell activity
+    @param cellCount (int) number of cells in the space being rendered
 
-    @param activityType The type of cell activity displayed.
+    @param activityType (string) type of cell activity being displayed
 
-    @param title an optional title for the figure
+    @param title (string) an optional title for the figure
 
-    @param showReset if true, the first set of cell activities after a reset
-                        will have a gray background
+    @param showReset (boolean) if true, the first set of cell activities
+                               after a reset will have a grayscale background
 
-    @param resetShading If showReset is true, this float specifies the
-    intensity of the reset background with 0.0 being white and 1.0 being black
+    @param resetShading (float) applicable if showReset is true, specifies the
+                                intensity of the reset background with 0.0
+                                being white and 1.0 being black
 
     @return (Plot) plot
     """
     plot = Plot(self, title)
     resetTrace = self.mmGetTraceResets().data
-    cellCount = self.numberOfCells()
     data = numpy.zeros((cellCount, 1))
     for i in xrange(len(cellTrace)):
       # Set up a "background" vector that is shaded or blank
