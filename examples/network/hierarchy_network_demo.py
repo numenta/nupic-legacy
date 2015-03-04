d80936b05c0674ebf0bead718f8a438148b47d74
Matthew Taylor <matt@numenta.org>
2015-03-04 11:12:02 -0800

    Front-loading calls to resource_filename when possible.

--- examples/network/hierarchy_network_demo.py
+++ examples/network/hierarchy_network_demo.py
@@ -46,7 +46,9 @@ _VERBOSITY = 0
 
 # Seed used for random number generation
 _SEED = 2045
-_INPUT_FILE_PATH = "extra/hotgym/rec-center-hourly.csv"
+_INPUT_FILE_PATH = resource_filename(
+  "examples.prediction.data", "extra/hotgym/rec-center-hourly.csv"
+)
 _OUTPUT_FILE_NAME = "hierarchy-demo-output.csv"
 
 # Parameter dict for SPRegion
@@ -348,8 +350,7 @@ def runNetwork(network, numRecords, writer):
 
 
 def runDemo():
-  trainFile = resource_filename("examples.prediction.data", _INPUT_FILE_PATH)
-  dataSource = FileRecordStream(streamID=trainFile)
+  dataSource = FileRecordStream(streamID=_INPUT_FILE_PATH)
   numRecords = dataSource.getDataRowCount()
   print "Creating network"
   network = createNetwork(dataSource)
