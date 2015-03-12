bb65e30e078138d2f17e6dd1a40e979c2e26a5c3
Austin Marshall <oxtopus@gmail.com>
2015-03-12 12:58:21 -0700

    Shift bulk of run_opf_experiment.py into experiment_runner.py for cli-friendly python module.  This lets you run an experiment by invoking `python -m nupic.frameworks.opf.experiment_runner <path to experiment>` with only the binary installation and without needing access to scripts/run_opf_experiment.py (not included in binary).
    Meanwhile, this maintains existing functionality of run_opf_experiment.py for non-binary installations.
    
    Fixes #1921

--- nupic/frameworks/opf/experiment_runner.py
+++ nupic/frameworks/opf/experiment_runner.py
@@ -41,6 +41,8 @@ from nupic.frameworks.opf.modelfactory import ModelFactory
 from nupic.frameworks.opf.opftaskdriver import OPFTaskDriver
 from nupic.frameworks.opf.opfutils import (InferenceElement, matchPatterns,
                                            validateOpfJsonValue)
+from nupic.support import initLogging
+
 
 
 g_defaultCheckpointExtension = ".nta"
@@ -833,3 +835,28 @@ class PeriodicActivityMgr(object):
           act.iteratorHolder[0] = None
 
     return True
+
+
+
+def main():
+  """ Module-level entry point.  Run according to options in sys.argv
+
+  Usage: python -m python -m nupic.frameworks.opf.experiment_runner
+
+  """
+  initLogging(verbose=True)
+
+  # Initialize pseudo-random number generators (PRNGs)
+  #
+  # This will fix the seed that is used by numpy when generating 'random'
+  # numbers. This allows for repeatability across experiments.
+  initExperimentPrng()
+
+  # Run it!
+  runExperiment(sys.argv[1:])
+
+
+
+if __name__ == "__main__":
+  main()
+
