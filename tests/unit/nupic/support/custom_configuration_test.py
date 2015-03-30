ebf3de464ad66d3ff885189750b90c473bb8a5bf
Pascal Weinberger <passiweinberger@gmail.com>
2015-03-30 12:53:53 +0200

    Remove NUPIC dependency

--- tests/unit/nupic/support/custom_configuration_test.py
+++ tests/unit/nupic/support/custom_configuration_test.py
@@ -803,8 +803,7 @@ class ConfigurationCustomTest(unittest.TestCase):
     configuration.Configuration._configPaths = None  # pylint: disable=W0212
     result = configuration.Configuration.getConfigPaths()
     self.assertTrue(isinstance(result, list))
-    self.assertEqual(result, [os.path.join(os.environ['NUPIC'],
-                                           'config', 'default')])
+    self.assertEqual(result, [os.path.dirname('nupic/config/default/')])
 
   @patch.object(configuration.Configuration, '_configPaths',
                 spec=configuration.Configuration._configPaths)
