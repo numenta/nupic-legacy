4eb3c2faa9fd631ac6c69b897e9be8821325561b
Marek Otahal <markotahal@gmail.com>
2015-04-21 10:18:59 +0200

    TP profile: can use args from command-line, random data used
    each time in compute method, this measures the worst case scenario

--- tp_large.py
+++ tp_large.py
@@ -1,7 +1,7 @@
-#! /usr/bin/env python
+#!/usr/bin/env python
 # ----------------------------------------------------------------------
 # Numenta Platform for Intelligent Computing (NuPIC)
-# Copyright (C) 2013, Numenta, Inc.  Unless you have an agreement
+# Copyright (C) 2015, Numenta, Inc.  Unless you have an agreement
 # with Numenta, Inc., for a separate license for this software code, the
 # following terms and conditions apply:
 #
@@ -20,8 +20,9 @@
 # http://numenta.org/licenses/
 # ----------------------------------------------------------------------
 
-## run python -m cProfile --sort cumtime tp_large.py 
+## run python -m cProfile --sort cumtime tp_large.py [nColumns nEpochs]
 
+import sys
 import numpy
 # chose desired TP implementation to compare:
 from nupic.research.TP10X2 import TP10X2 as CppTP 
@@ -32,14 +33,27 @@ def profileTP(tpClass, tpDim, nRuns):
   """Checks that feeding in the same input vector leads to polarized
   permanence values: either zeros or ones, but no fractions"""
 
+  # create TP instance to measure
   tp = tpClass(numberOfCols=tpDim)
 
+  # generate input data; if used only this - it's the easiest scenario (const data)
   data = numpy.random.randint(0, 2, tpDim).astype('float32')
 
   for _ in xrange(nRuns):
+    # new data every time, this is the worst case performance
+    # real performance would be better, as the input data would not be completely random
+    data = numpy.random.randint(0, 2, tpDim).astype('float32') # time spent for this can be ignored
+
+    # the actual function to profile!
     tp.compute(data, True)
 
 
 
 if __name__ == "__main__":
-  profileTP(CppTP, 2048, 10000)
+  columns=2048
+  epochs=10000
+  if len(sys.argv) == 3: # 2 args + name
+    columns=int(sys.argv[1])
+    epochs=int(sys.argv[2])
+
+  profileTP(CppTP, columns, epochs)
