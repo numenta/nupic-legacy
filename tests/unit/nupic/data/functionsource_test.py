2e049e6c5083891ceb61ea1854fe0cb09a97c2da
Frank Leon Rose <frankleonrose@gmail.com>
2015-04-22 21:53:50 -0400

    Tweaks to match style guidelines

--- tests/unit/nupic/data/functionsource_test.py
+++ tests/unit/nupic/data/functionsource_test.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # ----------------------------------------------------------------------
 # Numenta Platform for Intelligent Computing (NuPIC)
-# Copyright (C) 2013, Numenta, Inc.  Unless you have an agreement
+# Copyright (C) 2015, Numenta, Inc.  Unless you have an agreement
 # with Numenta, Inc., for a separate license for this software code, the
 # following terms and conditions apply:
 #
@@ -24,46 +24,59 @@
 Unit tests for functionsource.
 """
 
-import unittest2 as unittest
 import pickle
+import unittest
 
 from nupic.data import FunctionSource
+
+
 
 def dataFunction(stat):
   ret = {"reset": 0, "sequence": 0, "data": 0}
   if stat is not None:
-    val = stat.get('val', 0) + 1
+    val = stat.get("val", 0) + 1
     ret["val"] = stat["val"] = val
   return ret
 
 class FunctionSourceTest(unittest.TestCase):
-  def testFunctionSource(self):
-    fs = FunctionSource(dataFunction, state=None, resetFieldName=None, sequenceIdFieldName=None)
+
+
+  def testDefaultArgs(self):
+    fs = FunctionSource(dataFunction, state=None, resetFieldName=None,
+                        sequenceIdFieldName=None)
     self.assertIsNotNone(fs)
     r = fs.getNextRecordDict()
     self.assertIsNotNone(r)
 
-  def testFunctionSourceReset(self):
-    fs = FunctionSource(dataFunction, state=None, resetFieldName="reset", sequenceIdFieldName=None)
+
+  def testResetField(self):
+    fs = FunctionSource(dataFunction, state=None, resetFieldName="reset",
+                        sequenceIdFieldName=None)
     self.assertIsNotNone(fs)
     r = fs.getNextRecordDict()
     self.assertIsNotNone(r)
-      
-  def testFunctionSourceSequence(self):
-    fs = FunctionSource(dataFunction, state=None, resetFieldName=None, sequenceIdFieldName="sequence")
+
+
+  def testSequenceField(self):
+    fs = FunctionSource(dataFunction, state=None, resetFieldName=None,
+                        sequenceIdFieldName="sequence")
     self.assertIsNotNone(fs)
     r = fs.getNextRecordDict()
     self.assertIsNotNone(r)
-      
-  def testFunctionSourceResetAndSequence(self):
-    fs = FunctionSource(dataFunction, state=None, resetFieldName="reset", sequenceIdFieldName="sequence")
+
+
+  def testResetAndSequenceFields(self):
+    fs = FunctionSource(dataFunction, state=None, resetFieldName="reset",
+                        sequenceIdFieldName="sequence")
     self.assertIsNotNone(fs)
     r = fs.getNextRecordDict()
     self.assertIsNotNone(r)
-      
-  def testFunctionSourceState(self):
+
+
+  def testState(self):
     state = dict(val=100)
-    fs = FunctionSource(dataFunction, state=state, resetFieldName="reset", sequenceIdFieldName="sequence")
+    fs = FunctionSource(dataFunction, state=state, resetFieldName="reset",
+                        sequenceIdFieldName="sequence")
     self.assertIsNotNone(fs)
     r = fs.getNextRecordDict()
     self.assertIsNotNone(r)
@@ -71,20 +84,26 @@ class FunctionSourceTest(unittest.TestCase):
     r = fs.getNextRecordDict()
     self.assertEqual(103, state["val"])
 
-  def testFunctionSourcePickle(self):
+
+  def testPickle(self):
     state = dict(val=100)
-    fs = FunctionSource(dataFunction, state=state, resetFieldName="reset", sequenceIdFieldName="sequence")
+    fs = FunctionSource(dataFunction, state=state, resetFieldName="reset",
+                        sequenceIdFieldName="sequence")
     self.assertIsNotNone(fs)
-    r = fs.getNextRecordDict()    
+    r = fs.getNextRecordDict()
     self.assertIsNotNone(r)
-    
+
     pkl = pickle.dumps(fs)
-    
+    self.assertIsNotNone(pkl)
+
     fs2 = pickle.loads(pkl)
-    
+    self.assertIsNotNone(fs2)
+
     r = fs2.getNextRecordDict()
     r = fs2.getNextRecordDict()
     self.assertEqual(103, fs2.state["val"])
+
+
 
 if __name__ == "__main__":
   unittest.main()
