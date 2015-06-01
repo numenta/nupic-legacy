b4826f34b7149d2f79d5bc6e2c1ca0410c717e37
akhilaananthram <asa225@cornell.edu>
2015-06-01 15:32:09 -0700

    fixed a typo in the example

--- examples/network/custom_region/custom_region_demo.py
+++ examples/network/custom_region/custom_region_demo.py
@@ -1,4 +1,4 @@
-#!/usr/bin/encustom_regionv python
+#!/usr/bin/env python
 # ----------------------------------------------------------------------
 # Numenta Platform for Intelligent Computing (NuPIC)
 # Copyright (C) 2013, Numenta, Inc.  Unless you have an agreement
