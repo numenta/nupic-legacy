208e95bb9c40bf49fa79003e9467a47032f51903
akhilaananthram <asa225@cornell.edu>
2015-06-12 15:40:36 -0700

    pass a pointer to a class to register

--- extensions/cpp_region/PyRegion.cpp
+++ extensions/cpp_region/PyRegion.cpp
@@ -156,6 +156,7 @@ extern "C"
   {
     try
     {
+      std::cout << "NTA_createSpec" << std::endl;
       return PyRegion::createSpec(nodeType, className);
     }
     catch (nupic::Exception & e)
@@ -201,6 +202,7 @@ Spec * PyRegion::createSpec(const char * nodeType, const char* className)
   // return the exisiting one from the map.
   std::string name(nodeType);
   std::string realClassName(className);
+  std::cout << nodeType << std::endl;
   if (!name.empty() && !realClassName.empty())
     name = name + "." + realClassName;
 
@@ -878,6 +880,7 @@ void PyRegion::createSpec(const char * nodeType, Spec & ns, const char* classNam
   std::string realClassName(className);
   if (realClassName.empty())
     realClassName = Path::getExtension(nodeType);
+  std::string << realClassName << std::endl;
 
   py::Class nodeClass(nodeType, realClassName);
 
