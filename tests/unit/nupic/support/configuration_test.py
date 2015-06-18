1ed1749c3ea98cd4f83aaff9a7a890738b2ecfcd
Pascal Weinberger <passiweinberger@gmail.com>
2015-06-18 21:37:03 +0200

    Update configuration_test.py

--- tests/unit/nupic/support/configuration_test.py
+++ tests/unit/nupic/support/configuration_test.py
@@ -520,7 +520,8 @@ class ConfigurationTest(unittest.TestCase):
     configuration.Configuration._configPaths = None  # pylint: disable=W0212
     result = configuration.Configuration.getConfigPaths()
     self.assertTrue(isinstance(result, list))
-    self.assertEqual(result, [os.path.join(resource_filename("nupic", ""), "config", "default")])
+    self.assertListEqual(result, [resource_filename("nupic", os.path.join("config",
+                                                                    "default"))])
 
   @patch.object(configuration.Configuration, '_configPaths',
                 spec=configuration.Configuration._configPaths)
