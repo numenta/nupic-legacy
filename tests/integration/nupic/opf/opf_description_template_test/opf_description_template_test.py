2ac32d4ab1d41ca96674be2033cdc12fbbf62b5a
Pascal Weinberger <passiweinberger@gmail.com>
2015-06-18 15:25:42 +0200

    Update opf_description_template_test.py

--- tests/integration/nupic/opf/opf_description_template_test/opf_description_template_test.py
+++ tests/integration/nupic/opf/opf_description_template_test/opf_description_template_test.py
@@ -49,9 +49,7 @@ class MyTestEnvironment(object):
 
   def __init__(self):
 
-    nupicDir = resource_filename("nupic", "")
-
-    examplesDir = os.path.join(nupicDir, "examples")
+    examplesDir = resource_filename("nupic", "../examples")
 
     _debugOut("examplesDir=<%s>" % (examplesDir,))
 
@@ -60,7 +58,7 @@ class MyTestEnvironment(object):
 
     # This is where we find OPF binaries (e.g., run_opf_experiment.py, etc.)
     # In the autobuild, it is a read-only directory
-    self.__opfBinDir = os.path.join(nupicDir, "scripts")
+    self.__opfBinDir = resource_filename("nupic", "../scripts")
     assert os.path.exists(self.__opfBinDir), \
            "%s is not present in filesystem" % self.__opfBinDir
     _debugOut("self.__opfBinDir=<%s>" % self.__opfBinDir)
