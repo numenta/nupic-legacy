619ff8916a7c49baadb0f9cc27c1d492025d8dba
Pascal Weinberger <passiweinberger@gmail.com>
2015-06-18 21:25:36 +0200

    Update swarming_test.py

--- tests/swarming/nupic/swarming/swarming_test.py
+++ tests/swarming/nupic/swarming/swarming_test.py
@@ -257,7 +257,7 @@ class ExperimentTestBaseClass(HelperTestCaseBase):
 
       # Form the stream definition
       if dataPath is None:
-        dataPath = os.path.join(resource_filename("nupic.data", ""), "extra", "qa", "hotgym", "qa_hotgym.csv")
+        dataPath = resource_filename("nupic.data", os.path.join( "extra", "qa", "hotgym", "qa_hotgym.csv"))
         
       streamDef = dict(
         version = 1,
