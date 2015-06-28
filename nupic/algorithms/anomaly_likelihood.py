5dbd3fed1cac7f878df29aba5c5a42153fedae34
Marek Otahal <markotahal@gmail.com>
2015-06-28 16:12:48 +0200

    Likelihood: @param docstring

--- nupic/algorithms/anomaly_likelihood.py
+++ nupic/algorithms/anomaly_likelihood.py
@@ -138,12 +138,14 @@ class AnomalyLikelihood(object):
 
   def anomalyProbability(self, value, anomalyScore, timestamp=None):
     """
-    Return the probability that the current value plus anomaly score represents
+    Compute the probability that the current value plus anomaly score represents
     an anomaly given the historical distribution of anomaly scores. The closer
     the number is to 1, the higher the chance it is an anomaly.
 
-    Given the current metric value, plus the current anomaly score, output the
-    anomalyLikelihood for this record.
+    @param value - the current metric ("raw") value, eg. "orange", or '21.2' (deg. Celsius), ...
+    @param anomalyScore - the current anomaly score
+    @param timestamp - (optional) timestamp of the ocurrence, default (None) results in datetime.now()
+    @return theanomalyLikelihood for this record.
     """
     if timestamp is None:
       timestamp = datetime.datetime.now()
