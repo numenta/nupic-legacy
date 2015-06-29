a3c8c157765059131b5bea93d76ecd6984a83955
Yuwei <ywcui1990@gmail.com>
2015-06-29 15:23:01 -0700

    Fixes bug in mmGetCellActivityPlot

--- nupic/research/monitor_mixin/temporal_memory_monitor_mixin.py
+++ nupic/research/monitor_mixin/temporal_memory_monitor_mixin.py
@@ -421,7 +421,7 @@ class TemporalMemoryMonitorMixin(MonitorMixinBase):
     # If the trace contains ConnectionsCell, convert them to int
     cellTrace = self._mmTraces[activityType].data
     for i in xrange(len(cellTrace)):
-        cellTrace[i] = self.getCellIndices(cellTrace[i])
+      cellTrace[i] = self.getCellIndices(cellTrace[i])
 
     return self.mmGetCellTracePlot(cellTrace, self.numberOfCells(),
                                    activityType, title, showReset, resetShading)
