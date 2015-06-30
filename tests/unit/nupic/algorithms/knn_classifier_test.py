76562d59da02836e2f3ee9b65f01f122019fa143
mihail911 <mihail911@gmail.com>
2015-06-30 15:20:25 -0700

    added appropriate error messages to asserts

--- tests/unit/nupic/algorithms/knn_classifier_test.py
+++ tests/unit/nupic/algorithms/knn_classifier_test.py
@@ -40,20 +40,24 @@ class KNNClassifierTest(unittest.TestCase):
     b = np.array([2, 4, 8, 12, 14, 18, 20, 28, 30], dtype=np.int32)
 
     numPatterns = classifier.learn(a, 0, isSparse=dimensionality)
-    self.assertEquals(numPatterns, 1)
+    self.assertEquals(numPatterns, 1, "Number of patterns learned does "
+                                      "not match what is expected")
 
     numPatterns = classifier.learn(b, 1, isSparse=dimensionality)
-    self.assertEquals(numPatterns, 2)
+    self.assertEquals(numPatterns, 2, "Number of patterns learned does "
+                                      "not match what is expected")
 
     denseA = np.zeros(dimensionality)
     denseA[a] = 1.0
     cat, _, _, _ = classifier.infer(denseA)
-    self.assertEquals(cat, 0)
+    self.assertEquals(cat, 0, "Categories inferred do not match what"
+                              " is expected")
 
     denseB = np.zeros(dimensionality)
     denseB[b] = 1.0
     cat, _, _, _ = classifier.infer(denseB)
-    self.assertEquals(cat, 1)
+    self.assertEquals(cat, 1, "Categories inferred do not match what"
+                              " is expected")
 
 
   def testOverlapDistanceMethodBadSparsity(self):
@@ -65,7 +69,8 @@ class KNNClassifierTest(unittest.TestCase):
     a = np.array([1, 3, 7, 11, 13, 17, 19, 23, 29], dtype=np.int32)
 
     # Learn with incorrect dimensionality, less than some bits (23, 29)
-    with self.assertRaises(RuntimeError):
+    with self.assertRaises(RuntimeError, "Learning with bits that don't match"
+                                         "dimensionality"):
       classifier.learn(a, 0, isSparse=20)
 
 
@@ -80,13 +85,15 @@ class KNNClassifierTest(unittest.TestCase):
     # Learn with incorrect dimensionality, greater than largest ON bit, but
     # inconsistent when inferring
     numPatterns = classifier.learn(a, 0, isSparse=31)
-    self.assertEquals(numPatterns, 1)
+    self.assertEquals(numPatterns, 1, "Learning with bits that don't match"
+                                      "dimensionality")
 
     denseA = np.zeros(dimensionality)
     denseA[a] = 1.0
 
     cat, _, _, _ = classifier.infer(denseA)
-    self.assertEquals(cat, 0)
+    self.assertEquals(cat, 0, "Categories inferred do not match what"
+                              " is expected")
 
 
 
@@ -99,10 +106,10 @@ class KNNClassifierTest(unittest.TestCase):
     a = np.array([29, 3, 7, 11, 13, 17, 19, 23, 1], dtype=np.int32)
     b = np.array([2, 4, 20, 12, 14, 18, 8, 28, 30], dtype=np.int32)
 
-    with self.assertRaises(RuntimeError):
+    with self.assertRaises(RuntimeError, "Sparse representation is unsorted"):
       classifier.learn(a, 0, isSparse=dimensionality)
 
-    with self.assertRaises(RuntimeError):
+    with self.assertRaises(RuntimeError, "Sparse representation is unsorted"):
       classifier.learn(b, 1, isSparse=dimensionality)
 
 
@@ -115,12 +122,14 @@ class KNNClassifierTest(unittest.TestCase):
     a = np.array([], dtype=np.int32)
 
     numPatterns = classifier.learn(a, 0, isSparse=dimensionality)
-    self.assertEquals(numPatterns, 1)
+    self.assertEquals(numPatterns, 1, "Number of patterns learned does "
+                                      "not match what is expected")
 
     denseA = np.zeros(dimensionality)
     denseA[a] = 1.0
     cat, _, _, _ = classifier.infer(denseA)
-    self.assertEquals(cat, 0)
+    self.assertEquals(cat, 0, "Categories inferred do not match what"
+                              " is expected")
 
   def testMultipleReturnedLabelsSimple(self):
     """Simple tests for whether 'infer' properly returns top n most frequent labels."""
@@ -133,31 +142,37 @@ class KNNClassifierTest(unittest.TestCase):
     c = np.array([0,2], dtype=np.int32)
 
     numPatterns = classifier.learn(a, 0, isSparse=dimensionality)
-    self.assertEquals(numPatterns, 1)
+    self.assertEquals(numPatterns, 1, "Number of patterns learned does "
+                                      "not match what is expected")
 
     numPatterns = classifier.learn(b, 1, isSparse=dimensionality)
-    self.assertEquals(numPatterns, 2)
+    self.assertEquals(numPatterns, 2, "Number of patterns learned does "
+                                      "not match what is expected")
 
     numPatterns = classifier.learn(c, 2, isSparse=dimensionality)
-    self.assertEquals(numPatterns, 3)
+    self.assertEquals(numPatterns, 3, "Number of patterns learned does "
+                                      "not match what is expected")
 
     testA = np.zeros(dimensionality, dtype=np.float64)
     testA[a] = 1.0
 
     catA, _, _, _ = classifier.infer(testA, numWinners=2)
-    self.assertFalse(np.any(catA - np.array([0, 2])))
+    self.assertFalse(np.any(catA - np.array([0, 2])), "Categories inferred "
+                              "do not match what is expected")
 
     testB = np.zeros(dimensionality, dtype=np.float64)
     testB[b] = 1.0
 
     catB, _, _, _ = classifier.infer(testB, numWinners=2)
-    self.assertFalse(np.any(catB - np.array([1, 2])))
+    self.assertFalse(np.any(catB - np.array([1, 2])), "Categories inferred "
+                              "do not match what is expected")
 
     testC = np.zeros(dimensionality, dtype=np.float64)
     testC[c] = 1.0
 
     catC, _, _, _ = classifier.infer(testC, numWinners=2)
-    self.assertFalse(np.any(catC - np.array([2, 1])))
+    self.assertFalse(np.any(catC - np.array([2, 1])), "Categories inferred "
+                              "do not match what is expected")
 
   def testMultipleReturnedLabelsComplex(self):
     """More complicated tests for whether 'infer' properly returns top n most
@@ -171,31 +186,37 @@ class KNNClassifierTest(unittest.TestCase):
     c = np.array([0,2], dtype=np.int32)
 
     numPatterns = classifier.learn(a, 0, isSparse=dimensionality)
-    self.assertEquals(numPatterns, 1)
+    self.assertEquals(numPatterns, 1, "Number of patterns learned does "
+                                      "not match what is expected")
 
     numPatterns = classifier.learn(b, 0, isSparse=dimensionality)
-    self.assertEquals(numPatterns, 2)
+    self.assertEquals(numPatterns, 2, "Number of patterns learned does "
+                                      "not match what is expected")
 
     numPatterns = classifier.learn(c, 1, isSparse=dimensionality)
-    self.assertEquals(numPatterns, 3)
+    self.assertEquals(numPatterns, 3, "Number of patterns learned does "
+                                      "not match what is expected")
 
     testA = np.zeros(dimensionality, dtype=np.float64)
     testA[a] = 1.0
 
     catA, _, _, _ = classifier.infer(testA, numWinners=2)
-    self.assertFalse(np.any(catA - np.array([0, 1])))
+    self.assertFalse(np.any(catA - np.array([0, 1])), "Categories inferred "
+                              "do not match what is expected")
 
     testB = np.zeros(dimensionality, dtype=np.float64)
     testB[b] = 1.0
 
     catB, _, _, _ = classifier.infer(testB, numWinners=2)
-    self.assertFalse(np.any(catB - np.array([0, 1])))
+    self.assertFalse(np.any(catB - np.array([0, 1])), "Categories inferred "
+                              "do not match what is expected")
 
     testC = np.zeros(dimensionality, dtype=np.float64)
     testC[c] = 1.0
 
     catC, _, _, _ = classifier.infer(testC, numWinners=2)
-    self.assertFalse(np.any(catC - np.array([0, 1])))
+    self.assertFalse(np.any(catC - np.array([0, 1])), "Categories inferred "
+                              "do not match what is expected")
 
   def testMultipleReturnedLabelsMoreComplex(self):
     """More complicated tests for whether 'infer' properly returns top n most
@@ -210,34 +231,41 @@ class KNNClassifierTest(unittest.TestCase):
     d = np.array([0,3], dtype=np.int32)
 
     numPatterns = classifier.learn(a, 0, isSparse=dimensionality)
-    self.assertEquals(numPatterns, 1)
+    self.assertEquals(numPatterns, 1, "Number of patterns learned does "
+                                      "not match what is expected")
 
     numPatterns = classifier.learn(b, 0, isSparse=dimensionality)
-    self.assertEquals(numPatterns, 2)
+    self.assertEquals(numPatterns, 2, "Number of patterns learned does "
+                                      "not match what is expected")
 
     numPatterns = classifier.learn(c, 1, isSparse=dimensionality)
-    self.assertEquals(numPatterns, 3)
+    self.assertEquals(numPatterns, 3, "Number of patterns learned does "
+                                      "not match what is expected")
 
     numPatterns = classifier.learn(d, 1, isSparse=dimensionality)
-    self.assertEquals(numPatterns, 4)
+    self.assertEquals(numPatterns, 4, "Number of patterns learned does "
+                                      "not match what is expected")
 
     testA = np.zeros(dimensionality, dtype=np.float64)
     testA[a] = 1.0
 
     catA, _, _, _ = classifier.infer(testA, numWinners=2)
-    self.assertFalse(np.any(catA - np.array([1, 0])))
+    self.assertFalse(np.any(catA - np.array([1, 0])), "Categories inferred "
+                              "do not match what is expected")
 
     testB = np.zeros(dimensionality, dtype=np.float64)
     testB[b] = 1.0
 
     catB, _, _, _ = classifier.infer(testB, numWinners=2)
-    self.assertFalse(np.any(catB - np.array([0, 1])))
+    self.assertFalse(np.any(catB - np.array([0, 1])), "Categories inferred "
+                              "do not match what is expected")
 
     testD = np.zeros(dimensionality, dtype=np.float64)
     testD[d] = 1.0
 
     catD, _, _, _ = classifier.infer(testD, numWinners=2)
-    self.assertFalse(np.any(catD - np.array([0, 1])))
+    self.assertFalse(np.any(catD - np.array([0, 1])), "Categories inferred "
+                              "do not match what is expected")
 
   @unittest.skip("Finish when infer has options for sparse and dense "
                  "https://github.com/numenta/nupic/issues/2198")
