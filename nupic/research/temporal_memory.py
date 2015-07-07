308fd402feefcd58315ebdbc6656ca822597afb6
Yuwei <ywcui1990@gmail.com>
2015-07-07 15:25:55 -0700

    Change default value of predictedSegmentDecrement to be 0 to be backward compatible

--- nupic/research/temporal_memory.py
+++ nupic/research/temporal_memory.py
@@ -846,7 +846,4 @@ class TemporalMemory(object):
 
   @staticmethod
   def getCellIndex(cell):
-    if not isinstance(cell, int):
-      return cell.idx
-    else:
-      return cell
+    return cell
