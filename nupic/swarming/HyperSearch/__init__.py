24285c860f30e75edb526cfc9e893b363bb1a28a
mihail911 <mihail911@gmail.com>
2015-07-27 15:56:09 -0700

    reverting first_order and nupic-default.xml changes

--- nupic/swarming/HyperSearch/__init__.py
+++ nupic/swarming/HyperSearch/__init__.py
@@ -1,6 +1,6 @@
 # ----------------------------------------------------------------------
 # Numenta Platform for Intelligent Computing (NuPIC)
-# Copyright (C) 2013, Numenta, Inc.  Unless you have an agreement
+# Copyright (C) 2015, Numenta, Inc.  Unless you have an agreement
 # with Numenta, Inc., for a separate license for this software code, the
 # following terms and conditions apply:
 #
