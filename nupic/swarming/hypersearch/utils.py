0340038e47ff8c5fa59ad13ef02486e168b9f96a
mihail911 <mihail911@gmail.com>
2015-08-12 09:14:26 -0700

    copied some json utils into utils.py

--- nupic/swarming/hypersearch/utils.py
+++ nupic/swarming/hypersearch/utils.py
@@ -20,6 +20,7 @@
 # ----------------------------------------------------------------------
 
 import copy
+import json
 import os
 import sys
 import tempfile
@@ -33,6 +34,7 @@ import shutil
 import types
 import signal
 import uuid
+import validictory
 
 from nupic.database.ClientJobsDAO import (
     ClientJobsDAO, InvalidConnectionException)
@@ -663,3 +665,59 @@ def clippedObj(obj, maxElementSize=64):
       objOut = objOut[0:maxElementSize] + '...'
 
   return objOut
+
+
+class ValidationError(validictory.ValidationError):
+  pass
+
+def validate(value, **kwds):
+  """ Validate a python value against json schema:
+  validate(value, schemaPath)
+  validate(value, schemaDict)
+
+  value:          python object to validate against the schema
+
+  The json schema may be specified either as a path of the file containing
+  the json schema or as a python dictionary using one of the
+  following keywords as arguments:
+    schemaPath:     Path of file containing the json schema object.
+    schemaDict:     Python dictionary containing the json schema object
+
+  Returns: nothing
+
+  Raises:
+          ValidationError when value fails json validation
+  """
+
+  assert len(kwds.keys()) >= 1
+  assert 'schemaPath' in kwds or 'schemaDict' in kwds
+
+  schemaDict = None
+  if 'schemaPath' in kwds:
+    schemaPath = kwds.pop('schemaPath')
+    schemaDict = loadJsonValueFromFile(schemaPath)
+  elif 'schemaDict' in kwds:
+    schemaDict = kwds.pop('schemaDict')
+
+  try:
+    validictory.validate(value, schemaDict, **kwds)
+  except validictory.ValidationError as e:
+    raise ValidationError(e)
+
+
+
+def loadJsonValueFromFile(inputFilePath):
+  """ Loads a json value from a file and converts it to the corresponding python
+  object.
+
+  inputFilePath:
+                  Path of the json file;
+
+  Returns:
+                  python value that represents the loaded json value
+
+  """
+  with open(inputFilePath) as fileObj:
+    value = json.load(fileObj)
+
+  return value
