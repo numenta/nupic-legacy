829d8f6a407c2de6b8a9658fdb0e1c59a85a7162
akhilaananthram <asa225@cornell.edu>
2015-08-17 13:48:06 -0700

    core/build/release

--- ci/travis/before_deploy.sh
+++ ci/travis/before_deploy.sh
@@ -28,7 +28,7 @@ cd ${TRAVIS_BUILD_DIR}
 
 # If this branch is master, this is an iterative deployment, so we'll package
 # wheels ourselves for deployment to S3. No need to build docs.
-if [ "${TRAVIS_BRANCH}" = "swig" ]; then
+if [ "${TRAVIS_BRANCH}" = "master" ]; then
     # There are now a bunch of symlinks in ${TRAVIS_BUILD_DIR}/extensions that
     # need to be converted to real files. We will do this with a tar hack.
     echo "Removing symlinks from extensions..."
@@ -44,8 +44,8 @@ if [ "${TRAVIS_BRANCH}" = "swig" ]; then
     # Build all NuPIC and all required python packages into dist/wheels as .whl
     # files.
     echo "pip wheel --wheel-dir=dist/wheels . --find-links=extensions/core/build/release"
-    pip wheel --wheel-dir=dist/wheels -r extensions/core/requirements.txt
-    wheel convert extensions/core/*.egg --dest-dir=dist/wheels
+    pip wheel --wheel-dir=dist/wheels -r extensions/core/build/release/requirements.txt
+    wheel convert extensions/core/build/release/*.egg --dest-dir=dist/wheels
     pip wheel --wheel-dir=dist/wheels -r external/common/requirements.txt --find-links=dist/wheels
     python setup.py bdist_wheel -d dist/wheels
     python setup.py bdist_egg -d dist
