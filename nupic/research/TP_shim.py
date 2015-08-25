3895dbd82842fbb5b76865a23a1975ff9e4afaf9
Chetan Surpur <chetan51@gmail.com>
2015-08-25 14:27:23 -0700

    Allow specifying MonitoredTemporalMemory as TM implementation through OPF

--- nupic/research/TP_shim.py
+++ nupic/research/TP_shim.py
@@ -27,6 +27,9 @@ for use with OPF.
 import numpy
 
 from nupic.research.temporal_memory import TemporalMemory
+from nupic.research.monitor_mixin.temporal_memory_monitor_mixin import (
+  TemporalMemoryMonitorMixin)
+class MonitoredTemporalMemory(TemporalMemoryMonitorMixin, TemporalMemory): pass
 
 
 
@@ -96,6 +99,83 @@ class TPShim(TemporalMemory):
     return activeState
   
   
+  def getPredictedState(self):
+    predictedState = numpy.zeros(self.numberOfCells())
+    predictedState[self.getCellIndices(self.predictiveCells)] = 1
+    return predictedState
+
+
+
+class MonitoredTPShim(MonitoredTemporalMemory):
+  """
+  TP => Monitored Temporal Memory shim class.
+
+  TODO: This class is not very DRY. This whole file needs to be replaced by a
+  pure TemporalMemory region
+  (WIP at https://github.com/numenta/nupic.research/pull/247).
+  """
+  def __init__(self,
+               numberOfCols=500,
+               cellsPerColumn=10,
+               initialPerm=0.11,
+               connectedPerm=0.50,
+               minThreshold=8,
+               newSynapseCount=15,
+               permanenceInc=0.10,
+               permanenceDec=0.10,
+               permanenceMax=1.0,
+               globalDecay=0.10,
+               activationThreshold=12,
+               seed=42):
+    """
+    Translate parameters and initialize member variables specific to `TP.py`.
+    """
+    super(MonitoredTPShim, self).__init__(
+      columnDimensions=(numberOfCols,),
+      cellsPerColumn=cellsPerColumn,
+      activationThreshold=activationThreshold,
+      initialPermanence=initialPerm,
+      connectedPermanence=connectedPerm,
+      minThreshold=minThreshold,
+      maxNewSynapseCount=newSynapseCount,
+      permanenceIncrement=permanenceInc,
+      permanenceDecrement=permanenceDec,
+      seed=seed)
+
+    self.infActiveState = {"t": None}
+
+
+  def compute(self, bottomUpInput, enableLearn, computeInfOutput=None):
+    """
+    (From `TP.py`)
+    Handle one compute, possibly learning.
+
+    @param bottomUpInput     The bottom-up input, typically from a spatial pooler
+    @param enableLearn       If true, perform learning
+    @param computeInfOutput  If None, default behavior is to disable the inference
+                             output when enableLearn is on.
+                             If true, compute the inference output
+                             If false, do not compute the inference output
+    """
+    super(MonitoredTPShim, self).compute(set(bottomUpInput.nonzero()[0]),
+                                             learn=enableLearn)
+    numberOfCells = self.numberOfCells()
+
+    activeState = numpy.zeros(numberOfCells)
+    activeState[self.getCellIndices(self.activeCells)] = 1
+    self.infActiveState["t"] = activeState
+
+    output = numpy.zeros(numberOfCells)
+    output[self.getCellIndices(self.predictiveCells | self.activeCells)] = 1
+    return output
+
+
+  def getActiveState(self):
+    activeState = numpy.zeros(self.numberOfCells())
+    activeState[self.getCellIndices(self.activeCells)] = 1
+    return activeState
+
+
   def getPredictedState(self):
     predictedState = numpy.zeros(self.numberOfCells())
     predictedState[self.getCellIndices(self.predictiveCells)] = 1
