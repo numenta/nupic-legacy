7dd736a8243dceeaff2927bf9450d9ad84c2d555
Marek Otahal <markotahal@gmail.com>
2015-09-01 18:27:29 +0200

    PCA_Node test: some fixes, WIP
    does not work as it requires nupic.vision now

--- tests/unit/nupic/regions/pcanode_test.py
+++ tests/unit/nupic/regions/pcanode_test.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # ----------------------------------------------------------------------
 # Numenta Platform for Intelligent Computing (NuPIC)
-# Copyright (C) 2014, Numenta, Inc.  Unless you have an agreement
+# Copyright (C) 2015, Numenta, Inc.  Unless you have an agreement
 # with Numenta, Inc., for a separate license for this software code, the
 # following terms and conditions apply:
 #
@@ -20,7 +20,8 @@
 # http://numenta.org/licenses/
 # ----------------------------------------------------------------------
 
-import unittest2 as unittest
+import unittest
+import numpy
 
 from nupic.regions.PyRegion import PyRegion
 from nupic.engine import *
@@ -55,7 +56,7 @@ class PCANodeTest(unittest.TestCase):
     for i in range(10):
       pca.getSelf()._testInputs = numpy.random.random([inputSize])
       net.run(1)
-      #print s.sendRequest('nodeOPrint pca_node')
+      print s.sendRequest('nodeOPrint pca_node')
 
 
 
