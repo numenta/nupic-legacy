43098c505e77db8cbba63311eec80b40ed7b34d0
Vitaly Kruglikov <vkruglikov@numenta.com>
2015-09-01 18:36:09 -0700

    Updated docstring in AnomalyLikelihood._calcSkipRecords() per Scott's code review feedback.

--- nupic/algorithms/anomaly_likelihood.py
+++ nupic/algorithms/anomaly_likelihood.py
@@ -152,6 +152,12 @@ class AnomalyLikelihood(object):
   def _calcSkipRecords(numIngested, windowSize, learningPeriod):
     """Return the value of skipRecords for passing to estimateAnomalyLikelihoods
 
+    If `windowSize` is very large (bigger than the amount of data) then this
+    could just return `learningPeriod`. But when some values have fallen out of
+    the historical sliding window of anomaly records, then we have to take those
+    into account as well so we return the `learningPeriod` minus the number
+    shifted out.
+
     :param int numIngested: number of data points that have been added to the
       sliding window of historical data points.
     :param int windowSize: size of sliding window of historical data points.
