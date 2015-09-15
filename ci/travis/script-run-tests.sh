9b69511bd3196c89411fbe3f1929c73654d0127e
Chetan Surpur <chetan51@gmail.com>
2015-09-15 11:20:27 -0700

    Revert "Fix syntax error"
    This reverts commit fa3abc6769306e51a0163d8262c151131e467662.

--- ci/travis/script-run-tests.sh
+++ ci/travis/script-run-tests.sh
@@ -23,7 +23,7 @@
 echo
 echo Running script-run-tests.sh...
 pushd
-echo "Current directory (before tests):"
+echo Current directory (before tests):
 pwd
 echo
 
@@ -37,6 +37,6 @@ ${TRAVIS_BUILD_DIR}/scripts/run_nupic_tests -i --coverage --failfast || exit
 mv ${TRAVIS_BUILD_DIR}/.coverage ${TRAVIS_BUILD_DIR}/.coverage_integration
 
 popd
-echo "Current directory (after tests):"
+echo Current directory (after tests):
 pwd
 echo
