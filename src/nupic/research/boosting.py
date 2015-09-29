7f717e94f7a86b7896d49ef433ca5d179468a7eb
Marek Otahal <markotahal@gmail.com>
2015-09-29 23:52:58 +0200

    SpatialPooler: extract boosting to a separate file

---
+++ src/nupic/research/boosting.py
@@ -0,0 +1,310 @@
+import numpy
+
+from nupic.bindings.math import (GetNTAReal,
+                                 Random as NupicRandom)
+
+realDType = GetNTAReal()
+
+
+
+class Boosting(object):
+  """
+  Implements boosting for columns of the spatial pooler. 
+  """
+
+  def __init__(self,
+               numColumns,
+               maxBoost,
+               minPctOverlapDutyCycle,
+               minPctActiveDutyCycle):
+    self._boostFactors = numpy.ones(numColumns, dtype=realDType)
+    self._maxBoost = maxBoost
+    self._minActiveDutyCycles = numpy.zeros(numColumns, dtype=realDType)
+    self._activeDutyCycles = numpy.zeros(numColumns, dtype=realDType)
+    self._overlapDutyCycles = numpy.zeros(numColumns, dtype=realDType)
+    self._minOverlapDutyCycles = numpy.zeros(numColumns, dtype=realDType)
+    self._minPctOverlapDutyCycles = minPctOverlapDutyCycle
+    self._minPctActiveDutyCycles = minPctActiveDutyCycle
+
+
+  def getMaxBoost(self):
+    """Returns the maximum boost value"""
+    return self._maxBoost
+
+
+  def setMaxBoost(self, maxBoost):
+    """Sets the maximum boost value"""
+    self._maxBoost = maxBoost
+
+
+  def getBoostFactors(self, boostFactors):
+    """Returns the boost factors for all columns. 'boostFactors' size must
+    match the number of columns"""
+    boostFactors[:] = self._boostFactors[:]
+
+
+  def setBoostFactors(self, boostFactors):
+    """Sets the boost factors for all columns. 'boostFactors' size must match
+    the number of columns"""
+    self._boostFactors[:] = boostFactors[:]
+
+
+  def getMinActiveDutyCycles(self, minActiveDutyCycles):
+    """Returns the minimum activity duty cycles for all columns.
+    '_minActiveDutyCycles' size must match the number of columns"""
+    minActiveDutyCycles[:] = self._minActiveDutyCycles[:]
+
+
+  def setMinActiveDutyCycles(self, minActiveDutyCycles):
+    """Sets the minimum activity duty cycles for all columns.
+    '_minActiveDutyCycles' size must match the number of columns"""
+    self._minActiveDutyCycles = minActiveDutyCycles
+
+
+  def getActiveDutyCycles(self, activeDutyCycles):
+    """Returns the activity duty cycles for all columns. 'activeDutyCycles'
+    size must match the number of columns"""
+    activeDutyCycles[:] = self._activeDutyCycles[:]
+
+
+  def setActiveDutyCycles(self, activeDutyCycles):
+    """Sets the activity duty cycles for all columns. 'activeDutyCycles'
+    size must match the number of columns"""
+    self._activeDutyCycles[:] = activeDutyCycles
+
+
+  def getOverlapDutyCycles(self, overlapDutyCycles):
+    """Returns the overlap duty cycles for all columns. 'overlapDutyCycles'
+    size must match the number of columns"""
+    overlapDutyCycles[:] = self._overlapDutyCycles[:]
+
+
+  def setOverlapDutyCycles(self, overlapDutyCycles):
+    """Sets the overlap duty cycles for all columns. 'overlapDutyCycles'
+    size must match the number of columns"""
+    self._overlapDutyCycles[:] = overlapDutyCycles
+
+
+  def getMinOverlapDutyCycles(self, minOverlapDutyCycles):
+    """Returns the minimum overlap duty cycles for all columns.
+    '_minOverlapDutyCycles' size must match the number of columns"""
+    minOverlapDutyCycles[:] = self._minOverlapDutyCycles[:]
+
+
+  def setMinOverlapDutyCycles(self, minOverlapDutyCycles):
+    """Sets the minimum overlap duty cycles for all columns.
+    '_minOverlapDutyCycles' size must match the number of columns"""
+    self._minOverlapDutyCycles[:] = minOverlapDutyCycles[:]
+
+
+  def getMinPctOverlapDutyCycles(self):
+    """Returns the minimum tolerated overlaps, given as percent of
+    neighbors overlap score"""
+    return self._minPctOverlapDutyCycles
+
+
+  def setMinPctOverlapDutyCycles(self, minPctOverlapDutyCycles):
+    """Sets the minimum tolerated activity duty cycle, given as percent of
+    neighbors' activity duty cycle"""
+    self._minPctOverlapDutyCycles = minPctOverlapDutyCycles
+
+
+  def getMinPctActiveDutyCycles(self):
+    """Returns the minimum tolerated activity duty cycle, given as percent of
+    neighbors' activity duty cycle"""
+    return self._minPctActiveDutyCycles
+
+
+  def setMinPctActiveDutyCycles(self, minPctActiveDutyCycles):
+    """Sets the minimum tolerated activity duty, given as percent of
+    neighbors' activity duty cycle"""
+    self._minPctActiveDutyCycles = minPctActiveDutyCycles
+
+
+  def getBoostedOverlaps(self, overlaps, learn=True):
+    """
+    Compute boosted overlaps, 
+    @param inputVector: 1D ndarray of realDType, size of input to SP
+    """
+    # Apply boosting when learning is on
+    if learn:
+      boostedOverlaps = self._boostFactors * overlaps
+    else:
+      boostedOverlaps = overlaps
+
+    return boostedOverlaps
+
+
+  def updateBoosting(self, overlaps, activeColumns, doUpdateRound, doGlobal):
+    self._updateBoostFactors()
+    self._bumpUpWeakColumns()
+    self._updateDutyCycles(overlaps, activeColumns)
+    if doUpdateRound:
+      self._updateMinDutyCycles(doGlobal)
+
+
+  def _updateBoostFactors(self):
+    """
+    Update the boost factors for all columns. The boost factors are used to
+    increase the overlap of inactive columns to improve their chances of
+    becoming active. and hence encourage participation of more columns in the
+    learning process. This is a line defined as: y = mx + b boost =
+    (1-maxBoost)/minDuty * dutyCycle + maxFiringBoost. Intuitively this means
+    that columns that have been active enough have a boost factor of 1, meaning
+    their overlap is not boosted. Columns whose active duty cycle drops too much
+    below that of their neighbors are boosted depending on how infrequently they
+    have been active. The more infrequent, the more they are boosted. The exact
+    boost factor is linearly interpolated between the points (dutyCycle:0,
+    boost:maxFiringBoost) and (dutyCycle:minDuty, boost:1.0).
+
+            boostFactor
+                ^
+    maxBoost _  |
+                |\
+                | \
+          1  _  |  \ _ _ _ _ _ _ _
+                |
+                +--------------------> activeDutyCycle
+                   |
+            minActiveDutyCycle
+    """
+
+    mask = numpy.where(self._minActiveDutyCycles > 0)[0]
+    self._boostFactors[mask] = ((1 - self._maxBoost) /
+      self._minActiveDutyCycles[mask] * self._activeDutyCycles[mask]
+        ).astype(realDType) + self._maxBoost
+
+    self._boostFactors[self._activeDutyCycles > self._minActiveDutyCycles] = 1.0
+
+
+  def _bumpUpWeakColumns(self):
+    """
+    This method increases the permanence values of synapses of columns whose
+    activity level has been too low. Such columns are identified by having an
+    overlap duty cycle that drops too much below those of their peers. The
+    permanence values for such columns are increased.
+    """
+    weakColumns = numpy.where(self._overlapDutyCycles
+                                < self._minOverlapDutyCycles)[0]
+    for columnIndex in weakColumns:
+      perm = self._permanences[columnIndex].astype(realDType)
+      maskPotential = numpy.where(self._potentialPools[columnIndex] > 0)[0]
+      perm[maskPotential] += self._synPermBelowStimulusInc
+      self._updatePermanencesForColumn(perm, columnIndex, raisePerm=False)
+
+
+  def _updateMinDutyCycles(self, doGlobal=False):
+    """
+    Updates the minimum duty cycles defining normal activity for a column. A
+    column with activity duty cycle below this minimum threshold is boosted.
+    """
+    if doGlobal:
+      self._updateMinDutyCyclesGlobal()
+    else:
+      self._updateMinDutyCyclesLocal()
+
+
+  def _updateMinDutyCyclesGlobal(self):
+    """
+    Updates the minimum duty cycles in a global fashion. Sets the minimum duty
+    cycles for the overlap and activation of all columns to be a percent of the
+    maximum in the region, specified by minPctOverlapDutyCycle and
+    minPctActiveDutyCycle respectively. Functionality it is equivalent to
+    _updateMinDutyCyclesLocal, but this function exploits the globality of the
+    computation to perform it in a straightforward, and more efficient manner.
+    """
+    self._minOverlapDutyCycles.fill(
+        self._minPctOverlapDutyCycles * self._overlapDutyCycles.max()
+      )
+    self._minActiveDutyCycles.fill(
+        self._minPctActiveDutyCycles * self._activeDutyCycles.max()
+      )
+
+
+  def _updateMinDutyCyclesLocal(self):
+    """
+    Updates the minimum duty cycles. The minimum duty cycles are determined
+    locally. Each column's minimum duty cycles are set to be a percent of the
+    maximum duty cycles in the column's neighborhood. Unlike
+    _updateMinDutyCyclesGlobal, here the values can be quite different for
+    different columns.
+    """
+    for i in xrange(self._numColumns):
+      maskNeighbors = numpy.append(i,
+        self._getNeighborsND(i, self._columnDimensions,
+        self._inhibitionRadius))
+      self._minOverlapDutyCycles[i] = (
+        self._overlapDutyCycles[maskNeighbors].max() *
+        self._minPctOverlapDutyCycles
+      )
+      self._minActiveDutyCycles[i] = (
+        self._activeDutyCycles[maskNeighbors].max() *
+        self._minPctActiveDutyCycles
+      )
+
+
+  def _updateDutyCycles(self, overlaps, activeColumns):
+    """
+    Updates the duty cycles for each column. The OVERLAP duty cycle is a moving
+    average of the number of inputs which overlapped with the each column. The
+    ACTIVITY duty cycles is a moving average of the frequency of activation for
+    each column.
+
+    Parameters:
+    ----------------------------
+    @param overlaps:
+                    An array containing the overlap score for each column.
+                    The overlap score for a column is defined as the number
+                    of synapses in a "connected state" (connected synapses)
+                    that are connected to input bits which are turned on.
+    @param activeColumns:
+                    An array containing the indices of the active columns,
+                    the sparse set of columns which survived inhibition
+    """
+    overlapArray = numpy.zeros(self._numColumns, dtype=realDType)
+    activeArray = numpy.zeros(self._numColumns, dtype=realDType)
+    overlapArray[overlaps > 0] = 1
+    activeArray[activeColumns] = 1
+
+    period = self._dutyCyclePeriod
+    if (period > self._iterationNum):
+      period = self._iterationNum
+
+    self._overlapDutyCycles = self._updateDutyCyclesHelper(
+                                self._overlapDutyCycles,
+                                overlapArray,
+                                period
+                              )
+
+    self._activeDutyCycles = self._updateDutyCyclesHelper(
+                                self._activeDutyCycles,
+                                activeArray,
+                                period
+                              )
+
+
+  @staticmethod
+  def _updateDutyCyclesHelper(dutyCycles, newInput, period):
+    """
+    Updates a duty cycle estimate with a new value. This is a helper
+    function that is used to update several duty cycle variables in
+    the Column class, such as: overlapDutyCucle, activeDutyCycle,
+    minPctDutyCycleBeforeInh, minPctDutyCycleAfterInh, etc. returns
+    the updated duty cycle. Duty cycles are updated according to the following
+    formula:
+
+                  (period - 1)*dutyCycle + newValue
+      dutyCycle := ----------------------------------
+                              period
+
+    Parameters:
+    ----------------------------
+    @param dutyCycles: An array containing one or more duty cycle values that need
+                    to be updated
+    @param newInput: A new numerical value used to update the duty cycle
+    @param period:  The period of the duty cycle
+    """
+    assert(period >= 1)
+    return (dutyCycles * (period -1.0) + newInput) / period
+
+
