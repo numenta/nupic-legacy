61d4877ccad745e5afffde70f30795477bdec24d
Karthik Katipally <kkatipally@umassd.edu>
2015-10-05 17:11:06 -0400

    Update hello_tm.py

--- examples/tp/hello_tm.py
+++ examples/tp/hello_tm.py
@@ -6,7 +6,7 @@
 # following terms and conditions apply:
 #
 # This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License version 3 as
+# it under the terms of the GNU Affero General Public License version 3 as
 # published by the Free Software Foundation.
 #
 # This program is distributed in the hope that it will be useful,
