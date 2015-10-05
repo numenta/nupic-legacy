7fe1f68cbdf4694c0f3da9a6213b4618a02d4e5c
Karthik Katipally <kkatipally@umassd.edu>
2015-10-05 17:39:38 -0400

    Update hello_tm.py

--- examples/tp/hello_tm.py
+++ examples/tp/hello_tm.py
@@ -6,7 +6,7 @@
 # following terms and conditions apply:
 #
 # This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License version 3 as
+# it under the terms of the GNU Affero Public License version 3 as
 # published by the Free Software Foundation.
 #
 # This program is distributed in the hope that it will be useful,
@@ -14,11 +14,12 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 # See the GNU Affero Public License for more details.
 #
-# You should have received a copy of the GNU General Public License
+# You should have received a copy of the GNU Affero Public License
 # along with this program.  If not, see http://www.gnu.org/licenses.
 #
 # http://numenta.org/licenses/
 # ----------------------------------------------------------------------
+
 
 print """
 This program shows how to access the Temporal Memory directly by demonstrating
