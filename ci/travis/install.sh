2e23f3cec2f56f824a3f6844fad7493c7bacad0e
Matthew Taylor <matt@numenta.org>
2015-10-20 10:21:08 -0700

    Experimenting with different build matrices.

--- ci/travis/install.sh
+++ ci/travis/install.sh
@@ -34,7 +34,7 @@ pip --version
 
 # Build NuPIC
 cd ${TRAVIS_BUILD_DIR}
-python setup.py install
+python setup.py install --user
 
 # Show nupic installation folder by trying to import nupic, if works, it prints
 # the absolute path of nupic.__file__, which the installation folder itself.
