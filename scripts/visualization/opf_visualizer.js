1f59019d29c9c38e494728e76020c6c9702427ac
Jeff Fohl <jeff@fohl.com>
2015-10-29 13:06:46 -0700

    Addresses #2658. Fixes issue of Firefox being more strict about Date object creation.

--- scripts/visualization/opf_visualizer.js
+++ scripts/visualization/opf_visualizer.js
@@ -76,7 +76,10 @@ angular.module('app').controller('AppCtrl', ['$scope', '$timeout', function($sco
         var num;
         if (x === 0) {
           // this should always be the timestamp. See generateFieldMap
-          num = new Date(data[i][loadedFields[x]]);
+          var dateTime =  data[i][loadedFields[x]].split(" ");
+          var date = dateTime[0].split("-");
+          var time = dateTime[1].split(":");
+          num = new Date(date[0],date[1],date[2],time[0],time[1],time[2]);
         } else {
           num = (data[i][loadedFields[x]] === "None") ? 0 : data[i][loadedFields[x]];
         }
