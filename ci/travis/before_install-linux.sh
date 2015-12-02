71d5e5c7dd93e06aa2e8b8c6e885066f2c85fe10
Austin Marshall <amarshall@numenta.com>
2015-12-02 14:53:57 -0800

    Remove dependency on nupic-linux64

--- ci/travis/before_install-linux.sh
+++ ci/travis/before_install-linux.sh
@@ -32,22 +32,6 @@ if [ $CC == 'gcc' ]; then
     export CXX='g++-4.8'
 fi
 
-echo ">>> Installing nupic-linux64..."
-git clone https://github.com/numenta/nupic-linux64.git
-(cd nupic-linux64 && git reset --hard 99863c7da8b923c57bb4e59530ab087c91fd3992)
-source nupic-linux64/bin/activate
-
-# Upgrade the version of pip included with nupic-linux64
-# TODO: remove after nupic-linux64 has been updated
-pip install --upgrade --install-option="--prefix=`pwd`/nupic-linux64" pip
-pip --version
-
-pip uninstall numpy --yes
-
-# Assuming pip 1.5.X is installed.
-echo "pip install wheel --user"
-pip install wheel --user -q
-
 # Fetch nupic.core build
 export NUPIC_CORE_COMMITISH=`python -c "execfile('.nupic_modules'); print NUPIC_CORE_COMMITISH"`
 echo "Downloading nupic.core build: https://s3-us-west-2.amazonaws.com/artifacts.numenta.org/numenta/nupic.core/nupic_core-${NUPIC_CORE_COMMITISH}-linux64.tar.gz"
@@ -58,8 +42,3 @@ ls home/travis/build/numenta/nupic.core/bindings/py/dist/wheels
 
 # Install nupic.bindings and dependencies from wheels
 pip install --user --no-index --find-links=home/travis/build/numenta/nupic.core/bindings/py/dist/wheels nupic.bindings
-
-# Workaround for multiprocessing.Queue SemLock error from run_opf_bechmarks_test.
-# See: https://github.com/travis-ci/travis-cookbooks/issues/155
-# Commented out to test to see if it works witout it in container mode.
-# sudo rm -rf /dev/shm && sudo ln -s /run/shm /dev/shm
