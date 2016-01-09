90336b026b088732a315dcb1089085b4455a4d5e
Subutai Ahmad <sahmad@numenta.com>
2016-01-08 16:54:52 -0800

    Partition Id accessors
    Implemented getPartitionIdPerPattern and getPartitionIdList. Added tests for same.

--- tests/unit/nupic/algorithms/knn_classifier_test.py
+++ tests/unit/nupic/algorithms/knn_classifier_test.py
@@ -265,6 +265,11 @@ class KNNClassifierTest(unittest.TestCase):
 
     self.assertEquals(classifier.getNumPartitionIds(), 3)
 
+    # Check that the full set of partition ids is what we expect
+    self.assertItemsEqual(classifier.getPartitionIdPerPattern(),
+                          [433, 213, np.inf, 433, 413])
+    self.assertItemsEqual(classifier.getPartitionIdList(),[433, 413, 213])
+
     # Remove two rows - all indices shift down
     self.assertEquals(classifier._removeRows([0,2]), 2)
     self.assertItemsEqual(classifier.getPatternIndicesWithPartitionId(433),
@@ -275,6 +280,10 @@ class KNNClassifierTest(unittest.TestCase):
     # Remove another row and check number of partitions have decreased
     classifier._removeRows([0])
     self.assertEquals(classifier.getNumPartitionIds(), 2)
+
+    # Check that the full set of partition ids is what we expect
+    self.assertItemsEqual(classifier.getPartitionIdPerPattern(), [433, 413])
+    self.assertItemsEqual(classifier.getPartitionIdList(),[433, 413])
 
 
 
