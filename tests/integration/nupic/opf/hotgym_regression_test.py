3d151531dc491043efb28d0c9de5610a93defff9
Subutai Ahmad <sahmad@numenta.com>
2016-01-19 11:41:40 -0800

    Updated test with new error value
    Updated nupic.core
    Updated NUPIC_CORE_COMMITISH

--- tests/integration/nupic/opf/hotgym_regression_test.py
+++ tests/integration/nupic/opf/hotgym_regression_test.py
@@ -63,7 +63,7 @@ class HotgymRegressionTest(unittest.TestCase):
       # Changes that affect prediction results will cause this test to fail. If
       # the change is understood and reviewers agree that there has not been a
       # regression then this value can be updated to reflect the new result.
-      self.assertLess(abs(float(lastRow[14])-5.90725098207), 0.04)
+      self.assertAlmostEqual(float(lastRow[14]), 5.87970891453)
 
     finally:
       shutil.rmtree(resultsDir, ignore_errors=True)
