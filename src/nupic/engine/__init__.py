0d7773c617f6914cc5c3798f670ec6fe3d39ca22
Marcus Lewis <mrcslws@gmail.com>
2016-03-21 18:06:42 -0700

    Add getParameter/setParameter support for Bool and BoolArray

--- src/nupic/engine/__init__.py
+++ src/nupic/engine/__init__.py
@@ -28,7 +28,11 @@ from nupic.support.lockattributes import LockAttributesMixin
 import functools
 
 basicTypes = ['Byte', 'Int16', 'UInt16', 'Int32', 'UInt32', 'Int64', 'UInt64',
-              'Real32', 'Real64', 'Handle']
+              'Real32', 'Real64', 'Handle', 'Bool']
+
+arrayTypes = ['ByteArray', 'Int16Array', 'UInt16Array', 'Int32Array',
+              'UInt32Array', 'Int64Array', 'UInt64Array', 'Real32Array',
+              'Real64Array', 'BoolArray']
 
 pyRegions = (
     ("nupic.regions.AnomalyRegion", "AnomalyRegion"),
@@ -62,8 +66,6 @@ def registerBuiltInRegions():
 
 registerBuiltInRegions()
 
-# Import all the array types from engine (there is no HandleArray)
-arrayTypes = [t + 'Array' for t in basicTypes[:-1]]
 for a in arrayTypes:
   exec('from %s import %s as %s' % (engine.__name__, a, a))
 
