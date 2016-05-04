e485d72e505634702171a08a6b6f3b3c1111f5ea
Vitaly Kruglikov <vkruglikov@vitalys-mbp.corp.numenta.com>
2016-05-04 14:09:34 -0700

    Suppress this optional dependency on matplotlib without logging, because python logging implicitly adds the StreamHandler to root logger when calling `logging.debug`, etc., which may undermine an application's logging configuration

--- src/nupic/research/monitor_mixin/plot.py
+++ src/nupic/research/monitor_mixin/plot.py
@@ -31,10 +31,12 @@ try:
   import matplotlib.pyplot as plt
   import matplotlib.cm as cm
 except ImportError:
-  # Suppress; we log it at debug level to avoid polluting the logs of apps
-  # and services that don't care about plotting
-  logging.debug("Cannot import matplotlib. Plot class will not work.",
-                exc_info=True)
+  # Suppress this optional dependency on matplotlib. NOTE we don't log this,
+  # because python logging implicitly adds the StreamHandler to root logger when
+  # calling `logging.debug`, etc., which may undermine an application's logging
+  # configuration.
+  plt = None
+  cm = None
 
 
 
