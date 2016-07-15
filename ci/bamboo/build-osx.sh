94e84230d371d25756bd67e042cbc94d2ddec06e
Austin Marshall <amarshall@numenta.com>
2016-07-15 09:18:16 -0700

    DEVOPS-85 OS X build infrastructure for Bamboo CI environment

---
+++ ci/bamboo/build-osx.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -o errexit
+set -o xtrace
+
+# Fixup $PATH for --user installation
+export PATH=${HOME}/Library/Python/2.7/bin:${PATH}
+export PYTHONPATH=${HOME}/Library/Python/2.7/lib/python/site-packages:${PATH}
+
+# Install pip
+python ci/bamboo/get-pip.py --user --ignore-installed
+
+# Build installable python packages
+python setup.py bdist_wheel
+
+# Install nupic wheel and dependencies, including nupic.bindings artifact in
+# wheelwhouse/
+pip install --user --ignore-installed -f wheelhouse/ dist/nupic-`cat VERSION`*.whl
+
+# Invoke tests
+python setup.py test
