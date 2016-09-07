fae8f9c277a042d760a2fc475a6f0630210968b0
Austin Marshall <amarshall@numenta.com>
2016-09-07 07:52:24 -0700

    Extract version AND comparison from requirements.txt

--- ci/appveyor/get_nupic_bindings_version.py
+++ ci/appveyor/get_nupic_bindings_version.py
@@ -1,13 +1,40 @@
+# ----------------------------------------------------------------------
+# Numenta Platform for Intelligent Computing (NuPIC)
+# Copyright (C) 2016, Numenta, Inc.  Unless you have an agreement
+# with Numenta, Inc., for a separate license for this software code, the
+# following terms and conditions apply:
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero Public License version 3 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU Affero Public License for more details.
+#
+# You should have received a copy of the GNU Affero Public License
+# along with this program.  If not, see http://www.gnu.org/licenses.
+#
+# http://numenta.org/licenses/
+# ----------------------------------------------------------------------
 import os
+import re
+
+
 
 NUPIC = os.environ["NUPIC"]
+REGEXP = re.compile("nupic.bindings([<>=][=]?.+)")
+
 
 
 def extractNupicBindingsVersion(requirementsFile):
   with open(requirementsFile, "r") as f:
-    for line in f.readlines():
-      if line.startswith("nupic.bindings"):
-        return line.split("==").pop()
+    for line in f:
+      matchResult = REGEXP.match(line)
+      if matchResult:
+        return matchResult.group(1)
+
 
 
 if __name__ == "__main__":
