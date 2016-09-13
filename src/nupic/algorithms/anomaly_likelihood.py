f664d40324d0c888c23f03b03330f9e22a4f926e
BoltzmannBrain <alexdlavin@gmail.com>
2016-09-13 08:15:07 -0700

    Fix the redundant flip calc for Gaussian tail prob.

--- src/nupic/algorithms/anomaly_likelihood.py
+++ src/nupic/algorithms/anomaly_likelihood.py
@@ -743,7 +743,7 @@ def normalProbability(x, distributionParams):
   if x < distributionParams["mean"]:
     # Gaussian is symmetrical around mean, so flip to get the tail probability
     xp = 2 * distributionParams["mean"] - x
-    return 1.0 - normalProbability(xp, distributionParams)
+    return normalProbability(xp, distributionParams)
 
   # Calculate the Q function with the complementary error function, explained
   # here: http://www.gaussianwaves.com/2012/07/q-function-and-error-functions
