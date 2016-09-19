89fa49d34541c35bc0c18b9278ee60aa66820fdf
Vitaly Kruglikov <vkruglikov@numenta.com>
2016-09-19 14:10:53 -0700

    Make python2.7 the default alternative for Ubuntu build and test tasks.
    DEVOPS-184 Start mysql and run integration tests on linux.
    
    DEVOPS-184 Suppress mysql root password prompt when installing mysql-server on linux
    
    Install apt-utils in configure-test-ubuntu.sh to avoid "debconf: delaying package configuration, since apt-utils is not installed"
    
    Emit junit xml test results
    
    Use `python setup.py test` to execute unit and integration tests and generate junit and code coverage results.

---
+++ ci/bamboo/test-linux.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+# ----------------------------------------------------------------------
+# Numenta Platform for Intelligent Computing (NuPIC)
+# Copyright (C) 2016, Numenta, Inc.  Unless you have purchased from
+# Numenta, Inc. a separate commercial license for this software code, the
+# following terms and conditions apply:
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero Public License version 3 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU Affero Public License for more details.
+#
+# You should have received a copy of the GNU Affero Public License
+# along with this program.  If not, see http://www.gnu.org/licenses.
+#
+# http://numenta.org/licenses/
+# -----------------------------------------------------------------------------
+
+# Run NuPIC tests on Linux.
+
+# ASSUMES:
+#   1. Current working directory is root of nupic source tree
+#   2. The nupic wheel is in the current working directory
+#
+# OUTPUTS:
+#
+# test results: nupic junit test results will be written to the file
+#               nupic-test-results.xml in the root of the nupic source tree.
+#
+# code coverage report: in tests/htmlcov and tests/.coverage
+
+
+set -o errexit
+set -o xtrace
+
+
+MY_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+NUPIC_ROOT_DIR="$( cd "${MY_DIR}/../.." && pwd )"
+
+
+# ZZZ Temp workaround for linux until we have proper manylinux nupic.bindings
+# wheels on PyPi
+while read line; do
+    if [[ $line == nupic.bindings* ]];
+    then
+        IFS='=' read -ra ADDR <<< "$line"
+        NUPIC_BINDINGS_VERSION="${ADDR[2]}"
+    fi
+done <requirements.txt
+
+pip install https://s3-us-west-2.amazonaws.com/artifacts.numenta.org/numenta/nupic.core/releases/nupic.bindings/nupic.bindings-${NUPIC_BINDINGS_VERSION}-cp27-none-linux_x86_64.whl
+
+
+# Install nupic
+pip install nupic-*.whl
+
+
+#
+# Test
+#
+
+# Some tests require NUPIC env var to locate config files
+# Some nupic config files reference USER env var, so it needs to be defined.
+
+
+# Run unit and integration tests (integration tests require mysql server)
+PYTEST_OPTS="--verbose --boxed --junit-xml=`pwd`/nupic-test-results.xml --cov nupic --cov-report html"
+
+NUPIC="${NUPIC_ROOT_DIR}" \
+USER=$(whoami) \
+  python setup.py test --pytest-args="${PYTEST_OPTS} unit integration"
