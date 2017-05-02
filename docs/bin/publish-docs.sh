d501415c8b6a9be6443dc87e1c4cb73782651b7d
Matthew Taylor <matt@numenta.org>
2017-05-02 08:09:08 -0700

    Adding back git push to gh-pages

--- docs/bin/publish-docs.sh
+++ docs/bin/publish-docs.sh
@@ -124,16 +124,16 @@ versions=("latest" "${versions[@]}");
 echo "${versions[@]}"
 build_html_index "$NUPIC/index.html" versions[@]
 
-# # Add latest/stable version builds and new index
-# git add "$VERSION" index.html
-# # Runnning these individually in case they don't exist yet.
-# git add latest
-# git add stable
-# if [[ `git status --porcelain` ]]; then
-#   git commit -am "Development documentation build."
-#   git push upstream gh-pages --force
-# else
-#     echo "No doc changes"
-# fi
+# Add latest/stable version builds and new index
+git add "$VERSION" index.html
+# Runnning these individually in case they don't exist yet.
+git add latest
+git add stable
+if [[ `git status --porcelain` ]]; then
+  git commit -am "Development documentation build."
+  git push upstream gh-pages --force
+else
+    echo "No doc changes"
+fi
 
 cd $CWD
