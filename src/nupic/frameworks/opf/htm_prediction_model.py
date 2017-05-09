82ed58714bf313aec5249e4a9ba7060c4cf7c505
Matthew Taylor <matt@numenta.org>
2017-05-09 12:20:31 -0700

    Added labelFilter docs

--- src/nupic/frameworks/opf/htm_prediction_model.py
+++ src/nupic/frameworks/opf/htm_prediction_model.py
@@ -341,10 +341,13 @@ class HTMPredictionModel(Model):
   @requireAnomalyModel
   def anomalyRemoveLabels(self, start, end, labelFilter):
     """
-    Remove labels from the anomaly classifier within this model.
+    Remove labels from the anomaly classifier within this model. Removes all
+    records if ``labelFilter==None``, otherwise only removes the labels equal to
+    ``labelFilter``.
 
     :param start: (int) index to start removing labels
     :param end: (int) index to end removing labels
+    :param labelFilter: (string) If specified, only removes records that match
     """
     self._getAnomalyClassifier().getSelf().removeLabels(start, end, labelFilter)
 
