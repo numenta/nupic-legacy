28f6caf1cd8014bb558fbb12d48b984db6e29723
Matthew Taylor <matt@numenta.org>
2017-05-08 18:01:58 -0700

    Removed unimplemented abstract methods

--- src/nupic/data/file_record_stream.py
+++ src/nupic/data/file_record_stream.py
@@ -387,18 +387,6 @@ class FileRecordStream(RecordStreamIface):
     return record
 
 
-  def getRecordsRange(self, bookmark=None, range=None):
-    raise Exception('getRecordsRange() is not supported for the file storage')
-
-
-  def getLastRecords(self, numRecords):
-    raise Exception('getLastRecords() is not supported for the file storage')
-
-
-  def removeOldData(self):
-    raise Exception('removeOldData is not supported in this class.')
-
-
   def appendRecord(self, record, inputBookmark=None):
     """
     Saves the record in the underlying csv file.
