bec483a1029a7fbb2b2fc4ffde121bf2b7f444ad
Jonathan Mackenzie <jonmac1@gmail.com>
2018-03-07 10:21:51 +1030

    fixed typo and metric schema for experiment

--- src/nupic/swarming/exp_generator/experiment_generator.py
+++ src/nupic/swarming/exp_generator/experiment_generator.py
@@ -1859,6 +1859,8 @@ def _generateMetricSpecs(options):
 def _generateExtraMetricSpecs(options):
   """Generates the non-default metrics specified by the expGenerator params """
   global _metricSpecSchema
+  _metricSpecSchema = {}
+  _metricSpecSchema['properties'] = {}
 
   results = []
   for metric in options['metrics']:
