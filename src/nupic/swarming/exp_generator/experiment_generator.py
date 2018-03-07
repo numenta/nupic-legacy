5350b8b727cf0134e15a192e0270813d3a295163
Jonathan Mackenzie <jonmac1@gmail.com>
2018-03-07 12:33:07 +1030

    Fixed metric spec schema error

--- src/nupic/swarming/exp_generator/experiment_generator.py
+++ src/nupic/swarming/exp_generator/experiment_generator.py
@@ -1858,9 +1858,7 @@ def _generateMetricSpecs(options):
 
 def _generateExtraMetricSpecs(options):
   """Generates the non-default metrics specified by the expGenerator params """
-  global _metricSpecSchema
-  _metricSpecSchema = {}
-  _metricSpecSchema['properties'] = {}
+  _metricSpecSchema = {'properties': {}}
 
   results = []
   for metric in options['metrics']:
